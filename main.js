26d4e248c0b8314c110d430e6baa96e7d6593704
cscheid@research.att.com <cscheid@research.att.com>
2012-06-20 18:29:06 -0400

    more cleanup, now all our reusable javascript lives in js/, page-specific JS on the same page as the .html, and all dependencies live on lib/
    --HG--
    rename : Rsrv.js => js/Rsrv.js
    rename : buffer.js => js/buffer.js
    rename : chart.js => js/chart.js
    rename : facet_chart.js => js/facet_chart.js
    rename : interpreter.js => js/interpreter.js
    rename : objs.js => js/objs.js
    rename : parse.js => js/parse.js
    rename : peg_parser.js => js/peg_parser.js
    rename : plot.js => js/plot.js
    rename : rclient.js => js/rclient.js
    rename : rcloud.js => js/rcloud.js
    rename : websockets.js => js/websockets.js

--- main.js
+++ main.js
@@ -1,30 +1,28 @@
 
 //////////////////////////////////////////////////////////////////////////////
 
-function internal_float(container, id)
-{
-    return container.append("div")
-        .attr("class", "internals-float")
-        .attr("id", id);
-}
+(function() {
+    function internal_float(container, id)
+    {
+        return container.append("div")
+            .attr("class", "internals-float")
+            .attr("id", id);
+    }
 
-function cookie_float(container)
-{
-    var cookie_div = internal_float(container, "internals-cookie-div");
-    cookie_div.append("h3").text("Cookie info");
-    cookie_div.append("ul")
-        .selectAll("li")
-        .data(['user', 'sessid'])
-        .enter()
-        .append("li").text(function (i) { return i + ": " + $.cookies.get(i); });
-}
+    function cookie_float(container)
+    {
+        var cookie_div = internal_float(container, "internals-cookie-div");
+        cookie_div.append("h3").text("Cookie info");
+        cookie_div.append("ul")
+            .selectAll("li")
+            .data(['user', 'sessid'])
+            .enter()
+            .append("li").text(function (i) { return i + ": " + $.cookies.get(i); });
+    }
 
-function init_debug_internals()
-{
+    
     var container = d3.select("#internals-infocontainer");
     cookie_float(container);
 
     internal_float(container, "internals-user-files");
-}
-
-init_debug_internals();
+})();
