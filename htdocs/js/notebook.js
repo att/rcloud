31c13c5841b5aad1b6222daf730ee28272e0f0df
cscheid@research.att.com <cscheid@research.att.com>
2012-06-27 19:03:24 -0400

    reexecuting cells with changed text

--- htdocs/js/notebook.js
+++ htdocs/js/notebook.js
@@ -10,6 +10,7 @@ Notebook.new_cell = function(content, type)
     var result_button = $("<span class='fontawesome-button'><i class='icon-picture' alt='Show Result'></i></span>");
     var hide_button   = $("<span class='fontawesome-button'><i class='icon-resize-small' alt='Hide cell'></i></span>");
     var remove_button = $("<span class='fontawesome-button'><i class='icon-trash' alt='Remove cell'></i></span>");
+    var run_md_button = $("<span class='fontawesome-button'><i class='icon-repeat' alt='Re-execute'></i></span>");
 
     function enable(el) {
         el.removeClass("button-disabled");
@@ -34,14 +35,26 @@ Notebook.new_cell = function(content, type)
         if (!$(e.currentTarget).hasClass("button-disabled"))
             result.remove_self();
     });
+    run_md_button.click(function(e) {
+        r_result_div.html("Computing...");
+        result.execute(widget.getSession().getValue());
+        result.show_result();
+    });
 
     // Ace sets its z-index to be 1000; 
     // "and thus began the great z-index arms race of 2012"
-    var button_float = $("<div style='margin:0.5em; position:relative; float: right; z-index:10000'></div>");
-    button_float.append(source_button);
-    button_float.append(result_button);
-    button_float.append(hide_button);
-    button_float.append(remove_button);
+    var button_float = $("<div style='position:relative; float: right; z-index:10000'></div>");
+    var row1 = $("<div style='margin:0.5em;'></div>");
+    var editor_row = $("<div style='margin:0.5em;'></div>");
+    row1.append(source_button);
+    row1.append(result_button);
+    row1.append(hide_button);
+    row1.append(remove_button);
+    button_float.append(row1);
+    editor_row.append(run_md_button);
+    editor_row.hide();
+    button_float.append(editor_row);
+
     notebook_cell_div.append(button_float);
 
     //////////////////////////////////////////////////////////////////////////
@@ -58,46 +71,49 @@ Notebook.new_cell = function(content, type)
     markdown_div.append(ace_div);
     var widget = ace.edit(ace_div[0]);
     widget.setTheme("ace/theme/chrome");
-    widget.getSession().setValue(content);
     widget.getSession().setUseWrapMode(true);
     widget.resize();
 
     var r_result_div = $('<div class="r-result-div">Computing...</div>');
     inner_div.append(r_result_div);
     
-    if (type === 'markdown') {
-        var wrapped_command = rclient.markdown_wrap_command(content);
-        rclient.send_and_callback(wrapped_command[0], function(r) {
-            r_result_div.html(r.value[1].value[0]);
-            inner_div
-                .find("pre code")
-                .each(function(i, e) {
-                    hljs.highlightBlock(e);
-                });
-            MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
-            result.show_result();
-        });
-    } else if (type === 'interactive') {
-        var wrapped_command = rclient.markdown_wrap_command("```{r}\n" + content + "\n```\n");
-        rclient.send_and_callback(wrapped_command[0], function(r) {
-            r_result_div.html(r.value[1].value[0]);
-            inner_div
-                .find("pre code")
-                .each(function(i, e) {
-                    hljs.highlightBlock(e);
-                });
-            MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
-            result.show_result();
-        });
-    } else alert("Can only do markdown for now!");
-
     var result = {
+        execute: function(value) {
+            if (type === 'markdown') {
+                widget.getSession().setValue(value);
+                var wrapped_command = rclient.markdown_wrap_command(value);
+                rclient.send_and_callback(wrapped_command[0], function(r) {
+                    r_result_div.html(r.value[1].value[0]);
+                    inner_div
+                        .find("pre code")
+                        .each(function(i, e) {
+                            hljs.highlightBlock(e);
+                        });
+                    MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+                    this.show_result();
+                });
+            } else if (type === 'interactive') {
+                widget.getSession().setValue(value);
+                var wrapped_command = rclient.markdown_wrap_command("```{r}\n" + value + "\n```\n");
+                rclient.send_and_callback(wrapped_command[0], function(r) {
+                    r_result_div.html(r.value[1].value[0]);
+                    inner_div
+                        .find("pre code")
+                        .each(function(i, e) {
+                            hljs.highlightBlock(e);
+                        });
+                    MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+                    this.show_result();
+                });
+            } else alert("Can only do markdown or interactive for now!");
+        },
         show_source: function() {
             notebook_cell_div.css({'height': '70%'});
             disable(source_button);
             enable(result_button);
             enable(hide_button);
             enable(remove_button);
+            editor_row.show();
 
             markdown_div.show();
             widget.resize();
@@ -110,15 +126,18 @@ Notebook.new_cell = function(content, type)
             enable(hide_button);
             enable(remove_button);
 
+            editor_row.hide();
             markdown_div.hide();
             r_result_div.show();
         },
         hide_all: function() {
+            notebook_cell_div.css({'height': ''});
             enable(source_button);
             enable(result_button);
             disable(hide_button);
             enable(remove_button);
 
+            editor_row.hide();
             markdown_div.hide();
             r_result_div.hide();
         },
@@ -130,6 +149,7 @@ Notebook.new_cell = function(content, type)
         }
     };
 
+    result.execute(content);
     result.show_result();
 
     return result;    
