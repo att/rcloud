fd95bad55e26b2f75d19070138466915fc8af89a
cscheid@research.att.com <cscheid@research.att.com>
2012-06-27 13:54:01 -0400

    fontawesome update, cells have icons now, hide and show correctly

--- htdocs/js/rcloud_bundle.js
+++ htdocs/js/rcloud_bundle.js
@@ -2501,30 +2501,56 @@ Notebook = {};
 Notebook.new_cell = function(content, type)
 {
     var notebook_cell_div  = $("<div class='notebook-cell'></div>");
-    var source_button = $("<a style='margin:.5em' href='#'>[Show source]</a>");
-    var result_button = $("<a style='margin:.5em' href='#'>[Show result]</a>");
-    source_button.click(function() {
-        result.show_source();
+    var inner_div = $("<div></div>");
+    var clear_div = $("<div style='clear:both;'></div>");
+    notebook_cell_div.append(inner_div);
+    notebook_cell_div.append(clear_div);
+    var source_button = $("<span class='fontawesome-button'><i class='icon-edit' alt='Show Source'></i></span>");
+    var result_button = $("<span class='fontawesome-button'><i class='icon-picture' alt='Show Result'></i></span>");
+    var hide_button   = $("<span class='fontawesome-button'><i class='icon-resize-small' alt='Hide cell'></i></span>");
+    var remove_button = $("<span class='fontawesome-button'><i class='icon-trash' alt='Remove cell'></i></span>");
+
+    function enable(el) {
+        el.removeClass("button-disabled");
+    }
+    function disable(el) {
+        el.addClass("button-disabled");
+    }
+
+    source_button.click(function(e) {
+        if (!$(e.currentTarget).hasClass("button-disabled"))
+            result.show_source();
     });
-    result_button.click(function() {
-        result.show_result();
+    result_button.click(function(e) {
+        if (!$(e.currentTarget).hasClass("button-disabled"))
+            result.show_result();
+    });
+    hide_button.click(function(e) {
+        if (!$(e.currentTarget).hasClass("button-disabled"))
+            result.hide_all();
+    });
+    remove_button.click(function(e) {
+        if (!$(e.currentTarget).hasClass("button-disabled"))
+            result.remove_self();
     });
     var button_float = $("<div style='float: right'></div>");
-    notebook_cell_div.append(button_float);
+    inner_div.append(button_float);
     button_float.append(source_button);
     button_float.append(result_button);
+    button_float.append(remove_button);
+    button_float.append(hide_button);
 
     var markdown_div = $('<pre class="r-sent-command markdown-div"></pre>').html('> ' + content);
-    notebook_cell_div.append(markdown_div);
+    inner_div.append(markdown_div);
 
     var r_result_div = $('<div class="r-result-div">Computing...</div>');
-    notebook_cell_div.append(r_result_div);
+    inner_div.append(r_result_div);
     
     if (type === 'markdown') {
         var wrapped_command = rclient.markdown_wrap_command(content);
         rclient.send_and_callback(wrapped_command[0], function(r) {
             r_result_div.html(r.value[1].value[0]);
-            notebook_cell_div
+            inner_div
                 .find("pre code")
                 .each(function(i, e) {
                     hljs.highlightBlock(e);
@@ -2536,7 +2562,7 @@ Notebook.new_cell = function(content, type)
         var wrapped_command = rclient.markdown_wrap_command("```{r}\n" + content + "\n```\n");
         rclient.send_and_callback(wrapped_command[0], function(r) {
             r_result_div.html(r.value[1].value[0]);
-            notebook_cell_div
+            inner_div
                 .find("pre code")
                 .each(function(i, e) {
                     hljs.highlightBlock(e);
@@ -2548,16 +2574,34 @@ Notebook.new_cell = function(content, type)
 
     var result = {
         show_source: function() {
-            source_button.hide();
-            result_button.show();
+            disable(source_button);
+            enable(result_button);
+            enable(hide_button);
+            enable(remove_button);
+
             markdown_div.show();
             r_result_div.hide();
         },
         show_result: function() {
-            source_button.show();
-            result_button.hide();
-            r_result_div.show();
+            enable(source_button);
+            disable(result_button);
+            enable(hide_button);
+            enable(remove_button);
+
             markdown_div.hide();
+            r_result_div.show();
+        },
+        hide_all: function() {
+            enable(source_button);
+            enable(result_button);
+            disable(hide_button);
+            enable(remove_button);
+
+            markdown_div.hide();
+            r_result_div.hide();
+        },
+        remove_self: function() {
+            notebook_cell_div.remove();
         },
         div: function() {
             return notebook_cell_div;
