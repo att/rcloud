79fffd4fcfc93a870c33739b61143e020c2c34ef
cscheid@research.att.com <cscheid@research.att.com>
2012-07-16 18:33:30 -0400

    tooltips, transitions

--- htdocs/js/notebook/cell/create_html_view.js
+++ htdocs/js/notebook/cell/create_html_view.js
@@ -1,14 +1,26 @@
-Notebook.Cell.create_html_view = function(cell_model)
+(function() {
+
+function fa_button(which, title)
+{
+    return $("<span class='fontawesome-button'><i class='" + 
+             which + 
+             "'></i></span>").tooltip({ 
+                 title: title, 
+                 delay: { show: 250, hide: 0 }
+             });
+}
+
+function create_markdown_cell_html_view(cell_model)
 {
     var notebook_cell_div  = $("<div class='notebook-cell'></div>");
 
     //////////////////////////////////////////////////////////////////////////
     // button bar
-    var source_button = $("<span class='fontawesome-button'><i class='icon-edit' alt='Show Source'></i></span>");
-    var result_button = $("<span class='fontawesome-button'><i class='icon-picture' alt='Show Result'></i></span>");
-    var hide_button   = $("<span class='fontawesome-button'><i class='icon-resize-small' alt='Hide cell'></i></span>");
-    var remove_button = $("<span class='fontawesome-button'><i class='icon-trash' alt='Remove cell'></i></span>");
-    var run_md_button = $("<span class='fontawesome-button'><i class='icon-repeat' alt='Re-execute'></i></span>");
+    var source_button = fa_button("icon-edit", "source");
+    var result_button = fa_button("icon-picture", "result");
+    var hide_button   = fa_button("icon-resize-small", "hide");
+    var remove_button = fa_button("icon-trash", "remove");
+    var run_md_button = fa_button("icon-repeat", "run");
 
     function update_model() {
         cell_model.content(widget.getSession().getValue());
@@ -36,6 +48,10 @@ Notebook.Cell.create_html_view = function(cell_model)
     remove_button.click(function(e) {
         if (!$(e.currentTarget).hasClass("button-disabled")) {
             cell_model.parent_model.controller.remove_cell(cell_model);
+
+            // twitter bootstrap gets confused about its tooltips if parent element 
+            // is deleted while tooltip is active; let's help it
+            $(".tooltip").remove();
         }
     });
     function execute_cell() {
@@ -108,8 +124,10 @@ Notebook.Cell.create_html_view = function(cell_model)
         }
     });
 
-    var r_result_div = $('<div class="r-result-div"></div>');
+    var r_result_div = $('<div class="r-result-div"><span style="opacity:0.5">Not evaluated</span></div>');
     inner_div.append(r_result_div);
+
+    var current_mode;
 
     var result = {
 
@@ -123,7 +141,9 @@ Notebook.Cell.create_html_view = function(cell_model)
             notebook_cell_div.remove();
         },
         result_updated: function(r) {
+            r_result_div.hide();
             r_result_div.html(r.value[0]);
+            r_result_div.slideDown(150);
 
             // There's a list of things that we need to do to the output:
             var uuid = rcloud.wplot_uuid;
@@ -176,9 +196,11 @@ Notebook.Cell.create_html_view = function(cell_model)
             editor_row.show();
 
             markdown_div.show();
-            widget.resize();
             r_result_div.hide();
+            widget.resize();
             widget.focus();
+
+            current_mode = "source";
         },
         show_result: function() {
             notebook_cell_div.css({'height': ''});
@@ -189,7 +211,8 @@ Notebook.Cell.create_html_view = function(cell_model)
 
             editor_row.hide();
             markdown_div.hide();
-            r_result_div.show();
+            r_result_div.slideDown(150); // show();
+            current_mode = "result";
         },
         hide_all: function() {
             notebook_cell_div.css({'height': ''});
@@ -199,8 +222,11 @@ Notebook.Cell.create_html_view = function(cell_model)
             enable(remove_button);
 
             editor_row.hide();
-            markdown_div.hide();
-            r_result_div.hide();
+            if (current_mode === "result") {
+                r_result_div.slideUp(150); // hide();
+            } else {
+                markdown_div.slideUp(150); // hide();
+            }
         },
         remove_self: function() {
             cell_model.parent_model.remove_cell(cell_model);            
@@ -211,6 +237,9 @@ Notebook.Cell.create_html_view = function(cell_model)
         },
         update_model: function() {
             update_model();
+        },
+        focus: function() {
+            widget.focus();
         }
     };
 
@@ -218,3 +247,237 @@ Notebook.Cell.create_html_view = function(cell_model)
     result.content_updated();
     return result;
 };
+
+function create_interactive_cell_html_view(cell_model)
+{
+    var notebook_cell_div  = $("<div class='notebook-cell'></div>");
+
+    //////////////////////////////////////////////////////////////////////////
+    // button bar
+    var source_button = $("<span class='fontawesome-button'><i class='icon-edit'></i></span>").tooltip({ title: "source" });
+    var result_button = $("<span class='fontawesome-button'><i class='icon-picture'></i></span>").tooltip({ title: "result" });
+    var hide_button   = $("<span class='fontawesome-button'><i class='icon-resize-small'></i></span>").tooltip({ title: "hide" });
+    var remove_button = $("<span class='fontawesome-button'><i class='icon-trash'></i></span>").tooltip({ title: "remove" });
+
+    function update_model() {
+        cell_model.content($(input).val());
+    }
+    function enable(el) {
+        el.removeClass("button-disabled");
+    }
+    function disable(el) {
+        el.addClass("button-disabled");
+    }
+
+    source_button.click(function(e) {
+        if (!$(e.currentTarget).hasClass("button-disabled")) {
+            result.show_source();
+        }
+    });
+    result_button.click(function(e) {
+        if (!$(e.currentTarget).hasClass("button-disabled"))
+            result.show_result();
+    });
+    hide_button.click(function(e) {
+        if (!$(e.currentTarget).hasClass("button-disabled"))
+            result.hide_all();
+    });
+    remove_button.click(function(e) {
+        if (!$(e.currentTarget).hasClass("button-disabled")) {
+            cell_model.parent_model.controller.remove_cell(cell_model);
+
+            // twitter bootstrap gets confused about its tooltips if parent element 
+            // is deleted while tooltip is active; let's help it
+            $(".tooltip").remove();
+        }
+    });
+    function execute_cell() {
+        r_result_div.html("Computing...");
+        update_model();
+        result.show_result();
+        cell_model.controller.execute();
+    }
+
+    // Ace sets its z-index to be 1000; 
+    // "and thus began the great z-index arms race of 2012"
+    var button_float = $("<div style='position:relative; float: right; z-index:10000'></div>");
+    var row1 = $("<div style='margin:0.5em;'></div>");
+    var editor_row = $("<div style='margin:0.5em;'></div>");
+    row1.append(source_button);
+    row1.append(result_button);
+    row1.append(hide_button);
+    row1.append(remove_button);
+    button_float.append(row1);
+    editor_row.hide();
+    button_float.append(editor_row);
+
+    notebook_cell_div.append(button_float);
+
+    //////////////////////////////////////////////////////////////////////////
+
+    var inner_div = $("<div></div>");
+    var clear_div = $("<div style='clear:both;'></div>");
+    notebook_cell_div.append(inner_div);
+    notebook_cell_div.append(clear_div);
+
+    var markdown_div = $('<div style="position: relative; width:100%;"></div>');
+    var cell_buttons_div = $('<div style="position: absolute; right:-0.5em; top:-0.5em"></div>');
+    var insert_cell_button = $('<span class="fontawesome-button"><i class="icon-plus-sign"></i>');
+    inner_div.append(cell_buttons_div);
+    cell_buttons_div.append(insert_cell_button);
+    insert_cell_button.click(function(e) {
+        // truly the wrong way to go about this
+        var base_index = notebook_cell_div.index();
+        var model_index = base_index;
+        shell.insert_markdown_cell_before(model_index);
+    });
+    
+    var ace_div = $('<div style="width:100%; margin-left: 0.5em; margin-top: 0.5em"></div>');
+    inner_div.append(markdown_div);
+    markdown_div.append(ace_div);
+
+    var input = $('<input type="text" style="width:90%"/>');
+    ace_div.append(input);
+    // http://stackoverflow.com/questions/699065
+    input.keypress(function(e) {
+        if (e.which === 13) {
+            execute_cell();
+            e.preventDefault();
+            return false;
+        }
+        return true;
+    });
+
+    var r_result_div = $('<div class="r-result-div"></div>');
+    inner_div.append(r_result_div);
+    var current_mode;
+
+    var result = {
+
+        //////////////////////////////////////////////////////////////////////
+        // pubsub event handlers
+
+        content_updated: function() {
+            input.val(cell_model.content());
+        },
+        self_removed: function() {
+            notebook_cell_div.remove();
+        },
+        result_updated: function(r) {
+            r_result_div.hide();
+            r_result_div.html(r.value[0]);
+            r_result_div.slideDown(150);
+
+            // There's a list of things that we need to do to the output:
+            var uuid = rcloud.wplot_uuid;
+
+            // capture interactive graphics
+            inner_div.find("pre code")
+                .contents()
+                .filter(function() {
+                    return this.nodeValue.indexOf(uuid) !== -1;
+                }).parent().parent()
+                .each(function() {
+                    var uuids = this.childNodes[0].childNodes[0].data.substr(8,73).split("|");
+                    var that = this;
+                    rcloud.resolve_deferred_result(uuids[1], function(data) {
+                        $(that).replaceWith(function() {
+                            return shell.handle(data.value[0].value[0], data);
+                        });
+                    });
+                });
+            // highlight R
+            inner_div
+                .find("pre code")
+                .each(function(i, e) {
+                    hljs.highlightBlock(e);
+                });
+            
+            // typeset the math
+            MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+                
+            this.show_result();
+        },
+
+        //////////////////////////////////////////////////////////////////////
+
+        hide_buttons: function() {
+            button_float.css("display", "none");
+            cell_buttons_div.css("display", "none");
+        },
+        show_buttons: function() {
+            button_float.css("display", null);
+            cell_buttons_div.css("display", null);
+        },
+
+        show_source: function() {
+            notebook_cell_div.css({'height': ''});
+            disable(source_button);
+            enable(result_button);
+            enable(hide_button);
+            enable(remove_button);
+            editor_row.show();
+
+            markdown_div.show();
+            r_result_div.hide();
+            input.focus();
+
+            current_mode = "source";
+        },
+        show_result: function() {
+            notebook_cell_div.css({'height': ''});
+            enable(source_button);
+            disable(result_button);
+            enable(hide_button);
+            enable(remove_button);
+
+            editor_row.hide();
+            markdown_div.hide();
+            r_result_div.slideDown(150);
+            current_mode = "result";
+        },
+        hide_all: function() {
+            notebook_cell_div.css({'height': ''});
+            enable(source_button);
+            enable(result_button);
+            disable(hide_button);
+            enable(remove_button);
+
+            editor_row.hide();
+            if (current_mode === "result") {
+                r_result_div.slideUp(150); // hide();
+            } else {
+                markdown_div.slideUp(150); // hide();
+            }
+        },
+        remove_self: function() {
+            cell_model.parent_model.remove_cell(cell_model);            
+            notebook_cell_div.remove();
+        },
+        div: function() {
+            return notebook_cell_div;
+        },
+        update_model: function() {
+            update_model();
+        },
+        focus: function() {
+            input.focus();
+        }
+    };
+
+    result.show_result();
+    result.content_updated();
+    return result;
+}
+
+var dispatch = {
+    markdown: create_markdown_cell_html_view,
+    interactive: create_interactive_cell_html_view
+};
+
+Notebook.Cell.create_html_view = function(cell_model)
+{
+    return dispatch[cell_model.type()](cell_model);
+};
+
+})();
