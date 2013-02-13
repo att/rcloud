e49348c2288b85121ef135d6ed513a8d28c546bd
Simon Urbanek <su@ix>
2013-02-13 16:38:10 -0500

    add build script

---
+++ scripts/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# build JS and rcloud.support
+make -C htdocs/js && R CMD build rcloud.support && R CMD INSTALL rcloud.support_`sed -n 's/Version: *//p' rcloud.support/DESCRIPTION`.tar.gz
+
+# build internal packages (not in git)
+if [ -e internal ]; then
+    for pkg in `ls internal/*/DESCRIPTION | sed -e 's:internal/::' -e 's:/DESCRIPTION::'`; do
+	(cd internal && R CMD build $pkg && R CMD INSTALL ${pkg}_`sed -n 's/Version: *//p' $pkg/DESCRIPTION`.tar.gz)
+    done
+fi
