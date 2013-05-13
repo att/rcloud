f4a07ded883949589cdc128b35cd905bf7440d15
Simon Urbanek <urbanek@research.att.com>
2013-05-13 11:00:19 -0400

    add proper RCloud bootstrap and auto-installation

--- conf/run_rcloud.R
+++ conf/run_rcloud.R
@@ -1,6 +1,14 @@
-args <- commandArgs(trailingOnly=TRUE)
-library(Rserve)
-print(args)
+## check that RCloud is properly installed
+##installed <- gsub(".*/([^/]+)/DESCRIPTION$","\\1",Sys.glob(paste0(.libPaths(),"/*/DESCRIPTION")))
+installed <- gsub(".*/([^/]+)/DESCRIPTION$","\\1",Sys.glob(paste0(.libPaths(),"/rcloud.support/DESCRIPTION")))
 
-debug=FALSE
-Rserve(debug, args=c("--RS-conf", args[1], "--RS-source", args[2], "--vanilla", "--no-save"))
+if ("rcloud.support" %in% installed && is.function(try(rcloud.support:::check.installation, silent=TRUE))) rcloud.support:::check.installation() else {
+  cat("\n***** RCloud is not properly installed, attempting to install ... *****\n\n")
+  install.packages("rcloud.support",,c("http://rforge.net","http://r.research.att.com"),type='source')
+  rcloud.support:::check.installation()
+}
+
+args <- commandArgs(trailingOnly=TRUE)
+
+debug <- isTRUE(nzchar(Sys.getenv("DEBUG")))
+Rserve::Rserve(debug, args=c("--RS-conf", args[1], "--RS-source", args[2], "--vanilla", "--no-save"))
