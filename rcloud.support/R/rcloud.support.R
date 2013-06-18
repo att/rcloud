95a799f8381dcd22aaac0610025c436f120c48d6
Simon Urbanek <su@ix>
2013-06-18 16:28:18 -0400

    add cookie.domain rcloud configuration used for setting cookies. defaults to host

--- rcloud.support/R/rcloud.support.R
+++ rcloud.support/R/rcloud.support.R
@@ -132,7 +132,7 @@ configure.rcloud <- function () {
   setwd(.rc.conf$tmp.dir)
 
   ## if you have multiple servers it's good to know which machine this is
-  .rc.conf$host <- tolower(system("hostname -s", TRUE))
+  .rc.conf$host <- tolower(system("hostname -f", TRUE))
   cat("Starting Rserve on", .rc.conf$host,"\n")
 
   ## This is jsut a friendly way to load package and report success/failure
@@ -180,6 +180,9 @@ configure.rcloud <- function () {
     for (n in names(rc.c)) .rc.conf[[gsub("[ \t]", ".", tolower(n))]] <- as.vector(rc.c[n])
   }
 
+  if (is.null(.rc.conf$cookie.domain)) .rc.conf$cookie.domain <- .rc.conf$host
+  if (!isTRUE(grepl("[.:]", .rc.conf$cookie.domain))) stop("*** ERROR: cookie.domain must be a FQDN! Please set your hostname correctly or add cookie.domain directive to rcloud.conf")
+  
   rcloud.setup.dirs()
 
   .rc.conf$instanceID <- generate.uuid()
