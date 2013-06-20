7528065b5ad939780727d7131dbc75b9b31ace11
Simon Urbanek <su@ix>
2013-06-19 21:22:13 -0400

    update generated js files

--- htdocs/js/rcloud_bundle.js
+++ htdocs/js/rcloud_bundle.js
@@ -658,8 +658,7 @@ LuxChart.lux_osm_plot = function(lats, lons, color, width, height)
 
 // takes a string and returns the appropriate r literal string with escapes.
 function escape_r_literal_string(s) {
-    return "\"" + s.replace(/\\/g, "\\\\").replace(/"/g, "\\\"") + "\"";
-    // return "\"" + s.replace(/"/g, "\\\"") + "\"";
+    return (s == null) ? "NULL" : ("\"" + s.replace(/\\/g, "\\\\").replace(/"/g, "\\\"") + "\"");
 }
 
 function NoCallbackError() {
@@ -703,13 +702,14 @@ RClient = {
             shutdown();
         };
 
-        var token = $.cookies.get().token;
+        var token = $.cookies.get().token;  // document access token
+        var execToken = $.cookies.get().execToken; // execution token (if enabled)
         var rserve = Rserve.create({
             host: opts.host,
             on_connect: on_connect,
             on_error: on_error,
             on_close: on_close,
-            login: token + "\n" + token
+            login: token + "\n" + execToken
         });
 
         var _debug = opts.debug || false;
