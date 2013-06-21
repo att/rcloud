12423c2c8186a15e34b7ca94f55572f8f8f82617
Simon Urbanek <su@ix>
2013-06-21 09:49:59 -0400

    add rcloud.debug.level() -- integer, can be set via DEBUG env var or Debug: config; clean up debugging output; add revoke.token(); add home/<user>/library to .libPaths()

--- rcloud.support/R/credentials.R
+++ rcloud.support/R/credentials.R
@@ -2,31 +2,50 @@
 .get.token.list <- function()
 {
   rcloud.auth.path <- paste(rcloud.support:::.rc.conf$configuration.root, "/rcloud.auth", sep="")
-  d <- NULL
-  tryCatch(d <- readRDS(rcloud.auth.path),
+  tryCatch(readRDS(rcloud.auth.path),
            error=function(e) {
-             d <<- new.env(parent=emptyenv())
-             d$user.to.token <<- new.env(parent=emptyenv())
-             d$token.to.user <<- new.env(parent=emptyenv())
+             d <- new.env(parent=emptyenv())
+             d$user.to.token <- new.env(parent=emptyenv())
+             d$token.to.user <- new.env(parent=emptyenv())
+             d
            })
-  d
+}
+
+.save.token.list <- function(d) {
+  rcloud.auth.path <- paste(rcloud.support:::.rc.conf$configuration.root, "/rcloud.auth", sep="")
+  ## save + move to ensure that at least the content will be consitent
+  ## it is still unsafe because there is no lock between read and save, so
+  ## concurrent changes will be lost -- but that's why this is jsut
+  ## (discouraged) fallback -- admins are strongly encouraged to use
+  ## the session sever instead.
+  saveRDS(d, tmpfn <- paste0(rcloud.auth.path, Sys.getpid()))
+  file.rename(tmpfn, rcloud.auth.path)
 }
 
 set.token <- function(user, token, realm="rcloud")
 {
   if (is.null(rcloud.support:::.rc.conf$session.server)) {
-    rcloud.auth.path <- paste(rcloud.support:::.rc.conf$configuration.root, "/rcloud.auth", sep="")
     d <- .get.token.list()
     old.token <- d$user.to.token[[user]]
-    if (!is.null(old.token)) {
-      d$token.to.user[[old.token]] <- NULL
-    }
+    if (!is.null(old.token)) d$token.to.user[[old.token]] <- NULL
     d$user.to.token[[user]] <- token
     d$token.to.user[[token]] <- user
-    saveRDS(d, rcloud.auth.path)
+    .save.token.list(d)
   } else {
     RCurl::getURL(paste0(rcloud.support:::.rc.conf$session.server, "/stored_token?token=", URLencode(token), "&user=", URLencode(user), "&realm=", URLencode(realm)))
   }    
+}
+
+revoke.token <- function(token, realm="rcloud") {
+  if (is.null(rcloud.support:::.rc.conf$session.server)) {
+    d <- .get.token.list()
+    user <- d$token.to.user[[token]]
+    if (!is.null(user)) d$user.to.token[[user]] <- NULL
+    d$token.to.user[[token]] <- NULL
+    .save.token.list(d)
+  } else {
+    RCurl::getURL(paste0(rcloud.support:::.rc.conf$session.server, "/revoke?token=", URLencode(token), "&realm=", URLencode(realm)))
+  }
 }
 
 check.user.token.pair <- function(user, token, valid.sources="stored", realm="rcloud")
@@ -35,11 +54,7 @@ check.user.token.pair <- function(user, token, valid.sources="stored", realm="rc
     d <- .get.token.list()
     token.from.user <- d$user.to.token[[user]]
     user.from.token <- d$token.to.user[[token]]
-    cat("check.user.token.pair\n");
-    print(user);
-    print(token);
-    print(user.from.token);
-    print(token.from.user);
+    if (rcloud.debug.level()) cat("check.user.token.pair(", user, ", ", token, ")\n", sep='')
 
     (!is.null(user.from.token) &&
      (user.from.token == user) &&
@@ -48,7 +63,7 @@ check.user.token.pair <- function(user, token, valid.sources="stored", realm="rc
   } else {
     res <- RCurl::getURL(paste0(rcloud.support:::.rc.conf$session.server, "/valid?token=", URLencode(token), "&realm=", URLencode(realm)))
     res <- strsplit(res, "\n")[[1]]
-    cat("check.user.token.pair(", user, ", ", token, ", ", realm, ") valid: ", res[1],", user: ", res[2], ", source: ", res[3], "\n", sep='')
+    if (rcloud.debug.level()) cat("check.user.token.pair(", user, ", ", token, ", ", realm, ") valid: ", res[1],", user: ", res[2], ", source: ", res[3], "\n", sep='')
     (length(res) > 1) && isTRUE(res[1] == "YES") && isTRUE(res[2] == as.vector(user)) && isTRUE(res[3] %in% valid.sources)
   }
 }
@@ -65,7 +80,7 @@ check.token <- function(token, valid.sources="stored", realm="rcloud")
   } else {
     res <- RCurl::getURL(paste0(rcloud.support:::.rc.conf$session.server, "/valid?token=", URLencode(token), "&realm=", URLencode(realm)))
     res <- strsplit(res, "\n")[[1]]
-    cat("check.token(", token,", ", realm,") valid: ", res[1],", user: ", res[2], ", source: ", res[3], "\n", sep='')
+    if (rcloud.debug.level()) cat("check.token(", token,", ", realm,") valid: ", res[1],", user: ", res[2], ", source: ", res[3], "\n", sep='')
     if ((length(res) > 1) && isTRUE(res[1] == "YES") && isTRUE(res[3] %in% valid.sources)) res[2] else FALSE    
   }
 }
--- rcloud.support/R/rcloud.support.R
+++ rcloud.support/R/rcloud.support.R
@@ -36,7 +36,7 @@ rcloud.load.multiple.user.configs <- function(users) {
 }
 
 rcloud.save.user.config <- function(user, content) {
-  print("save it")
+  if (rcloud.debug.level()) cat("rcloud.save.user.config(", user, ")\n", sep='')
   filename <- rcloud.user.config.filename(user)
   invisible(write(content,filename))
 }
@@ -47,9 +47,11 @@ rcloud.get.notebook <- function(id) {
   # just a workaround because rserve.js doesn't support raw vectors yet
   # and i'm not sure if/how to deal with binary json either
   json <- rawToChar(res$content);
-  print("GOT GIST:")
-  print(json)
-  print("END GIST!")
+  if (rcloud.debug.level() > 1L) {
+    cat("==== GOT GIST ====\n")
+    cat(json)
+    cat("==== END GIST ====\n")
+  }
   content(res, as = 'text')
 }
 
@@ -64,7 +66,7 @@ rcloud.create.notebook <- function(content) {
 }
 
 rcloud.setup.dirs <- function() {
-    for (data.subdir in c("userfiles", "history"))
+    for (data.subdir in c("userfiles", "history", "home"))
         if (!file.exists(fn <- file.path(.rc.conf$data.root, data.subdir)))
              dir.create(fn, FALSE, TRUE, "0770")
 }
@@ -92,6 +94,8 @@ rcloud.record.cell.execution <- function(user, json.string) {
 rcloud.get.users <- function()
   content(get.users(.session$rgithub.context))
 
+rcloud.debug.level <- function() if (is.null(.rc.conf$debug)) 0L else .rc.conf$debug
+
 ################################################################################
 # setup the r-side environment
 # this is called once in the main Rserve instance, so it should do and load
@@ -104,6 +108,13 @@ configure.rcloud <- function () {
   if (is.null(.rc.conf$root) || nchar(.rc.conf$root) == 0) .rc.conf$root <- "/var/FastRWeb"
   cat("Using ROOT =", .rc.conf$root, "\n")
 
+  if (nzchar(Sys.getenv("DEBUG"))) {
+    dl <- as.integer(Sys.getenv("DEBUG"))
+    if (any(is.na(dl))) dl <- 1L
+    .rc.conf$debug <- dl
+    cat("=== NOTE: DEBUG is set, enabling debug mode at level", dl, "===\n")
+  }
+  
   # CONFROOT/DATAROOT are purely optional
   # Whom are we kidding? Although it may be nice to abstract out all paths
   # this is far from complete (what about htdocs?) and not very practical
@@ -183,6 +194,11 @@ configure.rcloud <- function () {
     for (n in names(rc.c)) .rc.conf[[gsub("[ \t]", ".", tolower(n))]] <- as.vector(rc.c[n])
   }
 
+  if (is.character(.rc.conf$debug)) {
+    .rc.conf$debug <- as.integer(.rc.conf$debug)
+    if (any(is.na(.rc.conf$debug))) .rc.conf$debug <- 1L
+  }
+
   if (is.null(.rc.conf$cookie.domain)) .rc.conf$cookie.domain <- .rc.conf$host
   if (!isTRUE(grepl("[.:]", .rc.conf$cookie.domain))) stop("*** ERROR: cookie.domain must be a FQDN! Please set your hostname correctly or add cookie.domain directive to rcloud.conf")
   
@@ -196,10 +212,7 @@ configure.rcloud <- function () {
 ## --- RCloud part folows ---
 ## this is called by session.init() on per-connection basis
 start.rcloud <- function(username="", token="", ...) {
-  cat("FOOOOOOOOOO\n")
-  print(username)
-  print(token)
-  cat("BAAARRRRRRR\n")
+  if (rcloud.debug.level()) cat("start.rcloud(", username, ", ", token, ")\n", sep='')
   if (!check.user.token.pair(username, token))
     stop("bad username/token pair");
   .session$username <- username
