dffc41855d835e28181d17c3a9a66c5016185ce0
Simon Urbanek <su@ix>
2013-07-02 17:36:21 -0400

    allow renamed packages to install correctly from internal (so rgithub works there, for example)

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,6 @@ make -C htdocs/js && R CMD build rcloud.support && R CMD INSTALL rcloud.support_
 # build internal packages (not in git)
 if [ -e internal ]; then
     for pkg in `ls internal/*/DESCRIPTION | sed -e 's:internal/::' -e 's:/DESCRIPTION::'`; do
-	(cd internal && R CMD build $pkg && R CMD INSTALL ${pkg}_`sed -n 's/Version: *//p' $pkg/DESCRIPTION`.tar.gz)
+	(cd internal && R CMD build $pkg && R CMD INSTALL `sed -n 's/Package: *//p' $pkg/DESCRIPTION`_`sed -n 's/Version: *//p' $pkg/DESCRIPTION`.tar.gz)
     done
 fi
