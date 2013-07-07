7149d81520a6637c93f3bc01ede92189bca388a4
Gordon Woodhull <gordon@woodhull.com>
2013-07-06 21:41:42 -0400

    a stab at row-major versus col-major dataframes er well at least a name for it

--- htdocs/lib/js/dcplot/dataframe.js
+++ htdocs/lib/js/dcplot/dataframe.js
@@ -2,29 +2,29 @@
  interface that should work for other data too (?) 
  note: test on array-of-records data!
  */
-
-function dataframe(frame) {
-    var result = {
-        accessor: function(column) { 
-            var columnv = frame[column];
-            return function(i) { 
-                return columnv[i];
-            };
-        },
-        // in particular this one may not work for array-of-records?
-        index_accessor: function() {
-            return function(i) {
-                return i; 
-            };
-        },
-        num_rows: function() {
-            for(var col in frame) 
-                return frame[col].length;
-        },
-        records: function() {
-            return _.range(0, this.num_rows());
-        }
-    };
-    return result;
-}
-
+var dataframe = {
+    cols: function(data) {
+        var result = {
+            accessor: function(column) { 
+                var columnv = data[column];
+                return function(i) { 
+                    return columnv[i];
+                };
+            },
+            // in particular this one may not work for array-of-records?
+            index_accessor: function() {
+                return function(i) {
+                    return i; 
+                };
+            },
+            num_rows: function() {
+                for(var col in data) 
+                    return data[col].length;
+            },
+            records: function() {
+                return _.range(0, this.num_rows());
+            }
+        };
+        return result;
+    }
+};
