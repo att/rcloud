1ef18a1b2a12ff15a59df32a157b249ed5d8fa3e
Carlos Scheidegger <cscheid@research.att.com>
2013-08-05 11:30:10 -0400

    command prompt on ACE editor. took more effort than I care to admit

--- htdocs/js/ui_utils.js
+++ htdocs/js/ui_utils.js
@@ -22,3 +22,43 @@ ui_utils.ace_editor_height = function(widget)
     return Math.max(75, newHeight);
 };
 
+// this is a hack, but it'll help giving people the right impression.
+// I'm happy to replace it witht the Right Way to do it when we learn
+// how to do it.
+ui_utils.make_prompt_chevron_gutter = function(widget)
+{
+    var dom = require("ace/lib/dom");
+    widget.renderer.$gutterLayer.update = function(config) {
+        var emptyAnno = {className: ""};
+        var html = [];
+        var i = config.firstRow;
+        var lastRow = config.lastRow;
+        var fold = this.session.getNextFoldLine(i);
+        var foldStart = fold ? fold.start.row : Infinity;
+        var foldWidgets = this.$showFoldWidgets && this.session.foldWidgets;
+        var breakpoints = this.session.$breakpoints;
+        var decorations = this.session.$decorations;
+        var firstLineNumber = this.session.$firstLineNumber;
+        var lastLineNumber = 0;
+        html.push(
+            "<div class='ace_gutter-cell ",
+            "' style='height:", this.session.getRowLength(0) * config.lineHeight, "px;'>", 
+            "&gt;", "</div>"
+        );
+
+        this.element = dom.setInnerHtml(this.element, html.join(""));
+        this.element.style.height = config.minHeight + "px";
+        
+        if (this.session.$useWrapMode)
+            lastLineNumber = this.session.getLength();
+        
+        var gutterWidth = ("" + lastLineNumber).length * config.characterWidth;
+        var padding = this.$padding || this.$computePadding();
+        gutterWidth += padding.left + padding.right;
+        if (gutterWidth !== this.gutterWidth && !isNaN(gutterWidth)) {
+            this.gutterWidth = gutterWidth;
+            this.element.style.width = Math.ceil(this.gutterWidth) + "px";
+            this._emit("changeGutterWidth", gutterWidth);
+        }
+    };
+};
