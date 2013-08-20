21a1d86a734783d65b97f30ee5a6d70c2b847799
Simon Urbanek <su@ix>
2013-08-20 09:31:03 -0400

    cleanup login/logout scripts

--- htdocs/login_successful.R
+++ htdocs/login_successful.R
@@ -1,20 +1,24 @@
+require(rcloud.support)
+require(httr)
+require(rjson)
+
 run <- function(url, query, body, headers)
 {
   state = fromJSON(URLdecode(query["state"]))
-  result <- POST(paste(rcloud.support:::.rc.conf$github.base.url, "login/oauth/access_token", sep=''),
+  result <- POST(paste(rcloud.config("github.base.url"), "login/oauth/access_token", sep=''),
                  config=accept_json(),
                  body=list(
-                   client_id=rcloud.support:::.rc.conf$github.client.id,
-                   client_secret=rcloud.support:::.rc.conf$github.client.secret,
+                   client_id=rcloud.config("github.client.id"),
+                   client_secret=rcloud.config("github.client.secret"),
                    code=query["code"]))
   if (rcloud.debug.level()) {
     cat("login_successful.R: github.access_token result:\n")
     print(content(result))
   }
   token <- content(result)$access_token
-  result <- rgithub.context.from.token(rcloud.support:::.rc.conf$github.api.url,
-                                    rcloud.support:::.rc.conf$github.client.id,
-                                    rcloud.support:::.rc.conf$github.client.secret,
+  result <- rgithub.context.from.token(rcloud.config("github.api.url"),
+                                    rcloud.config("github.client.id"),
+                                    rcloud.config("github.client.secret"),
                                     token)
   if(!result$ok)
     stop(paste("login failed: ", result$content$message))
@@ -23,10 +27,10 @@ run <- function(url, query, body, headers)
   if (is.null((ret = state$redirect)))
     ret = '/main.html'
   if (rcloud.debug.level()) cat("context: ", ctx$user$login, token, "\n")
-  set.token(ctx$user$login, token)
+  rcloud.support:::set.token(ctx$user$login, token)
   list(paste("<html><head></head><body>",
              "<pre>Welcome, ", ctx$user$login, ".</pre>",
              "</body></html>", sep=''),
        "text/html",
-       paste("Set-Cookie: user=", ctx$user$login, "; domain=", .rc.conf$cookie.domain,"; path=/;\r\nSet-Cookie: token=", token, "; domain=", .rc.conf$cookie.domain, "; path=/;\r\nRefresh: 0.1; url=", ret, sep=''))
+       paste("Set-Cookie: user=", ctx$user$login, "; domain=", rcloud.config("cookie.domain"), "; path=/;\r\nSet-Cookie: token=", token, "; domain=", rcloud.config("cookie.domain"), "; path=/;\r\nRefresh: 0.1; url=", ret, sep=''))
 }
