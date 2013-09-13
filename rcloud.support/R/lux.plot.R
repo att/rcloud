42ea3633fcc0b142755df903a1aab677c7235b50
Carlos Scheidegger <cscheid@research.att.com>
2013-09-13 10:56:03 -0600

    bringing wgeoplot up to date

--- rcloud.support/R/lux.plot.R
+++ rcloud.support/R/lux.plot.R
@@ -7,7 +7,7 @@ wgeoplot <- function(lats, lons, col=1L)
   if (is.null(dim(col))) col <- col2rgb(col) / 255
   #col <- rep(col, length.out = 3 * length(lats))
   col <- as.double(col)
-  deferred.rcloud.result(function() caps$handle_osm_plot("lux_osm_plot", lats, lons, col, c(960, 600)))
+  deferred.rcloud.result(function() caps$handle_osm_plot(list("lux_osm_plot", lats, lons, col, c(960, 600))))
 }
 
 wtour <- function(...)
