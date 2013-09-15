4cbb9584ef55d6b0c60fdb205f1c1d0518abef25
Gordon Woodhull <gordon@woodhull.com>
2013-09-15 05:23:22 -0400

    add autocomplete to input widget

--- htdocs/shell_tab.js
+++ htdocs/shell_tab.js
@@ -11,12 +11,15 @@ var shell = (function() {
             widget.resize();
         }
         entry_div.css({'background-color': "#E8F1FA"});
+        ace.require("ace/ext/language_tools");
         var widget = ace.edit(entry_div[0]);
         set_ace_height();
         var RMode = require("ace/mode/r").Mode;
         var session = widget.getSession();
         var doc = session.doc;
-
+        widget.setOptions({
+            enableBasicAutocompletion: true
+        });
         session.setMode(new RMode(false, doc, session));
         session.on('change', set_ace_height);
 
