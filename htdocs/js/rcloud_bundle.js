64a95b40e564cd53bbfae873fe1d07a6bcc05726
Gordon Woodhull <gordon@woodhull.com>
2013-11-09 08:12:18 -0500

    artifacts

--- htdocs/js/rcloud_bundle.js
+++ htdocs/js/rcloud_bundle.js
@@ -978,15 +978,28 @@ Notebook.create_model = function()
 {
     var readonly_ = false;
 
-    /* note, the code below is a little more sophisticated than it needs to be:
-       allows multiple inserts or removes but currently n is hardcoded as 1.  */
-
     function last_id(notebook) {
         if(notebook.length)
             return notebook[notebook.length-1].id;
         else
             return 0;
     }
+
+    function build_cell_change(id, content, language) {
+        // unfortunately, yet another workaround because github
+        // won't take blank files.  would prefer to make changes
+        // a high-level description but i don't see it yet.
+        var change = {id: id,
+                      language: language};
+        if(content === "")
+            change.erase = true;
+        else
+            change.content = content;
+        return change;
+    }
+
+    /* note, the code below is a little more sophisticated than it needs to be:
+       allows multiple inserts or removes but currently n is hardcoded as 1.  */
     return {
         notebook: [],
         views: [], // sub list for cell content pubsub
@@ -1001,7 +1014,7 @@ Notebook.create_model = function()
             id = id || 1;
             id = Math.max(id, last_id(this.notebook)+1);
             while(n) {
-                changes.push([id,{content: cell_model.content(), language: cell_model.language()}]);
+                changes.push({id: id, content: cell_model.content(), language: cell_model.language()});
                 cell_model.id = id;
                 this.notebook.push(cell_model);
                 _.each(this.views, function(view) {
@@ -1024,7 +1037,7 @@ Notebook.create_model = function()
                 id = Math.max(this.notebook[x].id-n, prev+1);
             }
             for(var j=0; j<n; ++j) {
-                changes.push([id+j, {content: cell_model.content(), language: cell_model.language()}]);
+                changes.push({id: id+j, content: cell_model.content(), language: cell_model.language()});
                 cell_model.id = id+j;
                 this.notebook.splice(x, 0, cell_model);
                 _.each(this.views, function(view) {
@@ -1040,9 +1053,10 @@ Notebook.create_model = function()
                 }
                 if(n<=0)
                     break;
-                changes.push([this.notebook[x].id,{content: this.notebook[x].content(),
-                                                   rename: this.notebook[x].id+n,
-                                                   language: this.notebook[x].language()}]);
+                changes.push({id: this.notebook[x].id,
+                              content: this.notebook[x].content(),
+                              rename: this.notebook[x].id+n,
+                              language: this.notebook[x].language()});
                 this.notebook[x].id += n;
                 ++x;
                 ++id;
@@ -1063,14 +1077,15 @@ Notebook.create_model = function()
                 cell_index = 0;
                 id = 1;
             }
-            var n = n || 1, x = cell_index;
+            n = n || 1;
+            var x = cell_index;
             var changes = [];
             while(x<this.notebook.length && n) {
                 if(this.notebook[x].id == id) {
                     _.each(this.views, function(view) {
                         view.cell_removed(that.notebook[x], x);
                     });
-                    changes.push([id, {erase: 1, language: that.notebook[x].language()} ]);
+                    changes.push({id: id, erase: 1, language: that.notebook[x].language()});
                     this.notebook.splice(x, 1);
                 }
                 ++id;
@@ -1079,8 +1094,7 @@ Notebook.create_model = function()
             return changes;
         },
         update_cell: function(cell_model) {
-            return [[cell_model.id, {content: cell_model.content(),
-                                     language: cell_model.language()}]];
+            return [build_cell_change(cell_model.id, cell_model.content(), cell_model.language())];
         },
         reread_cells: function() {
             var that = this;
@@ -1091,9 +1105,10 @@ Notebook.create_model = function()
                 throw "not expecting more than one notebook view";
             return _.reduce(changed_cells_per_view[0],
                             function(changes, content, index) {
-                                if(content)
-                                    changes.push([that.notebook[index].id, {content: content,
-                                                                            language: that.notebook[index].language()}]);
+                                if(content !== null)
+                                    changes.push(build_cell_change(that.notebook[index].id,
+                                                                   content,
+                                                                   that.notebook[index].language()));
                                 return changes;
                             },
                             []);
@@ -1131,14 +1146,14 @@ Notebook.create_controller = function(model)
         var cell_model = Notebook.Cell.create_model(content, type);
         var cell_controller = Notebook.Cell.create_controller(cell_model);
         cell_model.controller = cell_controller;
-        return [cell_controller, model.append_cell(cell_model, id)];
+        return {controller: cell_controller, changes: model.append_cell(cell_model, id)};
     }
 
     function insert_cell_helper(content, type, id) {
         var cell_model = Notebook.Cell.create_model(content, type);
         var cell_controller = Notebook.Cell.create_controller(cell_model);
         cell_model.controller = cell_controller;
-        return [cell_controller, model.insert_cell(cell_model, id)];
+        return {controller: cell_controller, changes: model.insert_cell(cell_model, id)};
     }
 
     function on_load(k, version, notebook) {
@@ -1161,6 +1176,8 @@ Notebook.create_controller = function(model)
         k && k(notebook);
     }
 
+    // calculate the changes needed to get back from the newest version in notebook
+    // back to what we are presently displaying (current_gist_)
     function find_changes_from(notebook) {
         var changes = [];
         var nf = notebook.files,
@@ -1170,16 +1187,20 @@ Notebook.create_controller = function(model)
                 continue; // R metadata
             if(f in cf) {
                 if(cf[f].language != nf[f].language || cf[f].content != nf[f].content) {
-                    changes.push([f, cf[f]]);
+                    changes.push({id: f,
+                                  language: cf[f].language,
+                                  content: cf[f].content});
                 }
                 delete cf[f];
             }
-            else changes.push([f, {erase: true, language: nf[f].language}]);
+            else changes.push({id: f, erase: true, language: nf[f].language});
         }
         for(f in cf) {
-            if(f==='r_type')
+            if(f==='r_type' || f==='r_attributes')
                 continue; // artifact of rserve.js
-            changes.push([f, cf[f]]);
+            changes.push({id: f,
+                          language: cf[f].language,
+                          content: cf[f].content});
         }
         return changes;
     }
@@ -1209,16 +1230,13 @@ Notebook.create_controller = function(model)
         },
         append_cell: function(content, type, id) {
             var cch = append_cell_helper(content, type, id);
-            // github gist api will not take empty cells, so drop them
-            if(content.length)
-                this.update_notebook(cch[1]);
-            return cch[0];
+            this.update_notebook(cch.changes);
+            return cch.controller;
         },
         insert_cell: function(content, type, id) {
             var cch = insert_cell_helper(content, type, id);
-            if(content.length)
-                this.update_notebook(cch[1]);
-            return cch[0];
+            this.update_notebook(cch.changes);
+            return cch.controller;
         },
         remove_cell: function(cell_model) {
             var changes = model.remove_cell(cell_model);
@@ -1268,6 +1286,10 @@ Notebook.create_controller = function(model)
                 });
         },
         update_notebook: function(changes, gistname, k) {
+            // remove any "empty" changes.  we can keep empty cells on the
+            // screen but github will refuse them.  if the user doesn't enter
+            // stuff in them before saving, they will disappear on next session
+            changes = changes.filter(function(change) { return !!change.content || change.erase; });
             if(!changes.length)
                 return;
             if(model.read_only())
@@ -1296,22 +1318,23 @@ Notebook.create_controller = function(model)
                 // instead, create y and if there is no longer any x, erase it
                 var post_names = _.reduce(changes,
                                          function(names, change) {
-                                             if(!change[1].erase) {
-                                                 var after = change[1].rename || change[0];
-                                                 names[partname(after, change[1].language)] = 1;
+                                             if(!change.erase) {
+                                                 var after = change.rename || change.id;
+                                                 names[partname(after, change.language)] = 1;
                                              }
                                              return names;
                                          }, {});
                 function xlate_change(filehash, change) {
                     var c = {};
-                    if(change[1].content !== undefined)
-                        c.content = change[1].content;
-                    var pre_name = partname(change[0], change[1].language);
-                    if(change[1].erase || !post_names[pre_name])
+                    if(change.content !== undefined)
+                        c.content = change.content;
+                    var pre_name = partname(change.id, change.language);
+                    if(change.erase || !post_names[pre_name])
                         filehash[pre_name] = null;
-                    var post_name = partname(change[1].rename || change[0], change[1].language);
-                    if(!change[1].erase)
+                    if(!change.erase) {
+                        var post_name = partname(change.rename || change.id, change.language);
                         filehash[post_name] = c;
+                    }
                     return filehash;
                 }
                 return {files: _.reduce(changes, xlate_change, {})};
