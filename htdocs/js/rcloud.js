8f7dc0b0bd9a732da16eac6c3252700aa4155ec9
Carlos Scheidegger <cscheid@research.att.com>
2013-11-13 13:42:19 -0500

    use rcloud.prevent_progress_modal and rcloud.allow_progress_modal to control appearance of progress modal

--- htdocs/js/rcloud.js
+++ htdocs/js/rcloud.js
@@ -268,15 +268,18 @@ RCloud.create = function(rcloud_ocaps) {
     if (rcloud.authenticated)
         setup_authenticated_ocaps();
 
+    //////////////////////////////////////////////////////////////////////////
+    // Progress indication
+
     // FIXME this doesn't feel like it belongs on rcloud, but then again,
     // where would it?
-    // 
-    // Progress indication
+
     var progress_dialog;
     var progress_counter = 0;
-
+    var allowed = 1;
     var curtains_on = false;
-    function show_progress_curtain() {
+
+    function set_curtain() {
         if (curtains_on)
             return;
         curtains_on = true;
@@ -286,31 +289,57 @@ RCloud.create = function(rcloud_ocaps) {
         }
         progress_dialog.modal({keyboard: true});
     }
-    function hide_progress_curtain() {
+    function clear_curtain() {
         if (!curtains_on)
             return;
         curtains_on = false;
         progress_dialog.modal('hide');
     }
-    rcloud.with_progress = function(thunk, delay) {
-        if (_.isUndefined(delay))
-            delay = 2000;
+    function set_cursor() {
         _.delay(function() {
             document.body.style.cursor = "wait";
         }, 0);
+    }
+    function clear_cursor() {
+        _.delay(function() {
+            document.body.style.cursor = '';
+        }, 0);
+    }
+    rcloud.with_progress = function(thunk, delay) {
+        if (_.isUndefined(delay))
+            delay = 2000;
+        set_cursor();
         function done() {
             progress_counter -= 1;
             if (progress_counter === 0) {
-                document.body.style.cursor = '';
-                hide_progress_curtain();
+                clear_cursor();
+                clear_curtain();
             }
         }
         _.delay(function() {
-            if (progress_counter > 0)
-                show_progress_curtain();
+            if (progress_counter > 0 && allowed > 0)
+                set_curtain();
         }, delay);
         progress_counter += 1;
         thunk(done);
+    };
+    rcloud.prevent_progress_modal = function() {
+        if (allowed === 1) {
+            if (progress_counter > 0) {
+                clear_cursor();
+                clear_curtain();
+            }
+        }
+        allowed -= 1;
+    };
+    rcloud.allow_progress_modal = function() {
+        if (allowed === 0) {
+            if (progress_counter > 0) {
+                set_cursor();
+                set_curtain();
+            }
+        }
+        allowed += 1;
     };
 
     return rcloud;
