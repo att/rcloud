82c1e910b6332421fdcdf97d66afc695ec89f741
Gordon Woodhull <gordon@woodhull.com>
2013-11-14 17:29:31 -0500

    quibble

--- htdocs/editor_tab.js
+++ htdocs/editor_tab.js
@@ -449,8 +449,8 @@ var editor = function () {
                 that.new_notebook();
             });
             publish_notebook_checkbox_ = ui_utils.checkbox_menu_item($("#publish-notebook"),
-               function() { rcloud.publish_notebook(shell.gistname()); },
-               function() { rcloud.unpublish_notebook(shell.gistname()); });
+               function() { rcloud.publish_notebook(config_.currbook); },
+               function() { rcloud.unpublish_notebook(config_.currbook); });
         },
         create_book_tree_widget: function(data) {
             var that = this;
