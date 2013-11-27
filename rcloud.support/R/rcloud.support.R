eb21b2459d1c1b5f181e76b5d74f2f737e4bf9b0
Simon Urbanek <su@ix>
2013-11-27 11:15:41 -0500

    clean up stashing and add support for version='all' that stashes all history

--- rcloud.support/R/rcloud.support.R
+++ rcloud.support/R/rcloud.support.R
@@ -56,25 +56,36 @@ rcloud.load.notebook <- function(id, version = NULL) {
   res
 }
 
+stash.key <- function(stash, notebook, version, type="gist")
+  usr.key(paste(version, type, sep='.'), user=paste0(".stash.", gsub("[-/]","_", stash)), notebook=notebook)
+
 ## stash the notebook into RCS such that it can ge used in non-github execution
 ## environment
-rcloud.stash.notebook <- function(stash, id = .session$current.notebook$content$id, version = NULL) {
+## version can be either a version hash, NULL to stash only the last commit
+## or "all" to store all versions
+## Note: HEAD tag is used at retrieval time to get the version if version=NULL so it
+## should be set if you want this stash to be the default version (unless changed
+## this will happen for NULL and "all" versions).
+rcloud.stash.notebook <- function(stash, id = .session$current.notebook$content$id, version = NULL, tag = if (is.null(version)) "HEAD" else NULL) {
+  get.all <- isTRUE(version == "all")
+  if (get.all) version <- NULL
   res <- rcloud.get.notebook(id, version)
   if (res$ok) {
     tag <- NULL
-    ## FIXME: maybe we should set HEAD even if the version is specified such
-    ##        that a get without version succeeds even if only a
-    ##        specific version is stashed
-    ##        But that will require us to fetch the current HEAD
-    ##        entry and compare commit timestamps or some such nonsense ...
-    if (is.null(version)) {
-      version <- res$content$history[[1L]]$version
-      tag <- "HEAD"
+    if (get.all) { ## get all history?
+      versions <- sapply(res$content$history, function(x) x$version)[-1L]
+      for (ver in versions) {
+        r <- rcloud.get.notebook(id, ver)
+        rcs.set(stash.key(stash, id, ver), r)
+      }
     }
-    stu <- paste0(".stash.", gsub("[-/]","_", stash))
-    rcs.set(usr.key(paste0(version, ".gist"), user=stu, notebook=id), res)
+    if (is.null(version)) {
+      force(tag) ## force tag before we mangle the version
+      version <- res$content$history[[1L]]$version
+    }
+    rcs.set(stash.key(stash, id, version), res)
     if (!is.null(tag))
-      rcs.set(usr.key(paste0(tag, ".tag"), user=stu, notebook=id), version)
+      rcs.set(stash.key(stash, id, tag, type="tag"), version)
     TRUE
   } else FALSE
 }
@@ -94,14 +105,15 @@ rcloud.unauthenticated.get.notebook <- function(id, version = NULL) {
 }
 
 rcloud.get.notebook <- function(id, version = NULL) {
-  res <- if (!is.null(.session$deployment.stash)) {
-    stu <- paste0(".stash.",gsub("[-/]","_",.session$deployment.stash))
+  res <- if (!is.null(stash <- .session$deployment.stash)) {
     if (is.null(version))
-      version <- rcs.get(usr.key("HEAD.tag", user=stu, notebook=id))
-    res <- rcs.get(usr.key(paste0(version, ".gist"), user=stu, notebook=id))
-    if (is.null(res$ok)) res$ok <- FALSE
+      version <- rcs.get(stash.key(stash, id, "HEAD", type="tag"))
+    res <- rcs.get(stash.key(stash, id, version))
+    if (is.null(res$ok)) res <- list(ok=FALSE)
     res
-  } else get.gist(id, version, ctx = .session$rgithub.context)
+  } else suppressWarnings(get.gist(id, version, ctx = .session$rgithub.context))
+  ## FIXME: suppressWarnings is a hack to get rid of the stupid "Duplicated curl options"
+  ##        which seem to be a httr bug
   if (rcloud.debug.level() > 1L) {
     if(res$ok) {
       cat("==== GOT GIST ====\n")
