e30f91a40a19b9d49eab90a5cf37cfe911775b1a
Simon Urbanek <su@ix>
2013-12-20 15:18:14 -0500

    fix rcloud.get.notebook.file() notebook default

--- rcloud.support/R/tools.R
+++ rcloud.support/R/tools.R
@@ -1,4 +1,4 @@
-rcloud.get.notebook.file <- function(file, notebook=.session$current.notebook, version=NULL) {  
+rcloud.get.notebook.file <- function(file, notebook=.session$current.notebook$content$id, version=NULL) {  
     res <- rcloud.get.notebook(notebook, version)  
     if (!isTRUE(res$ok)) stop("unable to get notebook `", notebook,"'")  
     res <- res$content$files[[file]]$content  
