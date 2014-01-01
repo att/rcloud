21dc214bb60b1be9b6c7d123cc14bf8e510b9dfb
Simon Urbanek <su@ix>
2013-12-31 22:23:01 -0500

    minor tweaks to error messages in notebook.R

--- htdocs/notebook.R
+++ htdocs/notebook.R
@@ -27,13 +27,14 @@ run <- function(url, query, body, headers)
     ## authenticate
     caps <- RSclient::RS.eval.qap(c, as.call(list(oc.init, c(cookies$token, cookies$execToken))))
     ## session init
+    if (!is.list(caps)) return(list(paste0(et, "<p><!-- error: ", as.character(caps)[1], " -->"), "text/html"))
     anonymous <- FALSE
     init.cap <- caps$rcloud$session_init
     if (is.null(init.cap)) {
       init.cap <- caps$rcloud$anonymous_session_init
       anonymous <- TRUE
     }
-    if (is.null(init.cap)) stop("Server refused to provide RCloud session initialization capabilites")
+    if (is.null(init.cap)) stop("Server refused to provide RCloud session initialization capabilites - access denied")
     RSclient::RS.eval.qap(c, as.call(list(init.cap, cookies$user, cookies$token)))
     et <- "Error fetching content:"
     version <- NULL
