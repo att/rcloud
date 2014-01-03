3edf78c4a82e59f436a27523cbf1f42f9aeb197c
Gordon Woodhull <gordon@woodhull.com>
2014-01-03 03:16:46 -0500

    retire dcrchart

--- rcloud.support/R/wdcchart.R
+++ rcloud.support/R/wdcchart.R
@@ -1,13 +1,3 @@
-
-wdcchart <- function(data, dcexpr)
-{
-  path <- system.file("javascript", "dc_chart.js", package="rcloud.support");
-  caps <- rcloud.install.js.module("dc_chart",
-                                   paste(readLines(path), collapse='\n'))
-  dcexpr2 <- substitute(dcexpr)
-  deferred.rcloud.result(function() caps$handle_dcchart(list("dcchart", data, dcexpr2)))
-}
-
 wdcplot <- function(data, dims=NULL, groups=NULL, charts=NULL)
 {
 
