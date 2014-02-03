d2db4df3da7484c091c0aa7f4673b4dff6418153
SMS Chauhan <smschauhan@gmail.com>
2014-02-03 00:23:44 -0500

    Package repo in rcloud/packages and bootstrapR

--- scripts/mkdist.sh
+++ scripts/mkdist.sh
@@ -8,7 +8,7 @@ DST="$SRC/dist"
 ## - others are not on CRAN
 RFORGE_PKG=Rserve,github,unixtools,rediscc,FastRWeb
 
-## distribution list 
+## distribution list
 dist_files="LICENSE \
 NEWS.md \
 README.md \
@@ -17,6 +17,7 @@ dcchart.md \
 doc \
 htdocs \
 rcloud.support \
+bootstrapR.sh \
 scripts"
 
 
@@ -44,7 +45,7 @@ mkdir "$DST"
 if [ ! -d "$DST" ]; then echo "ERROR: cannot create $DST"; exit 1; fi
 ## need abs path
 ADST=`cd "$DST"; pwd`
-mkdir -p "$DST/pkg.repos/src/contrib"
+mkdir -p "$DST/rcloud/packages/src/contrib"
 mkdir "$DST/tmp"
 
 REV=`( cd "$SRC" && git rev-list --abbrev-commit -n 1 HEAD )`
@@ -58,23 +59,22 @@ if [ -z "$rcs" ]; then
     echo "ERROR: unable to build rcloud.support package"
     exit 1
 fi
-mv "$rcs" "$DST/pkg.repos/src/contrib"
-echo "tools:::write_PACKAGES('$DST/pkg.repos/src/contrib')" | R --vanilla --slave
+mv "$rcs" "$DST/rcloud/packages/src/contrib"
+echo "tools:::write_PACKAGES('$DST/rcloud/packages/src/contrib')" | R --vanilla --slave
 
 echo '' && echo "=== downloading dependencies ===" && echo ''
 
-echo "options(warn=2);cran=available.packages(contrib.url('http://r.research.att.com/',type='source'),type='source');rf=available.packages(contrib.url('http://r.research.att.com/',type='source'),type='source');local=available.packages(contrib.url('file://$DST/pkg.repos',type='source'),type='source');rf.pkg=strsplit('$RFORGE_PKG',',',T)[[1]];stage1=unique(unlist(tools:::package_dependencies('rcloud.support',local,'all')));print(stage1);stage2=unique(c(stage1,unlist(tools:::package_dependencies(stage1,rbind(cran,rf,local),,TRUE))));rec=rownames(installed.packages(,'high'));stage2=stage2[!(stage2 %in% rec)];print(stage2);download.packages(rf.pkg,'$DST/pkg.repos/src/contrib',,'http://rforge.net',type='source');cran.pkg=stage2[!(stage2 %in% c('rcloud.support',rf.pkg))];download.packages(cran.pkg,'$DST/pkg.repos/src/contrib',,'http://r.research.att.com',type='source');writeLines(stage2,'$DST/pkg.repos/pkg.list')" | R --vanilla --slave
+echo "options(warn=2);cran=available.packages(contrib.url('http://r.research.att.com/',type='source'),type='source');rf=available.packages(contrib.url('http://r.research.att.com/',type='source'),type='source');local=available.packages(contrib.url('file://$DST/rcloud/packages',type='source'),type='source');rf.pkg=strsplit('$RFORGE_PKG',',',T)[[1]];stage1=unique(unlist(tools:::package_dependencies('rcloud.support',local,'all')));print(stage1);stage2=unique(c(stage1,unlist(tools:::package_dependencies(stage1,rbind(cran,rf,local),,TRUE))));rec=rownames(installed.packages(,'high'));stage2=stage2[!(stage2 %in% rec)];print(stage2);download.packages(rf.pkg,'$DST/rcloud/packages/src/contrib',,'http://rforge.net',type='source');cran.pkg=stage2[!(stage2 %in% c('rcloud.support',rf.pkg))];download.packages(cran.pkg,'$DST/rcloud/packages/src/contrib',,'http://r.research.att.com',type='source');writeLines(stage2,'$DST/rcloud/packages/pkg.list')" | R --vanilla --slave
 
-echo "tools:::write_PACKAGES('$DST/pkg.repos/src/contrib')" | R --vanilla --slave
+echo "tools:::write_PACKAGES('$DST/rcloud/packages/src/contrib')" | R --vanilla --slave
 
-if [ ! -e "$DST/pkg.repos/pkg.list" ]; then
+if [ ! -e "$DST/rcloud/packages/pkg.list" ]; then
     echo "ERROR: download failed"
     exit 1
 fi
 
 echo '' && echo "=== copying distributed files ===" && echo ''
 
-mkdir "$DST/rcloud"
 for i in $dist_files; do
     cp -pR "$SRC/$i" "$DST/rcloud/$i"
 done
@@ -84,7 +84,7 @@ echo $REV >> "$DST/rcloud/REVISION"
 
 rm -rf "$DST/tmp"
 
-( cd "$DST"; tar fcz rcloud-$BRANCH-$REV.tar.gz rcloud pkg.repos )
+( cd "$DST"; tar fcz rcloud-$BRANCH-$REV.tar.gz rcloud )
 
 echo '' && echo "=== done ===" && echo ''
 
