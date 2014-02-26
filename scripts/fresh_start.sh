fda1167d43288fb27922ba911a11fd2d0d3954f3
Gordon Woodhull <gordon@woodhull.com>
2014-02-26 15:35:06 -0500

    do not run RCloud when rcloud.support build fails

--- scripts/fresh_start.sh
+++ scripts/fresh_start.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-cd htdocs/js; 
+cd htdocs/js;
 if [ -d node_modules ]; then
     make || exit;
 else
     echo Skipping javascript build because node.js modules are not installed. Refer to Readme.md for details.
 fi
-cd ../lib; 
+cd ../lib;
 if [ -d node_modules ]; then
     make || exit;
 else
     echo Skipping javascript build because node.js modules are not installed. Refer to Readme.md for details.
 fi
 cd ../..
-R CMD build rcloud.support && R CMD INSTALL rcloud.support_`sed -n 's/Version: *//p' rcloud.support/DESCRIPTION`.tar.gz
+R CMD build rcloud.support && R CMD INSTALL rcloud.support_`sed -n 's/Version: *//p' rcloud.support/DESCRIPTION`.tar.gz || exit
 killall -9 Rserve
 #Kill Rserve debug instances as well
 killall -9 Rserve.dbg
