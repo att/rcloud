fe0ac1d964e86c3104d717e1145185bcbc93dc19
Carlos Scheidegger <cscheid@research.att.com>
2014-03-05 03:28:26 -0500

    gets notebook_runner from the package, and not a hardcoded path

--- rcloud.support/R/python.R
+++ rcloud.support/R/python.R
@@ -2,7 +2,7 @@ rcloud.start.python <- function()
 {
   require(rPython)
   python.exec.string("import sys")
-  python.exec.string("sys.path.append('/Users/cscheid/code/rcloud/python')")
+  python.exec(sys$path$append(.r(dirname(system.file("python", "notebook_runner.py", package="rcloud.support")))))
   python.exec.string("import notebook_runner")
   python.exec.string("runner = notebook_runner.NotebookRunner(extra_arguments=['--matplotlib=inline'], executable='python')")
   python.exec.string("def run(cmd): return runner.run_cmd(cmd)")
---
+++ rcloud.support/inst/python/notebook_runner.py
@@ -0,0 +1,158 @@
+try:
+    # python 2
+    from Queue import Empty
+except:
+    # python 3
+    from queue import Empty
+
+import platform
+from time import sleep
+import logging
+
+from IPython.nbformat.current import read, write, NotebookNode
+from IPython.kernel import KernelManager
+
+
+class NotebookError(Exception):
+    pass
+
+
+class NotebookRunner(object):
+    # The kernel communicates with mime-types while the notebook
+    # uses short labels for different cell types. We'll use this to
+    # map from kernel types to notebook format types.
+
+    MIME_MAP = {
+        'image/jpeg': 'jpeg',
+        'image/png': 'png',
+        'text/plain': 'text',
+        'text/html': 'html',
+        'text/latex': 'latex',
+        'application/javascript': 'html',
+    }
+
+    def __init__(self, **kw):
+        self.km = KernelManager()
+        self.km.start_kernel(**kw)
+
+        if platform.system() == 'Darwin':
+            # There is sometimes a race condition where the first
+            # execute command hits the kernel before it's ready.
+            # It appears to happen only on Darwin (Mac OS) and an
+            # easy (but clumsy) way to mitigate it is to sleep
+            # for a second.
+            sleep(1)
+
+        self.kc = self.km.client()
+        self.kc.start_channels()
+
+        self.shell = self.kc.shell_channel
+        self.iopub = self.kc.iopub_channel
+
+    def __del__(self):
+        self.kc.stop_channels()
+        self.km.shutdown_kernel(now=True)
+
+    def run_cmd(self, cmd):
+        d = {}
+        class Cell(object):
+            def __init__(self):
+                self.input = cmd
+            def __setitem__(self, k, v):
+                d[k] = v
+        self.run_cell(Cell())
+        return d['outputs']
+
+    def run_cell(self, cell):
+        '''
+        Run a notebook cell and update the output of that cell in-place.
+        '''
+        logging.info('Running cell:\n%s\n', cell.input)
+        self.shell.execute(cell.input)
+        reply = self.shell.get_msg()
+        status = reply['content']['status']
+        if status == 'error':
+            logging.info('Cell raised uncaught exception: \n%s', '\n'.join(reply['content']['traceback']))
+        else:
+            logging.info('Cell returned')
+
+        outs = list()
+        while True:
+            try:
+                msg = self.iopub.get_msg(timeout=1)
+                if msg['msg_type'] == 'status':
+                    if msg['content']['execution_state'] == 'idle':
+                        break
+            except Empty:
+                # execution state should return to idle before the queue becomes empty,
+                # if it doesn't, something bad has happened
+                raise
+
+            content = msg['content']
+            msg_type = msg['msg_type']
+
+            out = NotebookNode(output_type=msg_type)
+
+            if 'execution_count' in content:
+                cell['prompt_number'] = content['execution_count']
+                out.prompt_number = content['execution_count']
+
+            if msg_type in ['status', 'pyin']:
+                continue
+            elif msg_type == 'stream':
+                out.stream = content['name']
+                out.text = content['data']
+                #print(out.text, end='')
+            elif msg_type in ('display_data', 'pyout'):
+                for mime, data in content['data'].items():
+                    try:
+                        attr = self.MIME_MAP[mime]
+                    except KeyError:
+                        raise NotImplementedError('unhandled mime type: %s' % mime)
+
+                    setattr(out, attr, data)
+                #print(data, end='')
+            elif msg_type == 'pyerr':
+                out.ename = content['ename']
+                out.evalue = content['evalue']
+                out.traceback = content['traceback']
+
+                #logging.error('\n'.join(content['traceback']))
+            else:
+                raise NotImplementedError('unhandled iopub message: %s' % msg_type)
+            outs.append(out)
+        cell['outputs'] = outs
+
+        if status == 'error':
+            raise NotebookError()
+
+    def iter_code_cells(self, nb):
+        '''
+        Iterate over the notebook cells containing code.
+        '''
+        nb = read(open(nb), 'json')
+        for ws in nb.worksheets:
+            for cell in ws.cells:
+                if cell.cell_type == 'code':
+                    yield cell
+
+
+    def run_notebook(self, nb, skip_exceptions=False):
+        '''
+        Run all the cells of a notebook in order and update
+        the outputs in-place.
+
+        If ``skip_exceptions`` is set, then if exceptions occur in a cell, the
+        subsequent cells are run (by default, the notebook execution stops).
+        '''
+        for cell in self.iter_code_cells(nb):
+            try:
+                self.run_cell(cell)
+                print cell.outputs
+            except NotebookError:
+                if not skip_exceptions:
+                    raise
+
+    def save_notebook(self, nb_out):
+        logging.info('Saving to %s', nb_out)
+        write(self.nb, open(nb_out, 'w'), 'json')
