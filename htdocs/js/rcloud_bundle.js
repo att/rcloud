96ebbaec633db365e5b3d138bd0801b628eb9629
Gordon Woodhull <gordon@woodhull.com>
2014-03-11 08:51:35 -0400

    artifacts

--- htdocs/js/rcloud_bundle.js
+++ htdocs/js/rcloud_bundle.js
@@ -945,43 +945,105 @@ ui_utils.make_prompt_chevron_gutter = function(widget)
     };
 };
 
-ui_utils.make_editable = function(elem$, editable, on_edit) {
-    // http://stackoverflow.com/questions/6139107/programatically-select-text-in-a-contenteditable-html-element
-    function selectElementContents(el) {
-        var range = document.createRange();
-        range.selectNodeContents(el);
+// the existing jQuery editable libraries don't seem to do what we need, with
+// different active and inactive text, and customized selection.
+// this is a vague imitation of what a jquery.ui library might look like
+// except without putting it into $ namespace
+ui_utils.editable = function(elem$, command) {
+    function selectRange(range) {
         var sel = window.getSelection();
         sel.removeAllRanges();
         sel.addRange(range);
     }
-
-    if(elem$.attr('contenteditable') === (editable?'true':'false'))
-        return;
-
-    elem$.data('restore_edit', elem$.text());
-    function cancel() {
-        elem$.text(elem$.data('restore_edit'));
+    function options() {
+        return elem$.data('__editable');
     }
 
-    // remove all handlers, and then recreate them if the title is editable
-    elem$.off('keydown');
-    elem$.off('focus');
-    elem$.off('blur');
-    if (editable) {
+    var old_opts = options(),
+        new_opts = old_opts;
+    if(_.isObject(command)) {
+        var defaults = {on_change: function() { return true; },
+                        allow_edit: true,
+                        inactive_text: elem$.text(),
+                        active_text: elem$.text(),
+                        select: function(el) {
+                            var range = document.createRange();
+                            range.selectNodeContents(el);
+                            return range;
+                        }
+                       };
+        new_opts = $.extend(old_opts || defaults, command);
+        elem$.data('__editable', new_opts);
+    }
+    else {
+        if(command !== 'destroy' && !old_opts)
+            throw new Error('expected already editable for command ' + command);
+        function set_option(key, value) {
+            old_opts = $.extend({}, old_opts);
+            new_opts[key] = value;
+        }
+        switch(command) {
+        case 'destroy':
+            elem$.data('__editable', null);
+            new_opts = null;
+            break;
+        case 'option':
+            if(!arguments[2])
+                return old_opts;
+            else if(!arguments[3])
+                return old_opts[arguments[2]];
+            else {
+                set_option(arguments[2], arguments[3]);
+            }
+            break;
+        case 'disable':
+            set_option('allow_edit', false);
+            break;
+        case 'enable':
+            set_option('allow_edit', true);
+            break;
+        }
+    }
+    var action = null;
+    if((!old_opts || !old_opts.allow_edit) && (new_opts && new_opts.allow_edit))
+        action = 'melt';
+    else if((old_opts && old_opts.allow_edit) && (!new_opts || !new_opts.allow_edit))
+        action = 'freeze';
+
+    if(new_opts)
+        elem$.text(options().__active ? new_opts.active_text : new_opts.inactive_text);
+
+    switch(action) {
+    case 'freeze':
+        elem$.attr('contenteditable', 'false');
+        elem$.off('keydown');
+        elem$.off('focus');
+        elem$.off('blur');
+        break;
+    case 'melt':
         elem$.attr('contenteditable', 'true');
         elem$.focus(function() {
-            window.setTimeout(function() {
-                selectElementContents(elem$[0]);
-            }, 0);
-            elem$.off('blur');
-            elem$.blur(cancel); // click-off cancels
+            if(!options().__active) {
+                options().__active = true;
+                elem$.text(options().active_text);
+                window.setTimeout(function() {
+                    selectRange(options().select(elem$[0]));
+                    elem$.off('blur');
+                    elem$.blur(function() {
+                        elem$.text(options().inactive_text);
+                        options().__active = false;
+                    }); // click-off cancels
+                }, 10);
+            }
         });
         elem$.keydown(function(e) {
             if(e.keyCode === 13) {
                 var result = elem$.text();
-                if(on_edit(result)) {
+                if(options().validate(result)) {
+                    options().__active = false;
                     elem$.off('blur'); // don't cancel!
                     elem$.blur();
+                    options().change(result);
                 }
                 else return false; // don't let CR through!
             }
@@ -989,8 +1051,8 @@ ui_utils.make_editable = function(elem$, editable, on_edit) {
                 elem$.blur(); // and cancel
             return true;
         });
+        break;
     }
-    else elem$.attr('contenteditable', 'false');
 };
 
 ui_utils.on_next_tick = function(f) {
@@ -3089,53 +3151,70 @@ RCloud.UI.configure_readonly = function() {
         $('#output').sortable('enable');
     }
 };
-RCloud.UI.notebook_title = {
-    last_editable_: null,
-    set: function (text) {
-        var is_read_only = shell.notebook.model.read_only();
-        $("#notebook-title")
-            .text(text)
-            .data('restore_edit', text);
-        var ellipt_start = false, ellipt_end = false;
-        while(window.innerWidth - $("#notebook-title").width() < 505) {
-            var slash = text.search('/');
-            if(slash >= 0) {
-                ellipt_start = true;
-                text = text.slice(slash+1);
-            }
-            else {
-                ellipt_end = true;
-                text = text.substr(0, text.length - 2);
-            }
-            $("#notebook-title").text((ellipt_start ? '.../' : '')
-                                      + text +
-                                      (ellipt_end ? '...' : ''));
-        }
-        var title = $('#notebook-title');
-        ui_utils.make_editable(title, !is_read_only, function(text) {
-            if(editor.rename_notebook(shell.gistname(), text)) {
-                $("#notebook-title")
-                    .text(text)
-                    .data('restore_edit', text);
-                return true;
-            } else {
-                return false;
-            }
-        });
-    }, make_editable: function(node_title, gistname, editable) {
-        if(this.last_editable_ && (!node_title || this.last_editable_[0] !== node_title[0]))
-            ui_utils.make_editable(this.last_editable_, false);
-        if(node_title)
-            ui_utils.make_editable(node_title, editable, function(text) {
-                if(editor.rename_notebook(gistname, text)) {
-                    $("#notebook-title")
-                        .text(text)
-                        .data('restore_edit', text);
-                    return true;
-                } else {
-                    return false;
-                }
+RCloud.UI.notebook_title = (function() {
+    var last_editable_ =  null;
+    function rename_current_notebook(name) {
+        editor.rename_notebook(shell.gistname(), name)
+            .then(function() {
+                result.set(name);
             });
-        this.last_editable_ = node_title;
     }
-};
+    // always select all text after last slash, or all text
+    function select(el) {
+        if(el.childNodes.length !== 1 || el.firstChild.nodeType != el.TEXT_NODE)
+            throw new Error('expecting simple element with child text');
+        var text = el.firstChild.textContent;
+        var range = document.createRange();
+        range.setStart(el.firstChild, text.lastIndexOf('/') + 1);
+        range.setEnd(el.firstChild, text.length);
+        return range;
+    }
+    var editable_opts = {
+        change: rename_current_notebook,
+        select: select,
+        validate: function(name) { return editor.validate_name(name); }
+    };
+
+    var result = {
+        set: function (text) {
+            var is_read_only = shell.notebook.model.read_only();
+            var active_text = text;
+            var ellipt_start = false, ellipt_end = false;
+            var title = $('#notebook-title');
+            title.text(text);
+            while(window.innerWidth - title.width() < 505) {
+                var slash = text.search('/');
+                if(slash >= 0) {
+                    ellipt_start = true;
+                    text = text.slice(slash+1);
+                }
+                else {
+                    ellipt_end = true;
+                    text = text.substr(0, text.length - 2);
+                }
+                title.text((ellipt_start ? '.../' : '')
+                                          + text +
+                                          (ellipt_end ? '...' : ''));
+            }
+            ui_utils.editable(title, $.extend({allow_edit: !is_read_only,
+                                               inactive_text: title.text(),
+                                               active_text: active_text},
+                                              editable_opts));
+        }, make_editable: function(node, editable) {
+            function get_title(node) {
+                return $('.jqtree-title:not(.history)', node.element);
+            }
+            if(last_editable_ && (!node || last_editable_ !== node))
+                ui_utils.editable(get_title(last_editable_), 'destroy');
+            if(node) {
+                ui_utils.editable(get_title(node),
+                                  $.extend({allow_edit: editable,
+                                            inactive_text: node.name,
+                                            active_text: node.full_name},
+                                           editable_opts));
+            }
+            last_editable_ = node;
+        }
+    };
+    return result;
+})();
