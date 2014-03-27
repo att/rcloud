fb67660c4c7b02828e4fec6a136c9f4c1f477f73
Simon Urbanek <simon.urbanek@r-project.org>
2014-03-27 17:08:03 -0400

    fix uses of rcs.set() in the FF backend; closes #433

--- rcloud.support/R/rcs.ff.R
+++ rcloud.support/R/rcs.ff.R
@@ -28,7 +28,7 @@ rcs.set.RCSff <- function(key, value, counter=FALSE, engine=.session$rcs.engine)
     value <- as.integer(value)
   if (missing(value)) {
     if (!is.list(key) || is.null(names(key))) stop("Missing `value' and `key' is not a named vector")
-    for (i in seq.int(length(key))) rcs.set.RCSff(names(key)[i], key[[i]], engine)
+    for (i in seq.int(length(key))) rcs.set.RCSff(names(key)[i], key[[i]], engine=engine)
     key
   } else {
     tmp <- paste0(.ffpath(key, engine), "...tmp")
@@ -46,14 +46,14 @@ rcs.rm.RCSff <- function(key, engine=.session$rcs.engine)
 rcs.incr.RCSff <- function(key, engine=.session$rcs.engine) {
   x <- tryCatch(as.integer(rcs.get(key, engine=engine)), warning=rcs_ff_warning(0L), error=rcs_ff_error(0L))
   if (length(x) != 1L) x <- 0L
-  rcs.set(key, x + 1L, engine)
+  rcs.set(key, x + 1L, engine=engine)
 }
 
 rcs.decr.RCSff <- function(key, engine=.session$rcs.engine) {
   x <- tryCatch(as.integer(rcs.get(key, engine=engine)), warning=rcs_ff_warning(0L), error=rcs_ff_error(0L))
   if (length(x) != 1L) x <- 0L
   if (x < 1L) x <- 0L else x <- x - 1L
-  rcs.set(key, x, engine)
+  rcs.set(key, x, engine=engine)
 }
 
 rcs.list.RCSff <- function(pattern="*", engine=.session$rcs.engine) {
