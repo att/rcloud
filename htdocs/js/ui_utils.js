b5ea5c1660c12f939c9f515cd6bdfb28b0e49de0
Gordon Woodhull <gordon@woodhull.com>
2014-03-31 10:36:07 -0400

    use "screen length" to determine cell height
    fixes #409

--- htdocs/js/ui_utils.js
+++ htdocs/js/ui_utils.js
@@ -38,7 +38,7 @@ ui_utils.ace_editor_height = function(widget, min_rows, max_rows)
     min_rows = _.isUndefined(min_rows) ? 0  : min_rows;
     max_rows = _.isUndefined(max_rows) ? 30 : max_rows;
     var lineHeight = widget.renderer.lineHeight;
-    var rows = Math.max(min_rows, Math.min(max_rows, widget.getSession().getLength()));
+    var rows = Math.max(min_rows, Math.min(max_rows, widget.getSession().getScreenLength()));
     var newHeight = lineHeight*rows + widget.renderer.scrollBar.getWidth();
     return Math.max(75, newHeight);
     /*
