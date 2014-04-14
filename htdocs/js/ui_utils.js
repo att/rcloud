292fcf66c1f562e35538b18f7c8a1ff0fa19773f
Carlos Scheidegger <cscheid@research.att.com>
2014-04-14 14:25:43 -0400

    add a marker to the ace gutter so it gives better grab affordance. fixes #484, thanks @holyguster for suggestions

--- htdocs/js/ui_utils.js
+++ htdocs/js/ui_utils.js
@@ -370,3 +370,10 @@ ui_utils.editable = function(elem$, command) {
 ui_utils.on_next_tick = function(f) {
     window.setTimeout(f, 0);
 };
+
+ui_utils.add_ace_grab_affordance = function(element) {
+    debugger;
+    var sel = $(element).children().filter(".ace_gutter");
+    var div = $("<div style='position:absolute;top:0px'><object data='/img/grab_affordance.svg' type='image/svg+xml'></object></div>");
+    sel.append(div);
+};
