a6d3cafc2d9b679320e8e6858ba73b96bb6dab6e
Simon Urbanek <simon.urbanek@r-project.org>
2014-04-30 12:39:14 -0400

    update mkdist to include SKS and (optionally) SOLR; force-downgrade evaluate

--- scripts/mkdist.sh
+++ scripts/mkdist.sh
@@ -65,6 +65,11 @@ echo '' && echo "=== downloading dependencies ===" && echo ''
 
 echo "options(warn=2);cran=available.packages(contrib.url('http://r.research.att.com/',type='source'),type='source');rf=available.packages(contrib.url('http://r.research.att.com/',type='source'),type='source');local=available.packages(contrib.url('file://$DST/pkg.repos',type='source'),type='source');rf.pkg=strsplit('$RFORGE_PKG',',',T)[[1]];stage1=unique(unlist(tools:::package_dependencies('rcloud.support',local,'all')));print(stage1);stage2=unique(c(stage1,unlist(tools:::package_dependencies(stage1,rbind(cran,rf,local),,TRUE))));rec=rownames(installed.packages(,'high'));stage2=stage2[!(stage2 %in% rec)];print(stage2);download.packages(rf.pkg,'$DST/pkg.repos/src/contrib',,'http://rforge.net',type='source');cran.pkg=stage2[!(stage2 %in% c('rcloud.support',rf.pkg))];download.packages(cran.pkg,'$DST/pkg.repos/src/contrib',,'http://r.research.att.com',type='source');writeLines(stage2,'$DST/pkg.repos/pkg.list')" | R --vanilla --slave
 
+## explicit overrides:
+## * evaluate >0.5.1 has a fatal bug concerning plots so we have to fall back to 0.5.1
+rm -f "$DST/pkg.repos/src/contrib/"evaluate_*.tar.gz
+echo "download.file('http://cran.r-project.org/src/contrib/00Archive/evaluate/evaluate_0.5.1.tar.gz','$DST/pkg.repos/src/contrib/evaluate_0.5.1.tar.gz')" | R --vanilla --slave
+
 echo "tools:::write_PACKAGES('$DST/pkg.repos/src/contrib')" | R --vanilla --slave
 
 if [ ! -e "$DST/pkg.repos/pkg.list" ]; then
@@ -81,6 +86,27 @@ done
 
 echo $BRANCH > "$DST/rcloud/REVISION"
 echo $REV >> "$DST/rcloud/REVISION"
+
+## include SKS to simplify deployment
+mkdir -p "$DST/rcloud/services"
+git clone "https://github.com/s-u/SessionKeyServer.git" "$DST/rcloud/services/SessionKeyServer"
+rm -rf "$DST/rcloud/services/SessionKeyServer/.git"
+
+## include SOLR only if requested
+if [ -n "$INCLUDE_SOLR" ]; then
+## download SOLR - this duplicates conf/solrsetup.sh but makes
+## it self-contained in the release
+mkdir -p "$DST/rcloud/services/solr"
+VER=4.5.1
+curl -L "http://archive.apache.org/dist/lucene/solr/$VER/solr-$VER.tgz" | tar fxz - -C "$DST/rcloud/services/"
+mv "$DST/rcloud/services/solr-4.5.1" "$DST/rcloud/services/solr"
+## setup solr config
+cp -R "$DST/rcloud/services/solr/example/solr/collection1/" "$DST/rcloud/services/solr/example/solr/rcloudnotebooks"
+mkdir "$DST/rcloud/services/solr/example/solr/rcloudnotebooks/data"
+rm "$DST/rcloud/services/solr/example/solr/rcloudnotebooks/core.properties"
+cp "$DST/rcloud/conf/solr/schema.xml" "$DST/rcloud/services/solr/example/solr/rcloudnotebooks/conf/"
+cp "$DST/rcloud/conf/solr/solrconfig.xml" "$DST/rcloud/services/solr/example/solr/rcloudnotebooks/conf/"
+fi
 
 rm -rf "$DST/tmp"
 
