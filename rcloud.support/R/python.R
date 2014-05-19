ea34038ebc0338e79c40bafbf3b6e79b086d4bfa
Carlos Scheidegger <carlos.scheidegger@gmail.com>
2014-05-19 23:03:09 +0000

    custom IPython kernel; supports direct sending of ipython magics through a special channel, lets us automatically configure retina support because we're good people like that.

--- rcloud.support/R/python.R
+++ rcloud.support/R/python.R
@@ -3,15 +3,21 @@ rcloud.start.python <- function()
   require(rpython2, quietly=TRUE)
   py.init()
   sys <- py.import("sys")
-  py.attr(sys, "path", .ref=TRUE)$append(dirname(system.file("python", "notebook_runner.py", package="rcloud.support")))
+  path <- dirname(system.file("python", "notebook_runner.py", package="rcloud.support"))
+  py.attr(sys, "path", .ref=TRUE)$append(path)
   sys$argv <- c("rcloud")
   py.eval("import notebook_runner")
-  py.eval("runner = notebook_runner.NotebookRunner(extra_arguments=['--matplotlib=inline'], executable='python')")
+  py.eval(paste("runner = notebook_runner.NotebookRunner(rcloud_support_path='", path, "', extra_arguments=['--matplotlib=inline'], executable='python')", sep=''))
   py.eval("def run(cmd): return runner.run_cmd(cmd)")
+  py.eval("def magic(cmd): return runner.run_magic(cmd)")
   .session$python.run <- py.get("run", .ref=TRUE)
+  .session$python.magic <- py.get("magic", .ref=TRUE)
 }
 
 rcloud.exec.python <- function(cmd)
 {
+  if (.session$device.pixel.ratio > 1) {
+    py.call(.session$python.magic, "config InlineBackend.figure_format = 'retina'")
+  }
   py.call(.session$python.run, cmd)
 }
