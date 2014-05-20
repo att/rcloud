a21dc0ce79b8cd6d67bcdee1eb8206f88c8698ad
Carlos Scheidegger <cscheid@research.att.com>
2014-05-20 19:11:02 -0400

    start redis manually on travis

--- scripts/create-travis-configuration.sh
+++ scripts/create-travis-configuration.sh
@@ -8,8 +8,3 @@ echo "github.base.url: https://github.com/" >> conf/rcloud.conf
 echo "github.api.url: https://api.github.com/" >> conf/rcloud.conf
 echo "github.gist.url: https://gist.github.com/" >> conf/rcloud.conf
 echo "rcs.engine: redis" >> conf/rcloud.conf
-echo "this is what I got:"
-cat conf/rcloud.conf | grep -v client
-echo "directory:"
-ls
-ls conf
