4f0beb2dd2a708b5eecb1330ecfb075962d4584a
amol-jore <amol.jore@yahoo.com>
2014-06-11 17:54:36 -0400

    fixed #621 : (Corrected Indentation)

--- htdocs/js/ui/configure_readonly.js
+++ htdocs/js/ui/configure_readonly.js
@@ -3,13 +3,13 @@
  */
 RCloud.UI.configure_readonly = function() {
     var fork_revert = $('#fork-revert-notebook');
-	var readonly_notebook = $("#readonly-notebook");
+    var readonly_notebook = $("#readonly-notebook");
     if(shell.notebook.model.read_only()) {
         $('#prompt-div').hide();
         fork_revert.text(shell.notebook.controller.is_mine() ? 'Revert' : 'Fork');
         fork_revert.show();
-		readonly_notebook.html("<span style=\"color:#fff;\">Authored By <b>"+shell.notebook.model.user()+"</b> <small><i>*This Notebook is read-only</i></small></span>");
-		readonly_notebook.show();
+	readonly_notebook.html("<span style=\"color:#fff;\">Authored By <b>"+shell.notebook.model.user()+"</b> <small><i>*This Notebook is read-only</i></small></span>");
+	readonly_notebook.show();
         $('#save-notebook').hide();
         $('#output').sortable('disable');
         $('#upload-to-notebook')
@@ -19,7 +19,7 @@ RCloud.UI.configure_readonly = function() {
     else {
         $('#prompt-div').show();
         fork_revert.hide();
-		readonly_notebook.hide();
+	readonly_notebook.hide();
         $('#save-notebook').show();
         $('#output').sortable('enable');
         $('#upload-to-notebook')
