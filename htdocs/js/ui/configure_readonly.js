2360d884c1a8ce000e825ab152ac8a7d7a900204
vagrant <vagrant@precise32.(none)>
2014-06-12 04:53:49 +0000

    fixed #621 : author [NB](read only)

--- htdocs/js/ui/configure_readonly.js
+++ htdocs/js/ui/configure_readonly.js
@@ -4,12 +4,15 @@
 RCloud.UI.configure_readonly = function() {
     var fork_revert = $('#fork-revert-notebook');
     var readonly_notebook = $("#readonly-notebook");
+    var notebook_author = $("#notebook-author");
     if(shell.notebook.model.read_only()) {
         $('#prompt-div').hide();
         fork_revert.text(shell.notebook.controller.is_mine() ? 'Revert' : 'Fork');
         fork_revert.show();
-	readonly_notebook.html("<span style=\"color:#fff;\">Authored By <b>"+shell.notebook.model.user()+"</b> <small><i>*This Notebook is read-only</i></small></span>");
-	readonly_notebook.show();
+        notebook_author.text(shell.notebook.model.user());
+        notebook_author.show();
+        readonly_notebook.html("<span style=\"color:#fff;\">(read-only)</span>");
+        readonly_notebook.show();
         $('#save-notebook').hide();
         $('#output').sortable('disable');
         $('#upload-to-notebook')
@@ -19,7 +22,8 @@ RCloud.UI.configure_readonly = function() {
     else {
         $('#prompt-div').show();
         fork_revert.hide();
-	readonly_notebook.hide();
+        readonly_notebook.hide();
+        notebook_author.text(shell.notebook.model.user());
         $('#save-notebook').show();
         $('#output').sortable('enable');
         $('#upload-to-notebook')
