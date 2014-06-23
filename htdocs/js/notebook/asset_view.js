1eb23de29c122d10ea37a39c8afb910e9aa13c4f
vagrant <vagrant@precise32.(none)>
2014-06-23 09:42:07 +0000

    fixed #387 : handled the click event and open issues

--- htdocs/js/notebook/asset_view.js
+++ htdocs/js/notebook/asset_view.js
@@ -11,57 +11,53 @@ Notebook.Asset.create_html_view = function(asset_model)
     anchor.append(filename_span);
     filename_div.append(anchor);
     anchor.append(remove);
-    filename_span.attr("contenteditable","true");
-    var asset_old_name = "";
-    filename_span.click(function() {
-        if(asset_model.active()){
-            asset_old_name = filename_span.text();
-            var rename_file = function(v){
-                var new_asset_name = filename_span.text();
-                var old_asset_contet = asset_model.content();
-                if (new_asset_name == "") {
-                    filename_span.text(asset_old_name);
-                    return;
-                }
-                if (Notebook.is_part_name(new_asset_name)) {
-                    alert("Asset names cannot start with 'part[0-9]', sorry!");
-                    filename_span.text(asset_old_name);
-                    return;
-                }
-                var found = shell.notebook.model.has_asset(new_asset_name);
-                if (found){
-                    found.controller.select();
-                }
-                else {
-                    shell.notebook.controller
-                    .append_asset(old_asset_contet, new_asset_name)
-                    .then(function (controller) {
-                        controller.select();
-                    });
-                    asset_model.controller.remove(true);
-                }
-            }
-            function select(el) {
-                if(el.childNodes.length !== 1 || el.firstChild.nodeType != el.TEXT_NODE)
-                    throw new Error('expecting simple element with child text');
-                var text = el.firstChild.textContent;
-                var range = document.createRange();
-                range.setStart(el.firstChild, text.lastIndexOf('/') + 1);
-                range.setEnd(el.firstChild, text.length);
-                return range;
-            }
-            var editable_opts = {
-                change: rename_file,
-                select: select,
-                validate: function(name) { return editor.validate_name(name); }
-            };
-            var editable_mode = !shell.notebook.model.read_only();
-            ui_utils.editable(filename_span, $.extend({allow_edit: editable_mode,inactive_text: filename_span.text(),active_text: filename_span.text()},editable_opts));
+    var asset_old_name = filename_span.text();
+    var rename_file = function(v){
+        var new_asset_name = filename_span.text();
+        var old_asset_contet = asset_model.content();
+        if (new_asset_name == "") {
+            filename_span.text(asset_old_name);
+            return;
+        }
+        if (Notebook.is_part_name(new_asset_name)) {
+            alert("Asset names cannot start with 'part[0-9]', sorry!");
+            filename_span.text(asset_old_name);
+            return;
+        }
+        var found = shell.notebook.model.has_asset(new_asset_name);
+        if (found){
+            found.controller.select();
         }
         else {
-            asset_model.controller.select();
-            filename_span.attr("contenteditable",""+!shell.notebook.model.read_only());
+            shell.notebook.controller
+            .append_asset(old_asset_contet, new_asset_name)
+            .then(function (controller) {
+                controller.select();
+            });
+            asset_model.controller.remove(true);
         }
+    }
+    function select(el) {
+        if(el.childNodes.length !== 1 || el.firstChild.nodeType != el.TEXT_NODE)
+            throw new Error('expecting simple element with child text');
+        var text = el.firstChild.textContent;
+        var range = document.createRange();
+        range.setStart(el.firstChild, text.lastIndexOf('/') + 1);
+        range.setEnd(el.firstChild, text.length);
+        return range;
+    }
+    var editable_opts = {
+        change: rename_file,
+        select: select,
+        validate: function(name) { return editor.validate_name(name); }
+    };
+    var editable_mode = !shell.notebook.model.read_only();
+    ui_utils.editable(filename_span, $.extend({allow_edit: editable_mode,inactive_text: filename_span.text(),active_text: filename_span.text()},editable_opts));
+    filename_span.click(function() {
+        if(!asset_model.active()){
+            asset_model.controller.select();
+        }
+        filename_span.attr("contenteditable",""+!shell.notebook.model.read_only());        
     });
     remove.click(function() {
         asset_model.controller.remove();
