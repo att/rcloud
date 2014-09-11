cdd6208ac514932cc5ec43ea7f6872515baafd0f
amol-jore <amol.jore@mu-sigma.com>
2014-09-11 17:06:41 +0530

    Setting file element to null,instead of recreating

--- htdocs/shell_tab.js
+++ htdocs/shell_tab.js
@@ -314,11 +314,7 @@ var shell = (function() {
                                 editor.set_notebook_visibility(notebook.id, true);
                             });
                         }).then(function(v){
-                            $("#notebook-file-upload").remove();
-                            var file_select = $('<input type="file" id="notebook-file-upload" size="50"></input>');
-                            file_select.click(function() { ui_utils.disable_bs_button(import_button); })
-                                .change(function() { do_upload(file_select[0].files[0]); });
-                            $(body.children()[0]).append(file_select);
+                            $("#notebook-file-upload")[0].value = null;
                         });;
                     }
                     dialog.modal('hide');
