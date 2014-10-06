695ed5b4182caab2cad0ebfba29a5c9a79dda702
sujitbehera27 <sujitbehera27@gmail.com>
2014-10-06 14:41:40 -0400

    This fixes #798

--- htdocs/js/ui/comments_frame.js
+++ htdocs/js/ui/comments_frame.js
@@ -6,6 +6,7 @@ RCloud.UI.comments_frame = {
         $("#comment-submit").click(function() {
             if(!Notebook.empty_for_github($("#comment-entry-body").val())) {
                 editor.post_comment($("#comment-entry-body").val());
+                $("#comment-entry-body").height("38px");
             }
             return false;
         });
@@ -15,9 +16,15 @@ RCloud.UI.comments_frame = {
                 (e.ctrlKey || e.metaKey)) {
                 if(!Notebook.empty_for_github($("#comment-entry-body").val())) {
                     editor.post_comment($("#comment-entry-body").val());
+                    $("#comment-entry-body").height("38px");
                 }
                 return false;
             }
+            var t = $("#comment-entry-body");
+            t.bind('input', function() {
+                t.css("height", '38 px');
+                t.css("height", t[0].scrollHeight  + 'px');
+            });
             return undefined;
         });
     }
