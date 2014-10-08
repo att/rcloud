a57e6ded204e68fec8a9c01fad43edf5a9fc2dca
Gordon Woodhull <gordon@woodhull.com>
2014-10-08 10:30:40 -0400

    move styles into stylesheet; tweak margins
    to make the highlight visible on all sides

--- htdocs/js/rcloud_bundle.js
+++ htdocs/js/rcloud_bundle.js
@@ -3631,21 +3631,36 @@ RCloud.UI.comments_frame = {
         return RCloud.UI.panel_loader.load_snippet('comments-snippet');
     },
     init: function() {
+        var comment = $("#comment-entry-body");
+        var count = 0;
+        var scroll_height = "";
         $("#comment-submit").click(function() {
-            if(!Notebook.empty_for_github($("#comment-entry-body").val())) {
-                editor.post_comment($("#comment-entry-body").val());
+            if(!Notebook.empty_for_github(comment.val())) {
+                editor.post_comment(comment.val());
+                comment.height("41px");
             }
             return false;
         });
 
-        $("#comment-entry-body").keydown(function (e) {
-            if ((e.keyCode == 10 || e.keyCode == 13 || e.keyCode == 115 || e.keyCode == 19) &&
-                (e.ctrlKey || e.metaKey)) {
-                if(!Notebook.empty_for_github($("#comment-entry-body").val())) {
-                    editor.post_comment($("#comment-entry-body").val());
+        comment.keydown(function (e) {
+            if (e.keyCode == 13 && (e.ctrlKey || e.metaKey)) {
+                if(!Notebook.empty_for_github(comment.val())) {
+                    editor.post_comment(comment.val());
+                    comment.height("41px");
+                    count = 0;
+                    scroll_height = "";
                 }
                 return false;
             }
+            comment.bind('input', function() {
+                if(count > 1 && scroll_height != comment[0].scrollHeight) {
+                    comment.height((comment[0].scrollHeight)  + 'px');
+                }
+                count = count + 1;
+                scroll_height = comment[0].scrollHeight;
+                $("#comments-qux").animate({ scrollTop: $(document).height() }, "slow");
+                return false;
+            });
             return undefined;
         });
     }
