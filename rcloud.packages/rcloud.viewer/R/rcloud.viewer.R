d5ef4a2792568508a1d87e439a8a8eae360528c2
Gordon Woodhull <gordon@woodhull.com>
2014-10-13 10:11:12 -0400

    basic rcloud.viewer
    it shows stuff. it might be in the wrong order though.

---
+++ rcloud.packages/rcloud.viewer/R/rcloud.viewer.R
@@ -0,0 +1,30 @@
+# there doesn't seem to be a lower-level way to customize View?
+
+
+View <- function (x, title)
+{
+  # borrow pre-processing from base R, replace the final command
+  if (missing(title))
+    title <- paste("Data:", deparse(substitute(x))[1])
+  as.num.or.char <- function(x) {
+    if (is.character(x))
+      x
+    else if (is.numeric(x)) {
+      storage.mode(x) <- "double"
+      x
+    }
+    else as.character(x)
+  }
+  x0 <- as.data.frame(x)
+  x <- lapply(x0, as.num.or.char)
+  rn <- row.names(x0)
+  if (any(rn != seq_along(rn)))
+    x <- c(list(row.names = rn), x)
+  if (!is.list(x) || !length(x) || !all(sapply(x, is.atomic)) ||
+      !max(sapply(x, length)))
+    stop("invalid 'x' argument")
+
+  # R calls invisible(.External2(C_dataviewer, x, title)) here
+  invisible(rcloud.viewer.caps$view(x, title))
+}
+
---
+++ rcloud.packages/rcloud.viewer/R/zzz.R
@@ -0,0 +1,16 @@
+rcloud.viewer.caps <- NULL
+
+.onLoad <- function(libname, pkgname)
+{
+  f <- function(module.name, module.path) {
+    path <- system.file("javascript", module.path, package="rcloud.viewer")
+    caps <- rcloud.install.js.module(module.name,
+                                     paste(readLines(path), collapse='\n'))
+    caps
+  }
+  rcloud.viewer.caps <<- f("rcloud.viewer", "rcloud.viewer.js")
+  if(!is.null(rcloud.viewer.caps)) {
+    rcloud.viewer.caps$init()
+    rcloud.out(rcloud.viewer.caps)
+  }
+}
