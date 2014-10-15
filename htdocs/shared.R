16ff0d7aed75b55f3036fb0bd7c1db67de96fe7b
Simon Urbanek <simon.urbanek@R-project.org>
2014-10-15 10:38:41 -0400

    small fix in shared.R

--- htdocs/shared.R
+++ htdocs/shared.R
@@ -3,12 +3,12 @@ search.path <- c("rcloud.support", "shiny")
 
 run <- function(url, query, body, headers) {
   if (is.null(path.info)) stop("missing path in the URL")
-  pex <- strsplit(path.info, "/+")
+  pex <- strsplit(path.info, "/+")[[1]]
   if (any(pex == "..")) stop("invalid component in the path URL")
   base <- paste(c("www", "shared", pex), collapse="/")
   for (pkg in search.path) if(nzchar(fn <- system.file(base, package=pkg))) break
   if (!nzchar(fn) || !file.exists(fn))
-    return(list(paste0("ERROR: item '", path.info, "' not found"), "text/html", character(), 404L))
+    return(list(paste0("ERROR: item '", path.info, "' [", base, "] not found"), "text/html", character(), 404L))
   s <- file.info(fn)$size
   f <- file(fn, "rb")
   r <- readBin(f, raw(), s)
