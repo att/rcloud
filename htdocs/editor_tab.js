3ab895c47f94bf479abc2b237e5b7a5759117894
sujit <sujitbehera27@gmail.com>
2014-10-16 13:50:01 -0400

    This Fixes #856
    Typo corrected

--- htdocs/editor_tab.js
+++ htdocs/editor_tab.js
@@ -1340,7 +1340,7 @@ var editor = function () {
                 rcloud.config.set_current_notebook(current_);
                 rcloud.config.set_recent_notebook(result.id, (new Date()).toString());
                 $('.view_menu li a').click(function(){
-                    $("#view_mode").html(selText+' <span class="caret"></span>');
+                    $("#view_mode").html($(this).text()+' <span class="caret"></span>');
                     RCloud.UI.share_button.set_link(result,$(this).text());
                 });
                 RCloud.UI.share_button.set_link(result);
