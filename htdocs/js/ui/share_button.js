2bd2bd921245505fa52d3d647cf032d13ff2421e
Gordon Woodhull <gordon@woodhull.com>
2014-10-19 15:08:20 -0400

    use switch statement for share types; fix notebook.R&version=

--- htdocs/js/ui/share_button.js
+++ htdocs/js/ui/share_button.js
@@ -1,20 +1,23 @@
 RCloud.UI.share_button = {
     set_link: function(notebook, type) {
         var link = window.location.protocol + '//' + window.location.host + '/';
-        var suffix;
-        if(type != "" && type != undefined) {
-            if(type === "notebook.R") {
-                suffix = type + '/' + shell.gistname();
-            } else {
-                suffix = type + '?notebook=' + shell.gistname();
-            }
-        } else {
+        var suffix, query_started = true;
+        switch(type) {
+        case 'notebook.R':
+            suffix = type + '/' + shell.gistname();
+            query_started = false;
+            break;
+        case 'mini.html':
+            suffix = type + '?notebook=' + shell.gistname();
+            break;
+        case 'view.html':
+        default:
             suffix = 'view.html?notebook=' + shell.gistname();
         }
         link += suffix;
         var v = shell.version();
         if(v)
-            link += '&version=' + v;
+            link += query_started?'&':'?' + 'version=' + v;
         $("#share-link").attr("href", link);
     }
 };
