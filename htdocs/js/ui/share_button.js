a4cd5d714299af08afb50175a1cda33b39ab3d10
Gordon Woodhull <gordon@woodhull.com>
2014-10-19 14:34:09 -0400

    spacing changes

--- htdocs/js/ui/share_button.js
+++ htdocs/js/ui/share_button.js
@@ -1,20 +1,20 @@
 RCloud.UI.share_button = {
-    set_link: function(notebook,type) {
+    set_link: function(notebook, type) {
         var link = window.location.protocol + '//' + window.location.host + '/';
-        var suffix ;
-        if(type != "" && type!= undefined) {
-            if(type === "notebook.R"){
-                suffix= type + '/' + shell.gistname();
-            }else {
-                suffix= type + '?notebook=' + shell.gistname();
+        var suffix;
+        if(type != "" && type != undefined) {
+            if(type === "notebook.R") {
+                suffix = type + '/' + shell.gistname();
+            } else {
+                suffix = type + '?notebook=' + shell.gistname();
             }
-        }else{
+        } else {
             suffix = 'view.html?notebook=' + shell.gistname();
         }
-        link +=suffix;
+        link += suffix;
         var v = shell.version();
         if(v)
-            link += '&version='+v;
+            link += '&version=' + v;
         $("#share-link").attr("href", link);
     }
 };
