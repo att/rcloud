5a813102a00a178b0f2386cf244bb8115f3e88a7
Gordon Woodhull <gordon@woodhull.com>
2014-10-27 04:42:39 -0400

    export.R - currently just outputs the Markdown

---
+++ htdocs/export.R
@@ -0,0 +1,18 @@
+run <- function(url, query, body, headers)
+{
+  et <- "Unable to connect"
+  tryCatch({
+    require(rcloud.client)
+    conn <- connect.to.rcloud(url, query, body, headers)
+    result <- resolve.notebook.id(conn, path.info)
+
+    cells <- RSclient::RS.eval.qap(conn$rserve, as.call(list(conn$caps$rcloud$notebook_cells, result$notebook, result$version)))
+
+    canonicalized <- lapply(cells, function(cell) {
+      canonicalize.command(cell$content, cell$language)
+    })
+    nbmd <- paste(canonicalized, collapse='\n')
+  }, error=function(e) {
+    list(paste(et,"<pre>", paste(as.character(e), collapse='\n'), "</pre>"), "text/html")
+  })
+}
