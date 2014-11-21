3cd640e0bd3499fb1119165f811afc1ef775076b
Chris Hendrix and Weyman Fung <wfung@pivotal.io>
2014-11-21 10:45:55 -0800

    Updates banners to use heredoc syntax
    - Uses `echo -e` for single line echos requiring newlines
    - Uses `cat <<EOF EOF` heredoc style for larger banners
    - Improves readability of subshell processes by moving them to multiple
      lines

--- scripts/bootstrapR.sh
+++ scripts/bootstrapR.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
 
 if [ x"$1" = 'x-h' ]; then
-    echo ''
-    echo " Usage: $0 [--mk-dist]"
-    echo ''
-    echo ' This script must be run from the RCloud root directory and it'
-    echo ' installs R packages needed by RCloud in the R installation'
-    echo ''
-    echo ' Optional environment variables:'
-    echo ' RBIN        - location of the R executable [R]'
-    echo ' RCREPO      - repository of RCloud packages [<RCloud>/packages]'
-    echo ' DISTREP     - distribution repository [<RCloud>/dist-repos]'
-    echo ''
-    echo ' If DISTREP exists, it will be used to bootstrap R without using'
-    echo " remote repositories. If it doesn't exist, packages from RCREPO"
-    echo ' are used along with dependencies from remote repositories.'
-    echo " If RCREPO doesn't exist, it is created from the source tree."
-    echo ''
-    echo ' DISTREP can be created using --mk-dist in which case it'
-    echo ' is built from sources and remote repositories'
-    echo ''
+    cat <<EOF
+
+ Usage: $0 [--mk-dist]
+
+ This script must be run from the RCloud root directory and it
+ installs R packages needed by RCloud in the R installation
+
+ Optional environment variables:
+ RBIN        - location of the R executable [R]
+ RCREPO      - repository of RCloud packages [<RCloud>/packages]
+ DISTREP     - distribution repository [<RCloud>/dist-repos]
+
+ If DISTREP exists, it will be used to bootstrap R without using
+ remote repositories. If it doesn't exist, packages from RCREPO
+ are used along with dependencies from remote repositories.
+ If RCREPO doesn't exist, it is created from the source tree.
+
+ DISTREP can be created using --mk-dist in which case it
+ is built from sources and remote repositories
+
+EOF
     exit 0
 fi
 
@@ -36,9 +38,7 @@ fi
 
 ok=`echo 'if(R.version$major>=3)cat("OK\n")' | "$RBIN" --slave --vanilla`
 if [ "x$ok" != "xOK" ]; then
-    echo '' 1>&2
-    echo ' ERROR: R is not available in the correct version.' 1>&2
-    echo '' 1>&2
+    echo -e '\n ERROR: R is not available in the correct version.\n' 1>&2
     exit 1
 fi
 
@@ -48,6 +48,7 @@ mkdist=''
 if [ x"$1" = "x--mk-dist" ]; then
     mkdist=yes
 fi
+
 if [ -n "$mkdist" -o x"$1" = "x--clean" ]; then
     echo " --- Cleaning existing repositories"
     rm -rf "$DISTREP/src/contrib" "$RCREPO/src/contrib"
@@ -57,13 +58,21 @@ if [ ! -e "$DISTREP/src/contrib/PACKAGES" -o -n "$mkdist" ]; then
     ## we need RCREPO populated in any case
     if [ ! -e "$RCREPO/src/contrib/PACKAGES" -o -n "$mkdist" ]; then
         echo " --- Creating RCloud repository $RCREPO"
-        echo '' 1>&2
+
         if ! mkdir -p "$RCREPO/src/contrib"; then
-            echo "ERROR: cannot create src/contrib in $RCREPO, please set RCREPO if other location is desired" 1>&2
+            echo -e "\nERROR: cannot create src/contrib in $RCREPO, please set RCREPO if other location is desired" 1>&2
             exit 1
         fi
+
         echo " Builds RCloud packages"
-        ( cd "$RCREPO/src/contrib"; for src in `ls $WD/rcloud.*/DESCRIPTION $WD/rcloud.packages/*/DESCRIPTION $WD/packages/*/DESCRIPTION 2>/dev/null`; do "$RBIN" CMD build `dirname "$src"`; done )
+        (
+          cd "$RCREPO/src/contrib"
+
+          for src in `ls $WD/rcloud.*/DESCRIPTION $WD/rcloud.packages/*/DESCRIPTION $WD/packages/*/DESCRIPTION 2>/dev/null`
+          do
+            "$RBIN" CMD build `dirname "$src"`
+          done
+        )
         echo "tools:::write_PACKAGES('$RCREPO/src/contrib')" | "$RBIN" --vanilla --slave --no-save || exit 1
     fi
 
@@ -90,23 +99,27 @@ fi
 
 ok=`echo 'library(rcloud.support);library(rcloud.client);library(Cairo);library(rjson);cat("OK\n")' | "$RBIN" --slave --vanilla`
 if [ "x$ok" != "xOK" ]; then
-    echo '' 1>&2
-    echo ' ERROR: one or more packages could not be installed.' 1>&2
-    echo ' Please check that you have all necessary dependencies installed' 1>&2
-    echo '' 1>&2
+    cat <<EOF >&2
+
+ ERROR: one or more packages could not be installed.
+ Please check that you have all necessary dependencies installed
+
+EOF
     exit 1
 fi
 
-echo ''
-echo '============================================================================'
-echo ''
-echo ' IMPORTANT: please edit conf/rcloud.conf to setup your GitHub application'
-echo ' (see instructions on https://github.com/att/rcloud for the procedure)'
-echo ''
-echo ' After doing so, you can then use conf/start to start up the RCloud server'
-echo ''
-echo ' NOTE: If you use force or upgrade in check.installation() it will upgrade'
-echo '       to the latest development version of RCloud which may or may not be'
-echo '       what you want. Re-run this script if you get into trouble and want'
-echo '       back to the released version.'
-echo ''
+cat <<EOF
+
+ ============================================================================
+
+  IMPORTANT: please edit conf/rcloud.conf to setup your GitHub application
+  (see instructions on https://github.com/att/rcloud for the procedure)
+
+  After doing so, you can then use conf/start to start up the RCloud server
+
+  NOTE: If you use force or upgrade in check.installation() it will upgrade
+        to the latest development version of RCloud which may or may not be
+        what you want. Re-run this script if you get into trouble and want
+        back to the released version.
+
+EOF
