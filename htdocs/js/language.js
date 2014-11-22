d68f9e168aeb5cf8240458e146a071357463aa71
Gordon Woodhull <gordon@woodhull.com>
2014-11-21 23:18:03 -0500

    use usual "class pattern" in RCloud.language
    the "local variables pattern" is okay, but this is a little more expressive

--- htdocs/js/language.js
+++ htdocs/js/language.js
@@ -1,30 +1,29 @@
-(function() {
+RCloud.language = (function() {
+    var ace_modes_ = {
+        R: "ace/mode/r",
+        Python: "ace/mode/python",
+        Markdown: "ace/mode/rmarkdown",
+        CSS: "ace/mode/css",
+        JavaScript: "ace/mode/javascript",
+        Text: "ace/mode/text"
+    };
+    var langs_ = [];
 
-var langs = [];
-
-RCloud.language = {
-    ace_mode: function(language) {
-        var modes = {
-            R: "ace/mode/r",
-            Python: "ace/mode/python",
-            Markdown: "ace/mode/rmarkdown",
-            CSS: "ace/mode/css",
-            JavaScript: "ace/mode/javascript",
-            Text: "ace/mode/text"
-        };
-        return modes[language] || modes.Text;
-    },
-    // don't call _set_available_languages yourself; it's called
-    // by the session initialization code.
-    _set_available_languages: function(list) {
-        if (_.isArray(list))
-            langs = list;
-        else
-            langs = [list];
-    },
-    available_languages: function() {
-        return langs;
-    }
-};
+    return {
+        ace_mode: function(language) {
+            return ace_modes_[language] || ace_modes_.Text;
+        },
+        // don't call _set_available_languages yourself; it's called
+        // by the session initialization code.
+        _set_available_languages: function(list) {
+            if (_.isArray(list))
+                langs_ = list;
+            else
+                langs_ = [list];
+        },
+        available_languages: function() {
+            return langs_;
+        }
+    };
 
 })();
