7d1309b6fa905d3d13b816ce02d6c9e615c38d25
souravkjha <sourav.jha@mu-sigma.com>
2014-12-03 11:35:46 +0000

    This Fixes #1113 : Appended the asset name to be deleted in confirmation box

--- htdocs/js/notebook/asset_controller.js
+++ htdocs/js/notebook/asset_controller.js
@@ -13,7 +13,8 @@ Notebook.Asset.create_controller = function(asset_model)
             asset_model.active(false);
         },
         remove: function(force) {
-            var msg = "Are you sure you want to remove the asset from the notebook?";
+            var asset_name = asset_model.json().filename;
+            var msg = "Do you want to remove the asset '" +asset_name+ "' from the notebook?";
             if (force || confirm(msg)) {
                 asset_model.parent_model.controller.remove_asset(asset_model);
                 var assets = asset_model.parent_model.assets;
