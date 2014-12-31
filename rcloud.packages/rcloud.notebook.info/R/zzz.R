1e36f484448f800980bea431bf72f98c326dfb55
Gordon Woodhull <gordon@woodhull.com>
2014-12-30 21:31:59 -0500

    rename rcloud.NotebookInfo -> rcloud.notebook.info, 2/2
    the contents

--- rcloud.packages/rcloud.notebook.info/R/zzz.R
+++ rcloud.packages/rcloud.notebook.info/R/zzz.R
@@ -1,14 +1,14 @@
-rcloud.NotebookInfo.caps <- NULL
+rcloud.notebook.info.caps <- NULL
 
 .onLoad <- function(libname, pkgname)
 {
   f <- function(module.name, module.path) {
-    path <- system.file("javascript", module.path, package="rcloud.NotebookInfo")
+    path <- system.file("javascript", module.path, package="rcloud.notebook.info")
     caps <- rcloud.install.js.module(module.name,
                                      paste(readLines(path), collapse='\n'))
     caps
   }
-  rcloud.NotebookInfo.caps <<- f("rcloud.NotebookInfo", "rcloud.NotebookInfo.js")
-  if(!is.null(rcloud.NotebookInfo.caps))
-    rcloud.NotebookInfo.caps$init()
+  rcloud.notebook.info.caps <<- f("rcloud.notebook.info", "rcloud.notebook.info.js")
+  if(!is.null(rcloud.notebook.info.caps))
+    rcloud.notebook.info.caps$init()
 }
