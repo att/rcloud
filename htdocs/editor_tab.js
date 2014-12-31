att/rcloud

commit 61cb107907ab4d25b6abc8c8821399d1f21ba8da

don't have "terse dates" control years; always show dates for notebooks
.. as opposed to versions, where the date is controlled by "terse dates"

@sujitbehera, I didn't intend for "terse dates" to control whether years
are shown; I think that's unrelated.  So I'm reverting that part and only
applying "terse dates" for versions.

htdocs/editor_tab.js
@@ -924,9 +924,7 @@ var editor = function () {
         var time_part = '<span class="notebook-time">' + date.getHours() + ':' + pad(date.getMinutes()) + '</span>';
         var date_part = (date.getMonth()+1) + '/' + date.getDate();
         var year_part = date.getFullYear().toString().substr(2,2);
-        if(diff < 24*60*60*1000 && isDateSame && show_terse_dates_)
-            return time_part;
-        else if(date.getFullYear() === now.getFullYear() && show_terse_dates_)
+        if(date.getFullYear() === now.getFullYear())
             return '<span>' + date_part + ' ' + time_part + '</span>';
         else
             return '<span>' + date_part + '/' + year_part + ' ' + time_part + '</span>';

htdocs/js/rcloud_bundle.js
@@ -5312,6 +5312,17 @@ RCloud.UI.settings_frame = (function() {
                     label: "Subscribe To Comments"
                 })
             });
+            this.add({
+                'show-terse-dates': that.checkbox({
+                    id:"show-terse-dates",
+                    sort: 100,
+                    default_value: true,
+                    label: "Show Terse Version Dates",
+                    set: function(val) {
+                        editor.set_terse_dates(val);
+                    }
+                })
+            });
         },
         load: function() {
             var that = this;

htdocs/js/ui/settings_frame.js
@@ -85,7 +85,7 @@ RCloud.UI.settings_frame = (function() {
                     id:"show-terse-dates",
                     sort: 100,
                     default_value: true,
-                    label: "Show Terse Dates",
+                    label: "Show Terse Version Dates",
                     set: function(val) {
                         editor.set_terse_dates(val);
                     }

commit b7b4e6c2a732a9019ac9f13949ce8c77412f0f82

restore not showing date for terse dates
i broke this in 61cb107907ab4d25b6abc8c8821399d1f21ba8da
because i forgot format_date_time_stamp is used for both notebooks & versions

htdocs/editor_tab.js
@@ -593,7 +593,7 @@ var editor = function () {
                 if(diff <= 60*1000 && hour_same && min_same && show_terse_dates_)
                     return null;
                 else
-                    return format_date_time_stamp(d1, diff, isDateSame);
+                    return format_date_time_stamp(d1, diff, isDateSame, true);
             }
 
             function display_date_for_entry(i) {
@@ -903,13 +903,15 @@ var editor = function () {
         }
     }
 
-    function format_date_time_stamp(date, diff, isDateSame) {
+    function format_date_time_stamp(date, diff, isDateSame, for_version) {
         function pad(n) { return n<10 ? '0'+n : n; }
         var now = new Date();
         var time_part = '<span class="notebook-time">' + date.getHours() + ':' + pad(date.getMinutes()) + '</span>';
         var date_part = (date.getMonth()+1) + '/' + date.getDate();
         var year_part = date.getFullYear().toString().substr(2,2);
-        if(date.getFullYear() === now.getFullYear())
+        if(diff < 24*60*60*1000 && isDateSame && show_terse_dates_ && for_version)
+            return time_part;
+        else if(date.getFullYear() === now.getFullYear())
             return '<span>' + date_part + ' ' + time_part + '</span>';
         else
             return '<span>' + date_part + '/' + year_part + ' ' + time_part + '</span>';
@@ -923,7 +925,7 @@ var editor = function () {
         var date = new Date(ds);
         var now = new Date();
         var diff = now - date;
-        return format_date_time_stamp(date, diff, true); //Third parameter is true by default as the date comparison is not required here
+        return format_date_time_stamp(date, diff, true, false);
     }
 
     function display_date(ds) {