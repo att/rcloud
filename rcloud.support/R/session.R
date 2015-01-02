4a9caff893096d0689c35d222200f27065b06268
Gordon Woodhull <gordon@woodhull.com>
2015-01-02 02:53:39 -0500

    revert two last rmarkdowns

--- rcloud.support/R/session.R
+++ rcloud.support/R/session.R
@@ -51,7 +51,7 @@ session.markdown.eval <- function(command, language, silent) {
   opts_chunk$set(dev="CairoPNG", tidy=FALSE)
 
   if (command == "") command <- " "
-  val <- try(rcloud.fake.markdownToHTML(text=paste(knit(text=command, envir=.GlobalEnv), collapse="\n"),
+  val <- try(markdownToHTML(text=paste(knit(text=command, envir=.GlobalEnv), collapse="\n"),
                             fragment=TRUE), silent=TRUE)
   if (!inherits(val, "try-error") && !silent && rcloud.debug.level()) print(val)
   if (inherits(val, "try-error")) {
