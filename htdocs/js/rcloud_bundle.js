ae72f2bc5207221ed41422ea859cbfa018a57f80
Gordon Woodhull <gordon@woodhull.com>
2015-01-09 12:10:33 -0500

    animate stop button while attempting to stop

--- htdocs/js/rcloud_bundle.js
+++ htdocs/js/rcloud_bundle.js
@@ -5436,7 +5436,8 @@ RCloud.UI.run_button = (function() {
     var run_button_ = $("#run-notebook"),
         running_ = false,
         queue_ = [],
-        cancels_ = [];
+        cancels_ = [],
+        stop_interval_ = null;
 
     function display(icon, title) {
         $('i', run_button_).removeClass().addClass(icon);
@@ -5464,17 +5465,26 @@ RCloud.UI.run_button = (function() {
             var that = this;
             run_button_.click(function() {
                 if(running_)
-                    rcloud.signal_to_compute(2); // SIGINT
+                    that.stop();
                 else
                     shell.run_notebook();
             });
             RCloud.session.listeners.push({
                 on_reset: function() {
-                    that.stop();
+                    that.on_stopped();
                 }
             });
         },
         stop: function() {
+            rcloud.signal_to_compute(2) // SIGINT
+                .then(function() {
+                    stop_interval_ = window.setInterval(function() {
+                        run_button_.animate({opacity: 0.5}, 250, "easeOutCirc")
+                            .animate({opacity: 1.0}, 250, "easeOutCirc");
+                    }, 1000);
+                });
+        },
+        on_stopped: function() {
             cancels_.forEach(function(cancel) { cancel(); });
             queue_ = [];
             cancels_ = [];
@@ -5488,8 +5498,12 @@ RCloud.UI.run_button = (function() {
             if(!running_) {
                 start_queue()
                     .catch(function(xep) {
+                        if(stop_interval_) {
+                            window.clearInterval(stop_interval_);
+                            stop_interval_ = null;
+                        }
                         console.log(xep);
-                        that.stop();
+                        that.on_stopped();
                         // if this was due to a SIGINT, we're done
                         // otherwise we'll need to report this.
                         // stop executing either way.
