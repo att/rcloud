dec224691af66fca393b96d0954bfbbcb6ebc1c3
Gordon Woodhull <gordon@woodhull.com>
2015-01-12 04:34:15 -0500

    find next button, it's the default
    don't toggle the find dialog, esc or close button
    funniness of interleaved find and replace commands

--- htdocs/js/rcloud_bundle.js
+++ htdocs/js/rcloud_bundle.js
@@ -4408,24 +4408,28 @@ RCloud.UI.fatal_dialog = function(message, label, href) {
 })();
 RCloud.UI.find_replace = (function() {
     var find_dialog_ = null,
-        find_input_, replace_input_, replace_stuff_,
-        replace_all_,
+        find_input_, replace_desc_, replace_input_, replace_stuff_,
+        find_next_, replace_all_,
         shown_ = false, replace_mode_ = false,
-        nextReplace_ = null, nextFind_ = null;
+        nextReplace_ = null, nextFind_ = null,
+        matches_ = {}, currentCell_, currentMatch_;
     function toggle_find_replace(replace) {
         if(!find_dialog_) {
             find_dialog_ = $('<div id="find-dialog"></div>');
             var find_form = $('<form></form>');
             find_input_ = $('<input id="find-input" class="form-control-ext"></input>');
+            replace_desc_ = $('<span id="replace">&nbsp;Replace with: </span>');
             replace_input_ = $('<input id="replace-input" class="form-control-ext"></input>');
-            replace_all_ = $('<button id="replace-all" class="btn btn-primary">Replace All</button>');
-            replace_stuff_ = $('<span id="replace"></span>')
-                .append('&nbsp;Replace with: ', replace_input_, replace_all_);
-            find_form.append('Find: ', find_input_, replace_stuff_);
+            find_next_ = $('<button id="find-next" class="btn btn-primary">Find Next</button>');
+            replace_all_ = $('<button id="replace-all" class="btn">Replace All</button>');
+            replace_stuff_ = replace_desc_.add(replace_input_).add(replace_all_);
+            var close = $('<span id="close" style="float: right; cursor: pointer"><i class="icon-remove"></i></span>');
+            find_form.append('Find: ', find_input_, replace_desc_, replace_input_, find_next_, replace_all_, close);
             find_dialog_.append(find_form);
             $('#middle-column').prepend(find_dialog_);
 
             find_input_.on('input', function(val) {
+                currentCell_ = currentMatch_ = undefined;
                 highlight_all(find_input_.val());
             });
             replace_all_.click(function() {
@@ -4434,21 +4438,35 @@ RCloud.UI.find_replace = (function() {
             });
 
             nextFind_ = {
-                'find-input': find_input_
+                'find-input': find_next_,
+                'find-next': find_input_
             };
 
             nextReplace_ = {
                 'find-input': replace_input_,
-                'replace-input': replace_all_,
+                'replace-input': find_next_,
+                'find-next': replace_all_,
                 'replace-all': find_input_
             };
 
+            var click_find_next = function(e) {
+                if(e.keyCode===13) {
+                    find_next_.click();
+                    return false;
+                }
+                return undefined;
+            };
+            find_input_.keydown(click_find_next);
+            replace_input_.keydown(click_find_next);
+
             find_form.keydown(function(e) {
                 switch(e.keyCode) {
-                case 9:
+                case 9: // tab
                     (replace_mode_ ? nextReplace_ : nextFind_)[e.target.id].focus();
                     return false;
-                case 27:
+                case 13: // cr: don't do default anything
+                    return false;
+                case 27: // esc
                     hide_dialog();
                     return false;
                 }
@@ -4460,20 +4478,21 @@ RCloud.UI.find_replace = (function() {
                     this.select();
                 }.bind(this), 0);
             });
+
+            close.click(function() {
+                hide_dialog();
+            });
         }
-        if(shown_ && replace_mode_ === replace)
-            hide_dialog();
-        else {
-            find_dialog_.show();
-            find_input_.focus();
-            if(replace)
-                replace_stuff_.show();
-            else
-                replace_stuff_.hide();
-            highlight_all(find_input_.val());
-            shown_ = true;
-            replace_mode_ = replace;
-        }
+
+        find_dialog_.show();
+        find_input_.focus();
+        if(replace)
+            replace_stuff_.show();
+        else
+            replace_stuff_.hide();
+        highlight_all(find_input_.val());
+        shown_ = true;
+        replace_mode_ = replace;
     }
     function hide_dialog() {
         highlight_all(null);
@@ -4502,6 +4521,7 @@ RCloud.UI.find_replace = (function() {
             cell.notify_views(function(view) {
                 view.change_highlights(matches);
             });
+            matches_[cell.filename()] = matches;
         });
     }
     function replace_all(find, replace) {
