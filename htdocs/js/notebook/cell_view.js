748222e483b1f9b5199f43581caefb921d752109
Gordon Woodhull <gordon@woodhull.com>
2015-01-21 10:02:12 -0500

    put cell_processors in own file; debounce post-processors

--- htdocs/js/notebook/cell_view.js
+++ htdocs/js/notebook/cell_view.js
@@ -122,12 +122,15 @@ function create_cell_html_view(language, cell_model) {
         result_div_.html('<div class="non-result">' + status + '</div>');
     };
 
-    function result_updated() {
+    function call_postprocessors() {
         Notebook.Cell.postprocessors.entries('all').forEach(function(post) {
             post.process(result_div_);
         });
+    }
 
-        result.edit_source(false);
+    function result_updated() {
+        // postprocessing the dom is slow, so only do this when we have a break
+        _.debounce(call_postprocessors, 100);
     }
 
     function clear_result() {
@@ -449,100 +452,4 @@ Notebook.Cell.create_html_view = function(cell_model)
 {
     return create_cell_html_view(cell_model.language(), cell_model);
 };
-
-Notebook.Cell.preprocessors = RCloud.extension.create();
-Notebook.Cell.postprocessors = RCloud.extension.create();
-
-Notebook.Cell.postprocessors.add({
-    device_pixel_ratio: {
-        sort: 1000,
-        process: function(div) {
-            // we use the cached version of DPR instead of getting window.devicePixelRatio
-            // because it might have changed (by moving the user agent window across monitors)
-            // this might cause images that are higher-res than necessary or blurry.
-            // Since using window.devicePixelRatio might cause images
-            // that are too large or too small, the tradeoff is worth it.
-            var dpr = rcloud.display.get_device_pixel_ratio();
-            // fix image width so that retina displays are set correctly
-            div.find("img")
-                .each(function(i, img) {
-                    function update() { img.style.width = img.width / dpr; }
-                    if (img.width === 0) {
-                        $(img).on("load", update);
-                    } else {
-                        update();
-                    }
-                });
-        }
-    },
-    deferred_results: {
-        sort: 2000,
-        process: function(div) {
-            var uuid = rcloud.deferred_knitr_uuid;
-            div.find("span.deferred-result")
-                .each(function() {
-                    var that = this;
-                    var uuids = this.textContent.split("|");
-                    // FIXME monstrous hack: we rebuild the ocap from the string to
-                    // call it via rserve-js
-                    var ocap = [uuids[1]];
-                    ocap.r_attributes = { "class": "OCref" };
-                    var f = rclient._rserve.wrap_ocap(ocap);
-
-                    f(function(err, future) {
-                        var data;
-                        if (RCloud.is_exception(future)) {
-                            data = RCloud.exception_message(future);
-                            $(that).replaceWith(function() {
-                                return ui_utils.string_error(data);
-                            });
-                        } else {
-                            data = future();
-                            $(that).replaceWith(function() {
-                                return data;
-                            });
-                        }
-                    });
-                });
-        }
-    },
-    mathjax: {
-        sort: 3000,
-        process: function(div) {
-            // typeset the math
-            // MathJax is slow, so only do this when we have a break
-            _.debounce(function() {
-                // why does passing the div as last arg not work, as documented here?
-                // http://docs.mathjax.org/en/latest/typeset.html
-                if (!_.isUndefined(MathJax))
-                    MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
-            }, 100);
-        }
-    },
-    hide_source: {
-        sort: 4000,
-        process: function(div) {
-            // this is kinda bad
-            if (!shell.notebook.controller._r_source_visible) {
-                Notebook.hide_r_source(div);
-            }
-        }
-    }
-});
-
-Notebook.Cell.preprocessors.add({
-    quote_deferred_results: {
-        sort: 1000,
-        process: function(r) {
-            var deferred_result_uuid_, deferred_regexp_, deferred_replacement_;
-            if(!deferred_regexp_) {
-                deferred_result_uuid_ = rcloud.deferred_knitr_uuid;
-                deferred_regexp_ = new RegExp(deferred_result_uuid_ + '\\|[@a-zA-Z_0-9.]*', 'g');
-                deferred_replacement_ = '<span class="deferred-result">$&</span>';
-            }
-            return r.replace(deferred_regexp_, deferred_replacement_);
-        }
-    }
-});
-
 })();
