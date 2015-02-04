5b736c613e99affe4fb23ef3096a02d2879bc400
Sastry Isukapalli <sastry@research.att.com>
2015-02-04 09:38:04 -0500

    Changes to Python side to work with the new RCloud messaging architecture
    Main changes in the general RCloud world:
      added a .session parameter to rcloud.support.R tab-completion call.
    Changes in the rcloud.python side
      messages are sent as they arrive
      completer implemented (does object level completion, doesn't work with imports, etc.)
      notebook_runner code is cleaned up a bit

--- rcloud.packages/rcloud.python/inst/python/notebook_runner.py
+++ rcloud.packages/rcloud.python/inst/python/notebook_runner.py
@@ -4,25 +4,26 @@ Code base adopted from an older version of code from
 https://github.com/minrk and https://github.com/paulgb/runipy 
 Modified for running python code in RCloud with this piece as an intermediate
 proxy... Also, some of the formatting required to use it in RCloud is handled here.
+
+This will be replaced with an IPython client code
 """
 
 try:
-    # python 2
-    from Queue import Empty
+    from Queue import Empty  # python 2
 except:
-    # python 3
-    from queue import Empty
+    from queue import Empty  # python 3
 
 import platform
 from time import sleep
 import logging
 from RCloud_ansi2html import ansi2html # MIT license from github:Kronuz/ansi2html (Oct 2014) + rename/our changes
 from xml.sax.saxutils import escape as html_escape # based on reco from moin/EscapingHtml
+import re
 
 import tempfile
 debugFD, debugFile = "", ""
 
-_debugging = False
+_debugging = True
 if _debugging:
     debugFD, debugFile = tempfile.mkstemp(suffix=".log", prefix="ipy_log")
     logging.basicConfig(filename=debugFile, level=logging.DEBUG)
@@ -30,21 +31,23 @@ if _debugging:
 class NotebookError(Exception):
     pass
 
-_textFormatStr = u'<pre style="font-family:Consolas,Monaco,Lucida Console,Liberation Mono,DejaVu Sans Mono,Bitstream Vera Sans Mono,Courier New, monospace;">{}</pre>'
-def RClTextFormat(s, escape=True):
-    newtext = html_escape(s) if escape else s
-    return _textFormatStr.format(newtext)
+_textFormatStr = u'<span style="font-family:Consolas,Monaco,Lucida Console,Liberation Mono,DejaVu Sans Mono,Bitstream Vera Sans Mono,Courier New, monospace;">{}</span>'
 
-def RClansiconv(inputtext, full=False):
-    """ Uses Krounz/ansi2html (MIT) renamed to produce a reasonably minimal html"""
-    if _debugging: logging.info('Received ' + inputtext)
+def RClansiconv(inputtext, escape=True):
+    """ Uses Krounz/ansi2html (MIT) renamed to produce a reasonably minimal html."""
+    if _debugging: logging.debug('Received ' + inputtext)
+    txt = html_escape(inputtext) if escape else inputtext
+    txt = re.sub("\n", "<br>\n", txt)
+    txt = re.sub(" ", r"&nbsp;", txt)
     try:
-        txt = unicode(inputtext, 'utf-8') # we get unicode characters from IPython sometimes
+        rettxt = unicode(txt, 'utf-8') # we get unicode characters from IPython sometimes
     except:
-        txt = inputtext # if it is already unicode
-    htmlFragment = ansi2html(txt).encode('ascii', 'xmlcharrefreplace')
-    return _textFormatStr.format(htmlFragment, escape=False) # we already escaped the text
+        rettxt = txt # if it is already unicode
+    htmlFragment = ansi2html(rettxt).encode('ascii', 'xmlcharrefreplace')
+    return _textFormatStr.format(htmlFragment)
 
+from IPython import get_ipython
+from IPython.core import completer
 from IPython.nbformat.current import read, write, NotebookNode
 from IPython.kernel import KernelManager
 from IPython.kernel.blocking.channels import BlockingShellChannel
@@ -68,6 +71,7 @@ class MagicBlockingShellChannel(BlockingShellChannel):
         'shutdown',
         'magic', # this is the new proxy_method for the channel.
     ]
+        
     def magic(self, magic_str):
         """Run a magic in the kernel."""
         # Create class for content/msg creation. Related to, but possibly
@@ -100,25 +104,24 @@ class NotebookRunner(object):
         self.km = KernelManager()
         self.km.kernel_cmd = make_ipkernel_cmd("""
 import sys; 
+import time; # to test for slow staring kernels, add a delay here
 sys.path.extend("{RCPATH}".split(":")); 
 from rcloud_kernel import main; 
 main()""".format(RCPATH=kw["rcloud_python_lib_path"]), **kw)
         del kw["rcloud_python_lib_path"]
         self.km.client_factory = MyClient
-        self.km.start_kernel(**kw)
-
-        if platform.system() == 'Darwin':
-            # There is sometimes a race condition where the first
-            # execute command hits the kernel before it's ready.
-            # It appears to happen only on Darwin (Mac OS) and an
-            # easy (but clumsy) way to mitigate it is to sleep
-            # for a second.
-            sleep(1)
-
+        self.km.start_kernel(**kw) # This is a non-blocking call to launch the process
+        self.completer = completer.Completer()
+        self.completer.limit_to__all__ = True
+        # There is a possible race condition if the system is slow to load
+        # the modules for the kernel and we issue commands to the kernel rightaway.
+        # We saw these on CentOS and OSX. So, issue an empty command to the kernel and wait for return
         self.kc = self.km.client()
         self.kc.start_channels()
 
         self.shell = self.kc.shell_channel
+        self.shell.execute("")
+        _ = self.shell.get_msgs()
         self.iopub = self.kc.iopub_channel
 
     def __del__(self):
@@ -129,132 +132,70 @@ main()""".format(RCPATH=kw["rcloud_python_lib_path"]), **kw)
         self.kc.stop_channels()
         self.km.shutdown_kernel(now=True)
 
+    def complete(self, text, pos_fromR):
+        """Completions for text at pos"""
+        txtArr = text[:int(pos_fromR)].split('\n')
+        # We are ignoring surrounding lines and full context; a poor-man's tab-complete for now
+        _ = allres = self.shell.get_msgs()  # flush before execution
+        self.shell.complete(txtArr[-1], line="", cursor_pos=len(txtArr[-1])-1)
+        while True:
+            resp = self.shell.get_msg()
+            if _debugging: logging.debug(resp)
+            if resp['msg_type'] == 'complete_reply':
+                _ = self.shell.get_msgs() # flush
+                return resp['content']['matches']
+
     def run_cmd(self, cmd):
-        d = {}
-        class Cell(object):
-            def __init__(self):
-                self.input = cmd
-            def __setitem__(self, k, v):
-                d[k] = v
-        self.run_cell(Cell())
-        return d['outputs']
+        self.submit_cell(cmd)
 
     def run_magic(self, magic_line):
         self.shell.magic(magic_line)
 
-    def wait_for_msg(self):
-        outs = list()
-        reply = self.shell.get_msg()
-        status = reply['content']['status']
-        if status == 'error':
-            logging.info('Cell raised uncaught exception: \n%s', '\n'.join(reply['content']['traceback']))
+    def submit_cell(self, cmd):
+        """Submits the cell code to the kernel.
+        Now that we have a continuous IO on rcloud, and the clients can send oob messages to
+        RCloud [but only through R], we separate the pieces to submit and have a function
+        for looping to get messages"""
+        if _debugging: logging.debug('Running cell:\n%s\n', cmd)
+        _ = self.iopub.flush()
+        self.shell.execute(cmd)
+        self._previous_status = 'START'
+
+    def poll_for_msgs(self):
+        """Polls for messages from the kernel.
+        Used after submitting code for execution"""
+        try:
+            msg = self.iopub.get_msg(timeout=1)
+            if msg['msg_type'] == 'status' and msg['content']['execution_state'] == 'idle':
+                if _debugging: logging.info('Message -- {}:{}'.format(msg['msg_type'], msg['content']))
+                self._previous_status = 'IDLE'
+                return NotebookNode(output_type = 'IDLE')
+        except Empty: # state should return to idle before queue becomes empty, but we ignore it now
+            prevstat, self._previous_status = self._previous_status, 'EMPTY'
+            retstat = 'END_CELL' if prevstat == 'IDLE' else 'EMPTY'
+            # Assuming IDLE followed by EMPTY is the end-of-cell 
+            return NotebookNode(output_type = retstat)
+
+        self._previous_status = ''  # Not idle, that's all we are concerned about for now
+        content, msg_type = msg['content'], msg['msg_type']
+
+        if msg_type in ['status', 'pyin']: return NotebookNode(output_type = 'NoOp')
+
+        out = NotebookNode(output_type = msg_type)
+        if msg_type in ('display_data', 'pyout'):
+            for mime, data in content['data'].items():
+                try:
+                    attr = self.MIME_MAP[mime]
+                    tmpval =  RClansiconv(data) if attr == 'text' else data
+                    setattr(out, attr, tmpval)
+                except KeyError:
+                    raise NotImplementedError('unhandled mime type: %s' % mime)
+        elif msg_type == 'stream':
+            setattr(out, 'text', RClansiconv(content['data']))
+        elif msg_type == 'pyerr':
+            setattr(out, 'html', RClansiconv('\n'.join(content['traceback']) + '\n'))
         else:
-            if _debugging: logging.debug('Cell returned')
-
-        while True:
-            try:
-                msg = self.iopub.get_msg(timeout=1)
-                if msg['msg_type'] == 'status':
-                    if msg['content']['execution_state'] == 'idle':
-                        break
-            except Empty:
-                # execution state should return to idle before the queue becomes empty,
-                # if it doesn't, something bad has happened
-                raise
-
-            outs.append(msg)
-        return outs
-
-    def run_cell(self, cell):
-        '''
-        Run a notebook cell and update the output of that cell in-place.
-        '''
-        if _debugging: logging.debug('Running cell:\n%s\n', cell.input)
-        self.shell.execute(cell.input)
-        reply = self.shell.get_msg()
-        status = reply['content']['status']
-        if status == 'error':
-            logging.info('Cell raised uncaught exception: \n%s', '\n'.join(reply['content']['traceback']))
-        else:
-            if _debugging: logging.debug('Cell returned')
-
-        outs = list()
-        while True:
-            try:
-                msg = self.iopub.get_msg(timeout=1)
-                if msg['msg_type'] == 'status':
-                    if msg['content']['execution_state'] == 'idle':
-                        break
-            except Empty:
-                # execution state should return to idle before the queue becomes empty,
-                # if it doesn't, something bad has happened
-                # logging.warn('Execution state did not return to idle')
-                pass # raise
-
-            content = msg['content']
-            msg_type = msg['msg_type']
-
-            out = NotebookNode(output_type=msg_type)
-
-            if 'execution_count' in content:
-                cell['prompt_number'] = content['execution_count']
-                out.prompt_number = content['execution_count']
-
-            if msg_type in ['status', 'pyin']:
-                continue
-            elif msg_type == 'stream':
-                out.stream = content['name']
-                out.html = RClansiconv(content['data'])
-            elif msg_type in ('display_data', 'pyout'):
-                for mime, data in content['data'].items():
-                    try:
-                        attr = self.MIME_MAP[mime]
-                    except KeyError:
-                        logging.warning('unhandled mime type: %s' % mime)
-                        raise NotImplementedError('unhandled mime type: %s' % mime)
-                    if attr == 'text':
-                        if _debugging: logging.info(data)
-                        setattr(out, attr, RClansiconv(data))
-                    else:
-                        setattr(out, attr, data)
-            elif msg_type == 'pyerr':
-                logging.info('Received an exception: ' + content['ename'])
-                out.ename = content['ename']
-                out.evalue = content['evalue']
-                out.traceback = content['traceback']
-                out.html = RClansiconv('\n'.join(out.traceback))
-            else:
-                raise NotImplementedError('unhandled iopub message: %s' % msg_type)
-            outs.append(out)
-        cell['outputs'] = outs
-
-    def iter_code_cells(self, nb):
-        '''
-        Iterate over the notebook cells containing code.
-        '''
-        nb = read(open(nb), 'json')
-        for ws in nb.worksheets:
-            for cell in ws.cells:
-                if cell.cell_type == 'code':
-                    yield cell
-
-
-    def run_notebook(self, nb, skip_exceptions=False):
-        '''
-        Run all the cells of a notebook in order and update
-        the outputs in-place.
-
-        If ``skip_exceptions`` is set, then if exceptions occur in a cell, the
-        subsequent cells are run (by default, the notebook execution stops).
-        '''
-        for cell in self.iter_code_cells(nb):
-            try:
-                self.run_cell(cell)
-                print cell.outputs
-            except NotebookError:
-                if not skip_exceptions:
-                    raise
-
-    def save_notebook(self, nb_out):
-        if _debugging: logging.info('Saving to %s', nb_out)
-        write(self.nb, open(nb_out, 'w'), 'json')
+            if _debugging: logging.info('Unsupported: ' + msg_type)
+            raise NotImplementedError('unhandled iopub message: %s' % msg_type)
+        if _debugging: logging.info('Sending: msg_type: [{}]; HTML: [{}]; TEXT: [{}]'.format(msg_type, out.get('html', ''), out.get('text', '') ))
+        return out # upstream process will handle it [e.g. send as an oob message]
