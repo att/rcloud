719b5703fb5f654c7abd4554bd17395af4634597
Carlos Scheidegger <cscheid@cs.arizona.edu>
2015-02-05 22:19:07 -0700

    closes issue #1254

--- htdocs/js/notebook/cell_view.js
+++ htdocs/js/notebook/cell_view.js
@@ -60,8 +60,10 @@ function create_cell_html_view(language, cell_model) {
     cell_status_.append(cell_status_left);
     left_controls_ = RCloud.UI.cell_commands.decorate('left', cell_status_left, cell_model, result);
 
+    var cell_control_bar = $("<div class='cell-control-bar'></div>");
+    var cell_commands_above = $("<div class='cell-controls-above nonselectable'></div>");
+
     if(!shell.is_view_mode()) {
-        var cell_control_bar = $("<div class='cell-control-bar'></div>");
         cell_status_.append(cell_control_bar);
         // disable sort action on the control bar area
         cell_control_bar.mousedown(function(e) {
@@ -69,7 +71,6 @@ function create_cell_html_view(language, cell_model) {
         });
         cell_controls_ = RCloud.UI.cell_commands.decorate('cell', cell_control_bar, cell_model, result);
 
-        var cell_commands_above = $("<div class='cell-controls-above nonselectable'></div>");
         above_between_controls_ = RCloud.UI.cell_commands.decorate('above_between', cell_commands_above, cell_model, result);
         notebook_cell_div.append(cell_commands_above);
     }
