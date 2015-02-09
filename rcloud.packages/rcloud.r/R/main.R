1744dae4c46ec16c68cb34d12f22ca1d59bd7275
Gordon Woodhull <gordon@woodhull.com>
2015-02-08 19:22:55 -0500

    supply global environment for .eval
    fixes #1267

--- rcloud.packages/rcloud.r/R/main.R
+++ rcloud.packages/rcloud.r/R/main.R
@@ -1,14 +1,14 @@
 rcloud.language.support <- function()
 {
-    ## a bit ugly, but just to keep the formatting in one place
-    .eval <- rcloud.support:::.eval
-    
+  ## a bit ugly, but just to keep the formatting in one place
+  .eval <- rcloud.support:::.eval
+
   ev <- function(command, silent, rcloud.session) {
     .session <- rcloud.session
     # .session$device.pixel.ratio
     exp <- tryCatch(parse(text=command), error=function(o) structure(list(error=o$message), class="parse-error"))
     # ulog(".EXP: ", paste(capture.output(str(exp)), collapse='\n'))
-    res <- if (!inherits(exp, "parse-error")) .eval(exp, FALSE) else exp
+    res <- if (!inherits(exp, "parse-error")) .eval(exp, FALSE, .GlobalEnv) else exp
     ## FIXME: in principle this should move from rcloud.support to rcloud.R
     rcloud.support:::.post.eval()
     res
