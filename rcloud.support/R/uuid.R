6e1f19622b57a0c95f4faec1a18f21e583f8f392
Simon Urbanek <simon.urbanek@r-project.org>
2015-02-13 20:40:13 -0500

    create an actual 'deferred_result' class with a print method such that we control how to output deferred results (fixes #1317 and #1233)

--- rcloud.support/R/uuid.R
+++ rcloud.support/R/uuid.R
@@ -7,8 +7,17 @@ deferred.rcloud.result <- function(value) {
   uuid <- make.oc(value)
   if (is.null(.session$result.prefix.uuid))
     .session$result.prefix.uuid <- generate.uuid()
-  paste(.session$result.prefix.uuid, uuid, sep="|")
+  structure(paste(.session$result.prefix.uuid, uuid, sep="|"), class="deferred_result")
 }
 
 rcloud.prefix.uuid <- function()
   if (is.null(.session$result.prefix.uuid)) .session$result.prefix.uuid <- generate.uuid() else .session$result.prefix.uuid
+
+print.deferred_result <- function(x, ...) {
+    ## this is a horribe, horrible hack - we try to detect
+    ## if "knit" is anywhere on the call stack to detect
+    ## knitr processing where we cannot use OOB but have to
+    ## use a string result
+    if (any(sapply(sys.calls(), function(o) identical(o[[1]], quote(knit))))) return(cat(unclass(x), "\n", sep=''))
+    flush.console(); self.oobSend(list("html.out",unclass(x)))
+}
