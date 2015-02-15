0f266ffe8f4fbdfdb06db28ffd8cdc33aebd1188
Gordon Woodhull <gordon@woodhull.com>
2015-02-15 06:11:51 -0500

    remove dead code
    old bulky way of trying to update the active highlight
    may have been more efficient but less general
    and generally a pain

--- htdocs/js/rcloud_bundle.js
+++ htdocs/js/rcloud_bundle.js
@@ -1673,7 +1673,7 @@ function create_cell_html_view(language, cell_model) {
     var cell_status_;
     var above_between_controls_, cell_controls_, left_controls_;
     var edit_mode_; // note: starts neither true nor false
-    var highlights_, active_highlight_ = null;
+    var highlights_;
     var code_preprocessors_ = []; // will be an extension point, someday
 
     // input1
@@ -2181,8 +2181,7 @@ function create_cell_html_view(language, cell_model) {
                 outer_ace_div.hide();
             }
             edit_mode_ = edit_mode;
-            this.change_highlights(highlights_)
-                .change_active_highlight(active_highlight_); // restore highlights
+            this.change_highlights(highlights_); // restore highlights
         },
         hide_source: function(whether) {
             if(whether)
@@ -2262,47 +2261,6 @@ function create_cell_html_view(language, cell_model) {
             }
             else {
                 assign_code();
-            }
-            return this;
-        },
-        change_active_highlight: function(range) {
-            active_highlight_ = range;
-            if(edit_mode_) {
-                var markers = ace_session_.getMarkers();
-                var id, ace_range;
-                if(range) {
-                    ace_range = ui_utils.ace_range_of_character_range(ace_widget_, range.begin, range.end);
-                    for(id in markers)
-                        if(markers[id].type === 'rcloud-select' &&
-                           markers[id].range.isEqual(ace_range)) {
-                            markers[id].clazz = 'find-highlight active';
-                            ace_session_._signal("changeBackMarker");
-                        }
-                }
-                else for(id in markers) {
-                    if(markers[id].type === 'rcloud-select' &&
-                       markers[id].clazz.split(' ').indexOf('active') >= 0) {
-                        markers[id].clazz = 'find-highlight';
-                        ace_session_._signal("changeBackMarker");
-                    }
-                }
-            }
-            else {
-                if(!range)
-                    code_div_.find('.find-highlight.active').toggleClass('active', false);
-                else {
-                    // http://stackoverflow.com/questions/7969031/indexof-element-in-js-array-using-a-truth-function-using-underscore-or-jquery
-                    function find(collection, filter) {
-                        for (var i = 0; i < collection.length; i++) {
-                            if(filter(collection[i], i, collection)) return i;
-                        }
-                        return -1;
-                    }
-                    var i = find(highlights_, function(r) { return r.begin === range.begin && r.end === range.end; });
-                    if(i<0)
-                        throw new Error('unknown find result ' + JSON.stringify(range));
-                    code_div_.find('.find-highlight').eq(i).toggleClass('active', true);
-                }
             }
             return this;
         }
