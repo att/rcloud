7bf96872090e89a81e54f6783578a8d83b67656e
Gordon Woodhull <gordon@woodhull.com>
2015-06-09 15:10:27 -0400

    add rambo mode to shutdown.sh
    probably OSX-specific, I don't care

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
+KILL=0
+while [ "$1" != "" ]; do
+    case $1 in
+        --kill) KILL=1 ;;
+        --help) cat <<EOF
+
+ Usage: $0 [{--kill}]
+
+ Shuts down the server, nicely by default.
+
+ --kill : with extreme prejudice
+
+EOF
+        exit 0 ;;
+        *) echo "unknown option" $1 && exit 1 ;;
+    esac
+    shift
+done
 
 sudo_cmd=''
 ## check if user switching is enabled - in that case we have to sudo            
@@ -14,4 +32,9 @@ if [ -n "$pid" ]; then
     ## FIXME: should we check that it's the right process?
     echo " - shutdown RCloud server $pid"
     ${sudo_cmd} kill -INT "$pid"
+fi
+
+if [ $KILL -gt 0 ]; then
+    ## FIXME: this is probably OSX - specific
+    sudo killall RsrvCHx; sudo killall RsrvSRV
 fi
