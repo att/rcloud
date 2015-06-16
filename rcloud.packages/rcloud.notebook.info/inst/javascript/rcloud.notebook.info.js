8539a4a9539e9bf709a9aa2bb5a80041fab346b3
mj12inc <nu.way.productions@gmail.com>
2015-06-16 16:27:59 -0400

    fixed the notebook info popups

--- rcloud.packages/rcloud.notebook.info/inst/javascript/rcloud.notebook.info.js
+++ rcloud.packages/rcloud.notebook.info/inst/javascript/rcloud.notebook.info.js
@@ -1,8 +1,6 @@
 ((function() {
     return {
 
-
-
         init: function(k) {
 
             RCloud.UI.notebook_commands.add({
@@ -11,64 +9,87 @@
                     sort: 1000,
                     create: function(node) {
                         var info = ui_utils.fa_button('icon-info-sign', 'notebook info', 'info',
-                                                      RCloud.UI.notebook_commands.icon_style(), false);
+                                                    RCloud.UI.notebook_commands.icon_style(), false);
+                        var popupOpen = false;
+
                         info.click(function(e) {
                             e.preventDefault();
                             e.stopPropagation();
                             var thisIcon = this;
                             var info_content = '';
 
+                            $(document).on('destroy_all_popovers', function(){
+                                 $(info).popover('destroy');
+                                 popupOpen = false;
+                            })
+
                             Promise.all( [window.rcloud.protection.get_notebook_cryptgroup(node.gistname), 
                                         window.rcloud.stars.get_notebook_starrer_list(node.gistname)])
                             .spread(function(cryptogroup, list) {
+                                
+                                info_content =  buildPopover();
+                                function buildPopover(){
+                                    if(typeof(list) === 'string')
+                                        list = [list];
 
-                                if(typeof(list) === 'string')
-                                    list = [list];
+                                    var close_button = '<span class="pop-close" style="cursor: pointer; float:right;">x</span>';
+                                    var group_message = '<div class="info-category"><b>Group:</b></div>';
+                                    if(!cryptogroup[0] && !cryptogroup[1]){
+                                        group_message += '<div class="group-link info-item"><a href="#">no group</a></div>'
+                                    }
+                                    else{
+                                        group_message += '<div class="group-link info-item"><a href="#">'+cryptogroup[1]+'</a></div>'
+                                    }
+                                    var starrer_list = '<div class="info-category"><b>Starred by:</b></div>';
+                                    list.forEach(function (v) {
+                                        starrer_list = starrer_list + '<div class="info-item">' + v + '</div>';
+                                    });
+                                    return group_message + info_content + starrer_list;
+                                }
 
-                                var close_button = '<span class="pop-close" style="cursor: pointer; float:right;">x</span>';
-                                var group_message = '<div class="info-category"><b>Group:</b></div>';
-                                if(!cryptogroup[0] && !cryptogroup[1]){
-                                    group_message += '<div class="group-link info-item"><a href="#">no group</a></div>'
+
+                                $('html').off('mouseup');
+                                $('html').on('mouseup', function(e) {
+                                    if(!$(e.target).closest('.popover').length) {
+                                        // $('.popover').each(function(){
+                                        //     $(this).popover('destroy');
+                                        // });
+                                        $(document).trigger('destroy_all_popovers');
+                                    }
+                                });
+
+                                if(!popupOpen){
+                                    $(document).trigger('destroy_all_popovers');
+
+                                    $(info).popover({
+                                        title: node.name,
+                                        html: true,
+                                        content: info_content,
+                                        container: 'body',
+                                        placement: 'right',
+                                        animate: false,
+                                        delay: {hide: 0}
+                                    });
+
+                                    $(info).popover('show');
+                                    var thisPopover = $(info).popover().data()['bs.popover'].$tip[0];
+                                    thisPopover = $(thisPopover);
+                                    thisPopover.addClass('popover-offset notebook-info');
+
+                                    popupOpen = true;
                                 }
                                 else{
-                                    group_message += '<div class="group-link info-item"><a href="#">'+cryptogroup[1]+'</a></div>'
+                                    $(document).trigger('destroy_all_popovers');
                                 }
-
-                                var starrer_list = '<div class="info-category"><b>Starred by:</b></div>';
-                                list.forEach(function (v) {
-                                    starrer_list = starrer_list + '<div class="info-item">' + v + '</div>';
-                                });
-                                info_content = close_button + group_message + info_content + starrer_list;
-                                $(thisIcon).popover({
-                                    title: node.name,
-                                    html: true,
-                                    content: info_content,
-                                    container: 'body',
-                                    placement: 'right',
-                                    animate: false,
-                                    delay: {hide: 0}
-                                });
-                                $(thisIcon).popover('show');
-                                var thisPopover = $(thisIcon).popover().data()['bs.popover'].$tip[0];
-                                thisPopover = $(thisPopover);
-                                thisPopover.addClass('popover-offset notebook-info');
-                                
-
-
-                                $('.pop-close', thisPopover).click(function() {
-                                    $(thisIcon).popover("destroy");
-                                });
 
                                 $('.group-link', thisPopover).click(function(){
                                     $(thisIcon).popover("destroy");
-
                                     //set 
                                     RCloud.UI.notebook_protection.notebookFullName = node.full_name;
                                     RCloud.UI.notebook_protection.notebookGistName = node.gistname;
                                     RCloud.UI.notebook_protection.notebookId = node.id;
                                     RCloud.UI.notebook_protection.tipEl = $(thisIcon).closest('.group-link');
                                     //console.dir(RCloud.UI.notebook_protection.tipEl);
-
                                     //groups
 
                                     if(!cryptogroup[0] && !cryptogroup[1]){
@@ -84,8 +105,6 @@
                                     //show modal
                                     RCloud.UI.notebook_protection.init();
                                     //RCloud.UI.notebook_protection.showOverlay();
-
-                            
                                 });
 
                             });
