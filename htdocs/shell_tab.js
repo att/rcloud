a5fd3504bad6160aabe86f38d03100ded3451964
Gordon Woodhull <gordon@woodhull.com>
2015-08-14 11:15:45 -0400

    do not refresh notebook title when no notebook

--- htdocs/shell_tab.js
+++ htdocs/shell_tab.js
@@ -92,7 +92,8 @@ var shell = (function() {
             });
         },
         refresh_notebook_title: function() {
-            RCloud.UI.notebook_title.set(notebook_controller_.current_gist().description);
+            if(notebook_controller_.current_gist())
+                RCloud.UI.notebook_title.set(notebook_controller_.current_gist().description);
         },
         is_view_mode: function(val) {
             if(val !== undefined) {
