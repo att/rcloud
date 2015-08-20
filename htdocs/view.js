22ed99188a499533f66741282a6824a01f2ed496
Gordon Woodhull <gordon@woodhull.com>
2015-08-20 11:53:58 -0400

    interpret errors in view mode
    use shell.improve_load_error
    only replace the message with "may need to login" when it matches
    fixes #1638

--- htdocs/view.js
+++ htdocs/view.js
@@ -8,6 +8,7 @@ function main() {
     shell.is_view_mode(true);
     RCloud.UI.session_pane.init(); // really should be error logger which detects if there is a pane
     RCloud.UI.init();
+    var notebook, version;
     RCloud.session.init(true).then(function() {
         return Promise.all([
             RCloud.UI.navbar.load(),
@@ -22,9 +23,9 @@ function main() {
         shell.init();
         RCloud.UI.advanced_menu.init();
         RCloud.UI.menus.load();
-        var notebook = getURLParameter("notebook"),
-            version = getURLParameter("version"),
-            quiet = getURLParameter("quiet");
+        notebook = getURLParameter("notebook");
+        version = getURLParameter("version");
+        var quiet = getURLParameter("quiet");
 
         var promise = Promise.resolve(true);
         if (Number(quiet)) {
@@ -71,6 +72,10 @@ function main() {
         return promise;
     }).catch(function(err) {
         console.log(err.stack);
-        RCloud.UI.session_pane.post_error(ui_utils.disconnection_error("Could not load notebook. You may need to login to see it.", "Login"));
+        shell.improve_load_error(err, notebook, version).then(function(msg) {
+            if(/Notebook does not exist or has not been published/.test(msg))
+                msg = ui_utils.disconnection_error("Could not load notebook. You may need to login to see it.", "Login");
+            RCloud.UI.session_pane.post_error(msg);
+        });
     });
 }
