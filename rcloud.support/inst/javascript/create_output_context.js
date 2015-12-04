d0f0a6fafaa5c99e3321cce2f8e274f7d8ea24ab
Gordon Woodhull <gordon@woodhull.com>
2015-12-04 13:53:00 -0500

    fix name

--- rcloud.support/inst/javascript/create_output_context.js
+++ rcloud.support/inst/javascript/create_output_context.js
@@ -3,7 +3,7 @@
         k(RCloud.UI.output_context.create(selector));
     },
     close_context: function(context_id) {
-        RCloud.UI.output_context.close(id);
+        RCloud.UI.output_context.close(context_id);
         k();
     }
 })
