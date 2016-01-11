f28cf09fc29d322a77a098a5008e4898c88febc1
prateek032 <naik.prateek@outlook.com>
2016-01-11 19:33:47 +0530

    RCLoud Smoke test with Travis CI

---
+++ tests/Run.sh
@@ -0,0 +1,32 @@
+#Session Key Server Setup
+cd services
+git clone https://github.com/s-u/SessionKeyServer.git
+cd SessionKeyServer
+sudo make
+cd /home/travis/build/att/rcloud/services
+
+sudo sed -i -e '2iROOT=/home/travis/build/att/rcloud\' rcloud-sks
+sudo sh rcloud-sks &
+
+Rscript -e 'chooseCRANmirror(ind=81)'
+Rscript -e 'install.packages("XML", repos=c("http://RForge.net", "http://R.research.att.com"), type="source")'
+Rscript -e 'install.packages("rcloud.dcplot", repos="http://rforge.net")'
+Rscript -e 'install.packages("rpython2", repos="http://rforge.net")'
+
+cd /home/travis/build/att/rcloud/tests
+#sudo apt-get install xvfb
+pwd
+echo "Executing testscripts from $1"
+sudo xvfb-run -a casperjs test --ssl-protocol=any --engine=slimerjs $1 --username=iPrateek032 --password=musigma12 --url=http://127.0.0.1:8080/login.R --xunit=Reports/report.xml
+Rscript parse.R
+
+if [ $? -eq 0 ]
+then
+  echo "Build Pass"
+else
+  echo "Build Fail"
+exit 1
+fi
+
+
+
