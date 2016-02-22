6be31f9be7e175f12a1b6e916bd0d2a559b54fe3
Gordon Woodhull <gordon@woodhull.com>
2016-02-22 16:05:07 -0500

    move solr stuff and rcloud.search to search.R

--- rcloud.support/R/notebook.comments.R
+++ rcloud.support/R/notebook.comments.R
@@ -7,21 +7,6 @@ rcloud.get.comments <- function(id, source = NULL) {
         list()
 }
 
-.solr.post.comment <- function(id, content, comment.id) {
-  
-  ## query ID to see if it has existing comments
-  query <- list(q=paste0("id:",id),start=0,rows=1000)
-  solr.res <- .solr.get(query=query)
-  comment.content <- fromJSON(content)
-  
-  ## pick set/add depending on the exsitng content
-  method <- if(is.null(solr.res$response$docs[[1]]$comments)) "set" else "add"
-
-  ## send the update request
-  metadata <- paste0('{"id":"', id, '","comments":{"', method, '":"', paste(comment.id,':::',comment.content,':::',.session$username), '"}}')
- .solr.post(data=metadata)
-}
-
 rcloud.post.comment <- function(id, content)
 {
   res <- create.gist.comment(id, content, ctx = .rcloud.get.gist.context())
@@ -30,30 +15,12 @@ rcloud.post.comment <- function(id, content)
   res
 }
 
-.solr.modify.comment <- function(id, content, cid) {
-  query <- list(q=paste0("id:",id),start=0,rows=1000)
-  solr.res <- .solr.get(query=query)
-  index <- grep(cid, solr.res$response$docs[[1]]$comments)
-  solr.res$response$docs[[1]]$comments[[index]] <- paste(cid, fromJSON(content)$body, sep=' : ')
-  metadata <- paste0('{"id":"',id,'","comments":{"set":[\"',paste(solr.res$response$docs[[1]]$comments, collapse="\",\""),'\"]}}')
-  .solr.post(data=metadata)
-}
-
 rcloud.modify.comment <- function(id, cid, content)
 {
   res <- modify.gist.comment(id,cid,content, ctx = .rcloud.get.gist.context())
   rcloud.comments.email(id, content, ' modified an old')
   mcparallel(.solr.modify.comment(id, content, cid), detached=TRUE)
   res$ok
-}
-
-.solr.delete.comment <- function(id, cid) {
-  query <- list(q=paste0("id:",id),start=0,rows=1000)
-  solr.res <- .solr.get(query=query)
-  index <- grep(cid, solr.res$response$docs[[1]]$comments)
-  solr.res$response$docs[[1]]$comments <- solr.res$response$docs[[1]]$comments[-index]
-  metadata <- paste0('{"id":"',id,'","comments":{"set":[\"',paste(solr.res$response$docs[[1]]$comments, collapse="\",\""),'\"]}}')
-  .solr.post(data=metadata)
 }
 
 rcloud.delete.comment <- function(id,cid)
@@ -87,9 +54,9 @@ rcloud.comments.email <- function(id, content, type) {
   }
 }
 
-rcloud.create.email <- function(content) {
+Rcloud.create.email <- function(content) {
   url <- .session$url
-  email.content <- paste0('<html><head><title>Comment Notification</title></head><body><div><h3>Comment :</h3><p>"', 
+  email.content <- paste0('<html><head><title>Comment Notification</title></head><body><div><h3>Comment :</h3><p>"',
     content,'"</p><p><a href=\'', url, '\' target=\'', url, '\'>Go To Notebook</a></p></div></body></html>')
   email.content
 }
