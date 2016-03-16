dad725c52a599249fa80e7bb07647071d4bc2efe
shane <shane@freshclickmedia.com>
2016-03-16 18:31:58 +0000

    removed loop for secondary notebook processing

--- htdocs/js/ui/discovery_page.js
+++ htdocs/js/ui/discovery_page.js
@@ -13,47 +13,38 @@ RCloud.UI.discovery_page = (function() {
 
                   rcloud.config.get_recent_notebooks().then(function(data){
 
-                      var sorted = _.chain(data)
+                      var recent_notebooks = _.chain(data)
                       .pairs()
                       .filter(function(kv) {
                           return kv[0] != 'r_attributes' && kv[0] != 'r_type' && !_.isEmpty(editor.get_notebook_info(kv[0])) ;
                       })
                       .map(function(kv) { return [kv[0], Date.parse(kv[1])]; })
                       .sortBy(function(kv) { return kv[1] * -1; })
+                      .first(20)
+                      .map(function(notebook) {
+                        var current = editor.get_notebook_info(notebook[0]);
+                        return {
+                          id: notebook[0],
+                          time: notebook[1],
+                          description: current.description,
+                          last_commit: new Date(current.last_commit).toDateString(),
+                          username: current.username,
+                          num_stars: editor.num_stars(current[0]),
+                          image_url: 'notebook.R/' + notebook[0] + '/thumb.png'
+                        }
+                      })
                       .value();
 
-                      //store in a temporary array
-                      var recentTemp = [];
-
-                      sorted = sorted.slice(0, 20); //limit to 20 entries
-
-                      for(var i = 0; i < sorted.length; i ++) {
-
-                          var currItem = sorted[i];
-                          var currentNotebook = editor.get_notebook_info(sorted[i][0]);
-
-                          var d = {
-                              id: currItem[0],
-                              time: currItem[1],
-                              description: currentNotebook.description,
-                              last_commit: new Date(currentNotebook.last_commit).toDateString(),
-                              username: currentNotebook.username,
-                              num_stars: editor.num_stars(sorted[i][0]),
-                              image_url: '/notebook.R/' + currItem[0] + '/thumb.png'
-                          };
-                          recentTemp.push(d);
-                      }
-
                       // Grab the HTML out of our template tag and pre-compile it.
-                      _.templateSettings.variable = 'notebooks';
+                      //_.templateSettings.variable = 'notebooks';
 
                       var template = _.template(
                           $("#item_template").html()
                       );
 
-                      $('.grid').html(template(recentTemp));
-
-                      $('.grid').imagesLoaded()
+                      $('.grid').html(template({
+                        notebooks: recent_notebooks
+                      })).imagesLoaded()
                         .always(function() {
 
                           new Masonry( '.grid', {
