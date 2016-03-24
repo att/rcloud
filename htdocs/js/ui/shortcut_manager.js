25eae15afde39fe6e8457f9a6ff898549af8c678
shane <shane@freshclickmedia.com>
2016-03-24 15:50:31 +0000

    fix for #1864, #1874

--- htdocs/js/ui/shortcut_manager.js
+++ htdocs/js/ui/shortcut_manager.js
@@ -1,7 +1,10 @@
 RCloud.UI.shortcut_manager = (function() {
 
-    var extension_, 
-        shortcuts_changed = false;
+    var extension_;
+
+    function is_active(shortcut) {
+        return _.contains(shortcut.modes, shell.notebook.model.read_only() ? 'readonly' : 'writeable');
+    }
 
     function convert_extension(shortcuts) {
         var shortcuts_to_add, obj = {};
@@ -90,8 +93,14 @@ RCloud.UI.shortcut_manager = (function() {
                         shortcut_to_add.create = function() { 
                             _.each(shortcut_to_add.key_bindings, function(binding) {
                                 window.Mousetrap(document.querySelector('body')).bind(binding, function(e) { 
-                                    e.preventDefault(); 
-                                    shortcut.action();
+
+                                    if(!is_active(shortcut_to_add)) {
+                                        return;
+                                    } else {
+                                        e.preventDefault(); 
+                                        shortcut.action();
+                                    }
+
                                 });
                             });
                         }
@@ -138,7 +147,6 @@ RCloud.UI.shortcut_manager = (function() {
         add: function(s) {
             if(extension_) {
                 extension_.add(convert_extension(s));
-                shortcuts_changed = true;
             }
 
             return this;
@@ -148,18 +156,14 @@ RCloud.UI.shortcut_manager = (function() {
                 extension_.create('all');
             }
         },
-        shortcuts_changed: function() {
-            return shortcuts_changed;
-        },
         get_registered_shortcuts_by_category: function(sort_items) {
-            shortcuts_changed = false;
-
-            console.log();
 
             var rank = _.map(sort_items, (function(item, index) { return { key: item, value: index + 1 }}));
             rank = _.object(_.pluck(rank, 'key'), _.pluck(rank, 'value'));   
   
-            return _.sortBy(_.map(_.chain(extension_.sections.all.entries).groupBy('category').value(), function(item, key) {
+            var available_shortcuts = _.filter(extension_.sections.all.entries, function(s) { return is_active(s); });
+
+            return _.sortBy(_.map(_.chain(available_shortcuts).groupBy('category').value(), function(item, key) {
                 return { category: key, shortcuts: item }
             }), function(group) {
                 return rank[group.category];
