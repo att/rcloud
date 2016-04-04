5b7fa16e8daacf728883c46444f91242caa3951d
Gordon Woodhull <gordon@woodhull.com>
2016-04-04 16:18:43 -0400

    artifacts

--- htdocs/js/rcloud_bundle.js
+++ htdocs/js/rcloud_bundle.js
@@ -5722,7 +5722,7 @@ RCloud.UI.fatal_dialog = function(message, label, href_or_function) { // no href
         });
         fatal_dialog_ = $('<div id="fatal-dialog" class="modal fade" />')
             .append($('<div class="modal-dialog" />')
-                    .append($('<div class="modal-content" style="background-color: #f2dede" />')
+                    .append($('<div class="modal-content" />')
                             .append($('<div class="modal-body" />')
                                     .append(body))));
         $("body").append(fatal_dialog_);
@@ -7232,6 +7232,43 @@ RCloud.UI.menus = (function() {
 })();
 
 
+
+(function() {
+
+var message_dialog_;
+var message_;
+
+RCloud.UI.message_dialog = function(title, message, k) {
+    $('#loading-animation').hide();
+    if (_.isUndefined(message_dialog_)) {
+        var default_button = $("<button type='submit' class='btn btn-primary' style='float:right'>OK</span>"),
+            body = $('<div />')
+                .append($('<h1 />').append(title));
+        message_ = $('<p style="white-space: pre-wrap">' + message + '</p>');
+        body.append(message_, default_button);
+        body.append('<div style="clear: both;"></div>');
+        default_button.click(function(e) {
+            e.preventDefault();
+            message_dialog_.modal("hide");
+        });
+        message_dialog_ = $('<div id="message-dialog" class="modal fade" />')
+            .append($('<div class="modal-dialog" />')
+                    .append($('<div class="modal-content" />')
+                            .append($('<div class="modal-body" />')
+                                    .append(body))));
+        $("body").append(message_dialog_);
+        message_dialog_.on("shown.bs.modal", function() {
+            default_button.focus();
+        });
+    }
+    else message_.text(message);
+    message_dialog_.off("hidden.bs.modal").on("hidden.bs.modal", function() {
+        k();
+    });
+    message_dialog_.modal({keyboard: true});
+};
+
+})();
 
 RCloud.UI.middle_column = (function() {
     var result = RCloud.UI.column("#middle-column");
