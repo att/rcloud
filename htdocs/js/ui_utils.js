99ef3931072ac7c4ad158d2ca1e4ac829f9f10e2
shane <shane@freshclickmedia.com>
2016-04-25 11:48:12 +0100

    handling for up/down scroll

--- htdocs/js/ui_utils.js
+++ htdocs/js/ui_utils.js
@@ -117,7 +117,8 @@ ui_utils.ace_editor_height = function(widget, min_rows, max_rows)
     min_rows = _.isUndefined(min_rows) ? 0  : min_rows;
     max_rows = _.isUndefined(max_rows) ? 30 : max_rows;
     var lineHeight = widget.renderer.lineHeight;
-    var rows = Math.max(min_rows, Math.min(max_rows, widget.getSession().getScreenLength()));
+    //var rows = Math.max(min_rows, Math.min(max_rows, widget.getSession().getScreenLength()));
+    var rows = Math.max(min_rows, widget.getSession().getScreenLength());
     var newHeight = lineHeight*rows + widget.renderer.scrollBar.getWidth();
     return newHeight;
 };
