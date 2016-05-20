07d5797edf62d69cda59b93dc2297a5f38af4bcc
shane <shane@freshclickmedia.com>
2016-05-20 11:48:40 +0100

    added shift alt enter shortcut to shortcut manager

--- htdocs/js/ui/init.js
+++ htdocs/js/ui/init.js
@@ -239,6 +239,14 @@ RCloud.UI.init = function() {
         ],
         modes: ['writeable'],
         action: function() { }
+    }, {
+        category: 'Cell Management', 
+        id: 'cell_run_from_here', 
+        description: 'Run from here (within a cell)',
+        keys: [
+            ['shift', 'alt', 'enter']
+        ],
+        modes: ['writeable']
     }]);
 
     // general:
