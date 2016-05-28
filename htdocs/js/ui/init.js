164f43c1f74d5b3ac92a007a0be379513ad2ae6f
shane <shane@freshclickmedia.com>
2016-05-28 11:28:55 +0100

    enabled readonly revert, since older version are in readonly mode

--- htdocs/js/ui/init.js
+++ htdocs/js/ui/init.js
@@ -159,7 +159,6 @@ RCloud.UI.init = function() {
             ['ctrl', 'e'],
             ['command', 'e']
         ],
-        modes: ['writeable'],
         action: function() { 
             editor.revert_notebook(shell.notebook.controller.is_mine(), shell.gistname(), shell.version());
         }
