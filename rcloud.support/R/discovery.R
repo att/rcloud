b56fce2cc9b98289e16d7e8900d0df93c2ecdb90
Gordon Woodhull <gordon@woodhull.com>
2016-06-04 09:26:30 -0400

    follow dash-separated for recently-modified key
    for #2014

--- rcloud.support/R/discovery.R
+++ rcloud.support/R/discovery.R
@@ -6,12 +6,12 @@ rcloud.config.get.notebooks.discover <- function(order = "recently.modified") {
 
 rcloud.config.get.recently.modified.notebooks <- function() {
   users <- rcloud.get.users()
-  keys <- rcs.list(usr.key(user=users, notebook="system", "config", "recentlymodified", "*"))
+  keys <- rcs.list(usr.key(user=users, notebook="system", "config", "recently-modified", "*"))
   vals <- rcs.get(keys, list=TRUE)
   names(vals) <- gsub(".*/", "", names(vals))
   vals
 }
 
 rcloud.config.set.recently.modified.notebook <- function(id, date) {
-  rcs.set(usr.key(user=.session$username, notebook="system", "config", "recentlymodified", id), date)
+  rcs.set(usr.key(user=.session$username, notebook="system", "config", "recently-modified", id), date)
 }
