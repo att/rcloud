cb556a8071244b1c7b4b688f00c3613bdbea0890
Gordon Woodhull <gordon@woodhull.com>
2016-06-04 14:34:13 -0400

    promisify loading of discover page
    not sure if this is really needed but what the hay

--- htdocs/js/ui/discovery_page.js
+++ htdocs/js/ui/discovery_page.js
@@ -1,77 +1,79 @@
 RCloud.UI.discovery_page = (function() {
     return {
         init: function() {
-            require([
-                './../../lib/js/imagesloaded',
-                './../../lib/js/masonry.pkgd.min'
-            ], function(imagesLoaded, Masonry) {
-                'use strict';
+            return new Promise(function(resolve, reject) {
+                require([
+                    './../../lib/js/imagesloaded',
+                    './../../lib/js/masonry.pkgd.min'
+                ], function(imagesLoaded, Masonry) {
+                    'use strict';
 
-                window.imagesLoaded = imagesLoaded;
-                window.Masonry = Masonry;
+                    window.imagesLoaded = imagesLoaded;
+                    window.Masonry = Masonry;
 
-                rcloud.config.get_notebooks_discover().then(function(data){
-                    var recent_notebooks_ = _.chain(data)
-                            .pairs()
-                            .filter(function(kv) {
-                                return kv[0] != 'r_attributes' && kv[0] != 'r_type' && !_.isEmpty(editor.get_notebook_info(kv[0])) ;
-                            })
-                            .map(function(kv) { return [kv[0], Date.parse(kv[1])]; })
-                            .sortBy(function(kv) { return kv[1] * -1; })
-                            .first(20)
-                            .map(function(notebook) {
-                                var current = editor.get_notebook_info(notebook[0]);
-                                return rcloud.get_thumb(notebook[0]).then(function(thumb_src){
-                                    return {
-                                        id: notebook[0],
-                                        time: notebook[1],
-                                        description: current.description,
-                                        last_commit: new Date(current.last_commit).toDateString(),
-                                        username: current.username,
-                                        num_stars: editor.num_stars(current[0]),
-                                        image_src: "data:image/png;base64," + thumb_src
-                                    };
-                                });
-                            })
-                            .value();
+                    resolve(rcloud.config.get_notebooks_discover().then(function(data){
+                        var recent_notebooks_ = _.chain(data)
+                                .pairs()
+                                .filter(function(kv) {
+                                    return kv[0] != 'r_attributes' && kv[0] != 'r_type' && !_.isEmpty(editor.get_notebook_info(kv[0])) ;
+                                })
+                                .map(function(kv) { return [kv[0], Date.parse(kv[1])]; })
+                                .sortBy(function(kv) { return kv[1] * -1; })
+                                .first(20)
+                                .map(function(notebook) {
+                                    var current = editor.get_notebook_info(notebook[0]);
+                                    return rcloud.get_thumb(notebook[0]).then(function(thumb_src){
+                                        return {
+                                            id: notebook[0],
+                                            time: notebook[1],
+                                            description: current.description,
+                                            last_commit: new Date(current.last_commit).toDateString(),
+                                            username: current.username,
+                                            num_stars: editor.num_stars(current[0]),
+                                            image_src: "data:image/png;base64," + thumb_src
+                                        };
+                                    });
+                                })
+                                .value();
 
-                    Promise.all(recent_notebooks_).then(function(recent_notebooks) {
-                        $('progress').attr({
-                            max: recent_notebooks.length
-                        });
+                        Promise.all(recent_notebooks_).then(function(recent_notebooks) {
+                            $('progress').attr({
+                                max: recent_notebooks.length
+                            });
 
-                        var template = _.template(
-                            $("#item_template").html()
-                        );
+                            var template = _.template(
+                                $("#item_template").html()
+                            );
 
-                        $('.grid').html(template({
-                            notebooks: recent_notebooks
-                        })).imagesLoaded()
-                            .always(function() {
-                                new Masonry( '.grid', {
-                                    itemSelector: '.grid-item'
-                                });
+                            $('.grid').html(template({
+                                notebooks: recent_notebooks
+                            })).imagesLoaded()
+                                .always(function() {
+                                    new Masonry( '.grid', {
+                                        itemSelector: '.grid-item'
+                                    });
 
-                                $('#progress').fadeOut(200, function() {
-                                    $('.navbar').fadeIn(200, function() {
-                                        $('#discovery-app').css('visibility', 'visible');
-                                        $('body').addClass('loaded');
+                                    $('#progress').fadeOut(200, function() {
+                                        $('.navbar').fadeIn(200, function() {
+                                            $('#discovery-app').css('visibility', 'visible');
+                                            $('body').addClass('loaded');
+                                        });
+                                    });
+                                })
+                                .progress(function(imgLoad, image) {
+                                    if(!image.isLoaded) {
+                                        $(image.img).attr('src', './img/missing.png');
+                                    }
+
+                                    var new_value = +$('progress').attr('value') + 1;
+
+                                    $('progress').attr({
+                                        value: new_value
                                     });
                                 });
-                            })
-                            .progress(function(imgLoad, image) {
-                                if(!image.isLoaded) {
-                                    $(image.img).attr('src', './img/missing.png');
-                                }
-
-                                var new_value = +$('progress').attr('value') + 1;
-
-                                $('progress').attr({
-                                    value: new_value
-                                });
-                            });
-                    });
-                });
+                        });
+                    }));
+                }, reject);
             });
         }
     };
