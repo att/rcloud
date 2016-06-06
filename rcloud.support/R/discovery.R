a9ed50ea32131c9dc19317bb3f4921726380fb03
Gordon Woodhull <gordon@woodhull.com>
2016-06-06 01:05:57 -0400

    most popular notebooks
    by a somewhat dubious metric, anyway (stars + forks)

--- rcloud.support/R/discovery.R
+++ rcloud.support/R/discovery.R
@@ -18,5 +18,26 @@ rcloud.config.set.recently.modified.notebook <- function(id, date) {
   rcs.set(usr.key(user=.session$username, notebook="system", "config", "recently-modified", id), date)
 }
 
-rcloud.config.get.most.popular.notebooks <- function()
-  list(sort='number', values=list())
+middle.key <- function(x) sub('.*/(.*)/.*', '\\1', x)
+
+# i'm sure there's a sharper way to do this
+sum.lists <- function(a, b) {
+    both <- list(a, b)
+    n <- unique(unlist(lapply(both, names)))
+    names(n) <- n
+    lapply(n, function(ni) {
+        x <- if(is.null(a[[ni]])) 0 else as.numeric(a[[ni]])
+        y <- if(is.null(b[[ni]])) 0 else as.numeric(b[[ni]])
+        x + y
+    })
+}
+
+rcloud.config.get.most.popular.notebooks <- function() {
+  starkeys <- rcs.list(rcs.key(".notebook", '*', "starcount"))
+  starvals <- if(length(starkeys) == 0) list() else rcs.get(starkeys, list=TRUE)
+  forkeys <- rcs.list(rcs.key(".notebook", '*', "forkcount"))
+  forkvals <- if(length(forkeys) == 0) list() else rcs.get(forkeys, list=TRUE)
+  names(starvals) <- middle.key(names(starvals))
+  names(forkvals) <- middle.key(names(forkvals))
+  list(sort='number', values=sum.lists(starvals, forkvals))
+}
