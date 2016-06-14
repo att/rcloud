7db00d98e83cfd71c7e260a2446535878bf47e02
shane <shane@freshclickmedia.com>
2016-06-14 20:11:46 +0100

    promise rewrite

--- htdocs/discover_model.js
+++ htdocs/discover_model.js
@@ -6,7 +6,7 @@ var discover = function () {
         get_notebooks : function(notebook_ids) {
 
             var promise = Promise.resolve(),
-                notebook_ids = _.filter(notebook_ids, function(id) { return id[0] !== 'r' });
+                notebook_ids = _.filter(notebook_ids, function(id) { return id.length && id[0] !== 'r' });
 
             var get_ = function(notebook_ids) {
                 var notebooks = {};
@@ -22,27 +22,31 @@ var discover = function () {
             var ids = _.difference(notebook_ids, Object.keys(notebooks_));
 
             if(ids.length) {
-                // get notebooks' info:
-                promise = promise.then(function() {
-                    return rcloud.get_multiple_notebook_infos(ids);
-                }).then(function(notebooks) {
-                    _.extend(notebooks_, notebooks);
-                    return rcloud.stars.get_multiple_notebook_star_counts(ids);
-                }).then(function(stars) {
 
-                    _.each(Object.keys(stars), function(notebook_id){ 
-                        notebooks_[notebook_id].stars = stars[notebook_id];
+                // temp code for forks:
+                promise = Promise.all(
+                    ids.map(function(id) { return rcloud.get_fork_count(id); })
+                );
+
+                promise = promise.then(function(forks) {
+                    return Promise.all([
+                        rcloud.get_multiple_notebook_infos(ids),
+                        rcloud.stars.get_multiple_notebook_star_counts(ids)
+                    ]).spread(function(notebooks, stars) {
+                        
+                        // notebooks:
+                        _.extend(notebooks_, notebooks);
+
+                        // stars:
+                        _.each(Object.keys(stars), function(notebook_id){ 
+                            notebooks_[notebook_id].stars = stars[notebook_id];
+                        });
+
+                    }).then(function() {
+
+                        return Promise.resolve(get_(notebook_ids));
+
                     });
-                   
-                    var fork_promises = ids.map(function(id) { return rcloud.get_fork_count(id); });
-
-                    return Promise.all(fork_promises);
-
-                }).then(function(forks) {
-
-                    // todo: fork data:
-
-                    return Promise.resolve(get_(notebook_ids));  
                 });
 
             } else {
