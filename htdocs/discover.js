4f1c9173e8372b95abf05ec4eb8ab66a32668caa
Gordon Woodhull <gordon@woodhull.com>
2016-06-15 16:07:49 -0400

    remove unneeded initialization
    @shaneporter, I was almost able to remove the dependency on
    shell_tab entirely - not sure if that's the right step but just fyi
    for #2077

--- htdocs/discover.js
+++ htdocs/discover.js
@@ -9,23 +9,5 @@ function main() {
     RCloud.UI.session_pane.init(); // really should be error logger which detects if there is a pane
     RCloud.UI.init();
 
-    RCloud.session.init(true).then(function() {
-        return Promise.all([
-            RCloud.UI.navbar.load(),
-            (rcloud.config ?
-             rcloud.config.get_user_option('show-cell-numbers') :
-             Promise.resolve(true)).then(function(whether) {
-                 if(whether === null) whether = true;
-                 return shell.notebook.controller.show_cell_numbers(whether);
-             })
-        ]);
-    }).then(function() {
-        // we don't want to load_everything, it's not even sufficient with lazy notebook
-        // loading, but keep it for now to keep the circus rolling 
-        return Promise.all([shell.init()]).then(function() {
-            RCloud.UI.advanced_menu.init();
-            RCloud.UI.menus.load();
-            return RCloud.UI.discovery_page.init();
-        });
-    });
+    return RCloud.session.init(true).then(RCloud.UI.discovery_page.init);
 }
