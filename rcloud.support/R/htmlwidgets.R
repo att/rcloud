629c4e890fa70a33ba2cb79e3ce1856dea1aae29
Gábor Csárdi <csardi.gabor@gmail.com>
2016-06-22 11:53:03 +0100

    htmlwidgets: refactoting, preparing for better mini.html support

--- rcloud.support/R/htmlwidgets.R
+++ rcloud.support/R/htmlwidgets.R
@@ -1,15 +1,7 @@
 
 .htmlwidgets.cache <- new.env(parent = emptyenv())
 
-rcloud.htmlwidgets.viewer <- function(url, height) {
-
-  where <- paste0("rc_htmlwidget_", as.integer(runif(1)*1e6))
-  rcloud.html.out(paste0(
-    "<div class=\"rcloud-htmlwidget\">",
-    "<div id=\"", where, "\"></div>",
-    "</div>"))
-  where <- paste0("#", where)
-
+htmlwidgets.install.ocap <- function() {
   if (is.null(.htmlwidgets.cache$ocaps)) {
     jsfile <- file.path(
       system.file(package = "rcloud.support"),
@@ -20,10 +12,24 @@ rcloud.htmlwidgets.viewer <- function(url, height) {
     .htmlwidgets.cache$ocaps <- oc
   }
 
+  .htmlwidgets.cache$ocaps
+}
+
+rcloud.htmlwidgets.viewer <- function(url, height) {
+
+  where <- paste0("rc_htmlwidget_", as.integer(runif(1)*1e6))
+  rcloud.html.out(paste0(
+    "<div class=\"rcloud-htmlwidget\">",
+    "<div id=\"", where, "\"></div>",
+    "</div>"))
+  where <- paste0("#", where)
+
+  ocaps <- htmlwidgets.install.ocap()
+
   htmlwidgets:::pandoc_self_contained_html(url, url)
   widget <- paste(readLines(url), collapse = "\n")
 
-  .htmlwidgets.cache$ocaps$create(where, add.iframe.htmlwidget(widget))
+  ocaps$create(where, add.iframe.htmlwidget(widget))
 
   invisible()
 }
