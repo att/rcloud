53fff7d4beea60d75cc54ab58fd45dd715c1ce11
Gordon Woodhull <gordon@woodhull.com>
2016-07-01 10:04:45 -0400

    ... also discover page

--- htdocs/discover.js
+++ htdocs/discover.js
@@ -1,6 +1,11 @@
 function main() {
     Promise.longStackTraces();
 
+    if(ui_utils.is_ie()) {
+        RCloud.UI.fatal_dialog("Sorry, RCloud does not currently support IE or Edge. Please try another browser.", "Close");
+        return Promise.resolve();
+    }
+
     shell.is_view_mode(true);
     RCloud.UI.session_pane.init(); // really should be error logger which detects if there is a pane
     RCloud.UI.init();
