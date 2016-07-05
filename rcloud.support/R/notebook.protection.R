cf16d35aab4826ae400cb73b78740fa9b866016e
Prateek Baranwal <prateek.baranwal@gmail.com>
2016-07-05 13:48:04 -0400

    Delete , Update solr index while assigning crypt index. Note: Visibility flag is still checked at a lower search update functional call

--- rcloud.support/R/notebook.protection.R
+++ rcloud.support/R/notebook.protection.R
@@ -42,6 +42,10 @@ rcloud.set.notebook.cryptgroup <- function(notebookid, groupid, modify=TRUE) { #
                          stop(e) })
         }
     }
+    ## Remove notebook from SOLR Caching notebook was successfully assigned a crypto group
+    if(is.notebook.encrypted(notebookid)) resp <- .solr.delete.doc(notebookid)
+    ## If group change is set back to Public update the solr index
+    if(!is.notebook.encrypted(notebookid)) resp <- update.solr(rcloud.get.notebook(notebookid),rcloud.notebook.star.count(notebookid))
     invisible(TRUE)
 }
 
