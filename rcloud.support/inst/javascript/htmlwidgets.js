8e74c0e3aa7405756c2bebb4c8b554f52a9e5474
Gábor Csárdi <csardi.gabor@gmail.com>
2016-07-06 15:02:48 +0100

    "Fix" htmlwidgets sizing

--- rcloud.support/inst/javascript/htmlwidgets.js
+++ rcloud.support/inst/javascript/htmlwidgets.js
@@ -7,26 +7,38 @@ function getDocHeight(D) {
     );
 }
 
-function size_this(div) {
+var lastWidths = { };
+
+function size_this(div, reset) {
+    // Check if the widget has a <body> already. If not, we need to wait
+    // a bit
     var D = $(div).find('iframe').contents()[0];
 
     if (!D || !D.body) {
-        setTimeout(function() { size_this($(div)); }, 100);
+        setTimeout(function() { size_this($(div), reset); }, 100);
     } else {
-        var h = getDocHeight(D);
-        $(div).find('iframe').height(h);
-        $(div).find('iframe').attr('height', h);
+        // Check if the width of the iframe is different. If not, then
+        // we don't need to do anything.
+        var rcid = div.id;
+        var width = $(div).find('iframe').width();
+        if (reset || (! rcid in lastWidths) || (lastWidths[rcid] < width)) {
+            var h = getDocHeight(D);
+            $(div).find('iframe').height(h);
+            $(div).find('iframe').attr('height', h);
+        }
+        lastWidths[rcid] = width;
     }
 }
 
-function resize_all() {
+function resize_all(reset) {
     var widgets = $('.rcloud-htmlwidget').find('div');
     $.map(
         widgets,
         function(w) {
-            setTimeout(function() { size_this(w) }, 200)
+            setTimeout(function() { size_this(w, reset) }, 200)
         }
     );
+
     return widgets.length;
 }
 
@@ -42,18 +54,26 @@ function add_hooks() {
 // The resizer is mainly for mini.html, but might be handy for
 // notebook as well, if some widgets resize very slowly.
 
+var lastWidth = window.innerWidth;
+
 $(document).ready(function() {
     add_hooks()
-    function resizer() {
-        var num_widgets = resize_all();
-        if (num_widgets == 0) { setTimeout(resizer, 200); }
+    function resizer(reset) {
+        var num_widgets = resize_all(reset);
+        var interval = 200;
+        if (num_widgets > 0) {
+            setTimeout(resizer, 5000);
+        } else {
+            setTimeout(function() { resizer(true) }, interval);
+        }
     }
-    resizer()
+    resizer(lastWidth < window.innerWidth);
+    lastWidth = window.innerWidth;
 });
 
 function initWidget(div, html, k) {
     $(div).html(html)
-    setTimeout(function() { size_this($(div)); }, 100);
+    setTimeout(function() { size_this($(div), true); }, 100);
     k(null, div);
 }
 
