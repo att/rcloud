05fd4c53b7be799343e933652886b3c5eb44c1de
shane <shane@freshclickmedia.com>
2016-07-11 15:07:15 +0100

    added thumb dialog

---
+++ htdocs/js/ui/thumb_dialog.js
@@ -0,0 +1,13 @@
+RCloud.UI.thumb_dialog = (function() {
+
+    var result = {
+        show: function() {
+            $('#thumb-dialog').modal({
+                //keyboard: false
+            });
+        }
+    };
+
+    return result;
+
+})();
