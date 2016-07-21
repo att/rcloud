448e779d8da55445cddefef9882303b191036f30
shane <shane@freshclickmedia.com>
2016-07-21 15:43:02 +0100

    paste from clipboard, tidy up

--- htdocs/js/ui/thumb_dialog.js
+++ htdocs/js/ui/thumb_dialog.js
@@ -11,126 +11,91 @@ RCloud.UI.thumb_dialog = (function() {
         added_file_ = null,
         thumb_filename_ = 'thumb.png';
 
-    var add_image = function(selector, img_src) {
-        if(selector.find('img').length === 0) {
-            selector.append($('<img/>'));
-        }
-
-        selector.find('img').attr({
-            'src' : img_src
-        });
-
-        return selector;
-    };
-
-    var reset = function() {
-        // remove selected thumb
-        $drop_zone_.removeClass('active dropped');
-        $drop_zone_.find('img').remove();
-        added_file_ = null;
-
-        // reset size of drop zone:
-        $drop_zone_.css('height', $drop_zone_.data('height') + 'px');
-    };
-
-    var verify = function(selected_file) {
-        var valid = selected_file.length === 1 && selected_file[0].type === 'image/png';
-
-        if(!valid) {
-            $instruction_.effect('shake');
-        }
-
-        return valid;
-    };
-
-    var upload = function(file) {
-        // process:
-        added_file_ = file;
-
-        var reader = new FileReader();
-        reader.onload = function(e) {
-            $drop_zone_.addClass('dropped');
-            add_image($drop_zone_, e.target.result);
-
-            // show the complete:
-            $upload_success_.show();
-
-            setTimeout(function() {
-
-                $upload_success_.animate({
-                    'margin-top': '0px', 'opacity' : '0'
-                    }, {
-                        duration: 'fast', 
-                        complete: function() {
-                            $upload_success_.css({ 'opacity' :  '1.0', 'margin-top' : '35px' }).hide();
-                    }
-                });
-
-            }, 1500);
-        }
-
-        reader.readAsDataURL(added_file_);
-    };
-
-    var result = {
-        init: function() {
-
-            $dialog_.on('hidden.bs.modal', function() {
-                reset();
-            });
-
-            $footer_.find('.btn-cancel').on('click', function() { 
-                $dialog_.modal('hide'); 
-                reset();
-            });
-
-            $footer_.find('.btn-primary').on('click', function() { 
-                $dialog_.modal('hide'); 
-
-                if(added_file_) {
-                    //added_file_.name = 'thumb.png';
-                    RCloud.UI.upload_with_alerts(true, {
-                        files: [added_file_],
-                        filenames: [thumb_filename_]
-                    }).catch(function() {}); // we have special handling for upload errors
-                }
-
-                reset();
-            });
-
-            $drop_zone_remove_.click(function() {
-                reset();
-            });
-
-            $thumb_upload_.click(function() {
-                $selected_file_.click();
-            });
-
-            $selected_file_.change(function(evt) {
-                if(verify(evt.target.files)) {
-                    upload(evt.target.files[0]);
-                    // reset so identical file next time would trigger a change:
-                    $selected_file_.val('');
-                }
-            });
-
-            this.setup_asset_drop();
-        },
-        show: function() {
-
-            $drop_zone_.removeClass('active');
-
-            $dialog_.modal({
-                keyboard: true
-            });
-        },
+    var utils = {
         is_visible: function() {
             return $dialog_.is(':visible');
         },
+        add_image: function(selector, img_src) {
+            if(selector.find('img').length === 0) {
+                selector.append($('<img/>'));
+            }
+
+            selector.find('img').attr({
+                'src' : img_src
+            });
+
+            return selector;
+        },
+        reset: function() {
+            // remove selected thumb
+            $drop_zone_.removeClass('active dropped');
+            $drop_zone_.find('img').remove();
+            added_file_ = null;
+
+            // reset size of drop zone:
+            $drop_zone_.css('height', $drop_zone_.data('height') + 'px');
+        },
+        verify: function(selected_file) {
+            var valid = selected_file.length === 1 && selected_file[0].type === 'image/png';
+
+            if(!valid) {
+                $instruction_.effect('shake');
+            }
+
+            return valid;
+        },
+        upload: function(file) {
+            // process:
+            added_file_ = file;
+            var that = this;
+
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                $drop_zone_.addClass('dropped');
+                that.add_image($drop_zone_, e.target.result);
+
+                // show the complete:
+                $upload_success_.show();
+
+                setTimeout(function() {
+
+                    $upload_success_.animate({
+                        'margin-top': '0px', 'opacity' : '0'
+                        }, {
+                            duration: 'fast', 
+                            complete: function() {
+                                $upload_success_.css({ 'opacity' :  '1.0', 'margin-top' : '35px' }).hide();
+                        }
+                    });
+
+                }, 1500);
+            }
+
+            reader.readAsDataURL(added_file_);
+        },
+        setup_paste: function() {
+
+            var that = this;
+
+            $(document).on('paste', function(event){
+                
+                if(!that.is_visible())
+                    return;
+
+                var items = (event.clipboardData || event.originalEvent.clipboardData).items;
+
+                var thumb = _.find(items, function(item) {
+                    return item.kind === 'file';
+                });
+
+                if(thumb && that.verify([thumb])) {
+                    that.upload(thumb.getAsFile());
+                }
+            });
+        },
         setup_asset_drop: function() {
 
-            var that = this,
-                showOverlay_;
+            var showOverlay_, that = this;
 
             $(document).on('dragstart dragenter dragover', function (e) {
 
@@ -178,8 +143,8 @@ RCloud.UI.thumb_dialog = (function() {
                     e = e.originalEvent || e;
                     var files = (e.files || e.dataTransfer.files);
 
-                    if(verify(files)) {
-                        upload(files[0]);
+                    if(that.verify(files)) {
+                        that.upload(files[0]);
                     }
                 },
                 "dragenter dragover": function(e) {
@@ -188,9 +153,72 @@ RCloud.UI.thumb_dialog = (function() {
                         dt.dropEffect = 'copy';
                 }
             });
+        },
+        init: function() {
+
+            var that = this;
+
+            $dialog_.on('hidden.bs.modal', function() {
+                that.reset();
+            });
+
+            $footer_.find('.btn-cancel').on('click', function() { 
+                $dialog_.modal('hide'); 
+                that.reset();
+            });
+
+            $footer_.find('.btn-primary').on('click', function() { 
+                $dialog_.modal('hide'); 
+
+                if(added_file_) {
+                    //added_file_.name = 'thumb.png';
+                    RCloud.UI.upload_with_alerts(true, {
+                        files: [added_file_],
+                        filenames: [thumb_filename_]
+                    }).catch(function() {}); // we have special handling for upload errors
+                }
+
+                that.reset();
+            });
+
+            $drop_zone_remove_.click(function() {
+                that.reset();
+            });
+
+            $thumb_upload_.click(function() {
+                $selected_file_.click();
+            });
+
+            $selected_file_.change(function(evt) {
+                if(that.verify(evt.target.files)) {
+                    that.upload(evt.target.files[0]);
+                    // reset so identical file next time would trigger a change:
+                    $selected_file_.val('');
+                }
+            });
+
+            this.setup_asset_drop();
+            this.setup_paste();
+        },
+        show: function() {
+
+            $drop_zone_.removeClass('active');
+
+            $dialog_.modal({
+                keyboard: true
+            });
         }
     };
 
-    return result;
-
+    return {
+        init: function() {
+            utils.init();            
+        },
+        show: function() {
+            utils.show();
+        },
+        is_visible: function() {
+            return utils.is_visible();
+        }
+    };
 })();
