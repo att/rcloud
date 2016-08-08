a490b364dc4c808fa052fca54cb38e0b78d42eca
Gordon Woodhull <gordon@woodhull.com>
2016-08-08 16:15:43 -0400

    disable readonly ace some more
    fixes #2218

--- htdocs/js/ui_utils.js
+++ htdocs/js/ui_utils.js
@@ -314,6 +314,7 @@ ui_utils.set_ace_readonly = function(widget, readonly) {
         highlightGutterLine: !readonly
     });
     widget.renderer.$cursorLayer.element.style.opacity = readonly?0:1;
+    widget.textInput.getElement().disabled = readonly;
 };
 
 ui_utils.twostate_icon = function(item, on_activate, on_deactivate,
