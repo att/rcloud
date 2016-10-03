e72992cc68827eee9ead646daf8bc03d37e53830
Shane Porter <shane@freshclickmedia.com>
2016-10-03 13:44:12 +0100

    store value and type

--- htdocs/js/ui/pull_and_replace.js
+++ htdocs/js/ui/pull_and_replace.js
@@ -2,14 +2,25 @@ RCloud.UI.pull_and_replace = (function() {
 	
 	var dialog_ = $('#pull-changes-dialog'),
 		select_by_ = $('#pull-changes-by'),
+		// pull_notebook_file_ = $('#pull-notebook-file'),
+		// pull_notebook_url_ = $('#pull-notebook-url'),
+		// pull_notebook_id_ = $('#pull-notebook-id'),
 		btn_cancel_ = dialog_.find('.btn-cancel'),
 		btn_close_ = dialog_.find('.close'),
 		btn_pull_ = dialog_.find('.btn-primary'),
 		method_,
 		show_dialog = function() {
 			rcloud.get_notebook_property(shell.gistname(), 'pull-changes-by').then(function(val) {
-				if(val) {
-					update_pulled_by(val);
+				if(val && val.indexOf(':') !== -1) {
+
+					// split and set:
+					var separatorIndex = val.indexOf(':');
+					update_pulled_by(val.substring(0, separatorIndex));
+					get_input().val(val.substring(separatorIndex + 1));	
+
+				} else {
+					// default to id:
+					update_pulled_by('id');
 				}
 
 				dialog_.modal({
@@ -18,14 +29,20 @@ RCloud.UI.pull_and_replace = (function() {
 			});
 		},
 		update_pulled_by = function(pulled_method) {
-			method_ = pulled_method ? pulled_method : 'id';
+			method_ = pulled_method;
 			select_by_.val(method_);
 			$(dialog_).find('div[data-by]').hide();
 			$(dialog_).find('div[data-by="' + pulled_method + '"]').show();
 		},
 		do_pull = function() {
 			console.log('Notebook pulled');
-			rcloud.set_notebook_property(shell.gistname(), 'pull-changes-by', method_);
+			// ['file', 'url', 'id'].forEach(function(type) {
+			// 	$('pull-notebook-' + type).val('');
+			// });
+			rcloud.set_notebook_property(shell.gistname(), 'pull-changes-by', method_ + ':' + get_input().val());
+		},
+		get_input = function() {
+			return $('#pull-notebook-' + method_);
 		};
 
 	return {
