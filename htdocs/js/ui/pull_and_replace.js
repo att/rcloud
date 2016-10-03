e1dd2b3cb8aaf617a99365de2a9bb2ecc86f8010
Shane Porter <shane@freshclickmedia.com>
2016-10-03 11:03:36 +0100

    retention of pull by method

--- htdocs/js/ui/pull_and_replace.js
+++ htdocs/js/ui/pull_and_replace.js
@@ -5,18 +5,26 @@ RCloud.UI.pull_and_replace = (function() {
 		btn_cancel_ = dialog_.find('.btn-cancel'),
 		btn_close_ = dialog_.find('.close'),
 		btn_pull_ = dialog_.find('.btn-primary'),
-		show = function() {
-			dialog_.modal({
-                keyboard: false
-            });
+		method_,
+		show_dialog = function() {
+			rcloud.get_notebook_property(shell.gistname(), 'pull-changes-by').then(function(val) {
+				if(val) {
+					update_pulled_by(val);
+				}
+
+				dialog_.modal({
+	                keyboard: false
+	            });
+			});
 		},
-		update_change_by = function() {
-			var by_method = select_by_.val();
+		update_pulled_by = function(pulled_method) {
+			method_ = pulled_method ? pulled_method : 'id';
 			$(dialog_).find('div[data-by]').hide();
-			$(dialog_).find('div[data-by="' + by_method + '"]').show();
+			$(dialog_).find('div[data-by="' + pulled_method + '"]').show();
 		},
 		do_pull = function() {
 			console.log('Notebook pulled');
+			rcloud.set_notebook_property(shell.gistname(), 'pull-changes-by', method_);
 		};
 
 	return {
@@ -27,10 +35,7 @@ RCloud.UI.pull_and_replace = (function() {
                     text: "Pull and replace from notebook",
                     modes: ['edit'],
 					action: function() {
-
-						// initialise dialog UI state:
-						update_change_by();
-						dialog_.modal({ keyboard: true });
+						show_dialog();
                     }
                 }
             });
@@ -38,7 +43,10 @@ RCloud.UI.pull_and_replace = (function() {
 			btn_cancel_.click(function() { dialog_.modal('hide'); });
 			btn_close_.click(function() { dialog_.modal('hide'); });
 
-			select_by_.change(update_change_by);
+			select_by_.change(function() {
+				update_pulled_by($(this).val());
+			});
+
 			btn_pull_.click(do_pull);
 
             return this;
