6b032f7d5b20c83a44d848b287d4659c4a7ccf70
Shane Porter <shane@freshclickmedia.com>
2016-10-13 10:23:19 +0100

    next with focus bug fix

--- htdocs/js/ui/find_replace.js
+++ htdocs/js/ui/find_replace.js
@@ -227,21 +227,6 @@ RCloud.UI.find_replace = (function() {
 
         find_dialog_.show();
 
-        var active_cell_selection = get_active_cell_selection();
-
-        if(opts && opts.search_again) {
-            find_input_.val(find_input_.data('searchagain'));
-        } else if(active_cell_selection !== null) {
-            find_input_.val(active_cell_selection);
-        } else {
-            ui_utils.select_allowed_elements();
-            var text = window.getSelection().toString();
-
-            if(text) {
-                find_input_.val(text);
-            }
-        }
-
         if(replace)
             replace_stuff_.show();
         else
@@ -305,6 +290,22 @@ RCloud.UI.find_replace = (function() {
             }
 
         } else {
+            // find/replace dialog already shown:
+            var active_cell_selection = get_active_cell_selection();
+
+            if(opts && opts.search_again) {
+                find_input_.val(find_input_.data('searchagain'));
+            } else if(active_cell_selection !== null) {
+                find_input_.val(active_cell_selection);
+            } else {
+                ui_utils.select_allowed_elements();
+                var text = window.getSelection().toString();
+
+                if(text) {
+                    find_input_.val(text);
+                }
+            }
+
             if(replace) {
                 replace_input_.focus();
             } else {
