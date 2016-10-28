56de411bf87868b274bef4872fa3813550d2b8ad
Gordon Woodhull <gordon@woodhull.com>
2016-10-27 22:49:20 -0400

    unbind findnext and findprevious for cells
    for #2205
    we've got a better command for this

--- htdocs/js/notebook/cell_view.js
+++ htdocs/js/notebook/cell_view.js
@@ -347,6 +347,8 @@ function create_cell_html_view(language, cell_model) {
 
         ace_widget_.resize();
 
+        ace_widget_.commands.removeCommand('findnext');
+        ace_widget_.commands.removeCommand('findprevious');
         ui_utils.install_common_ace_key_bindings(ace_widget_, function() {
             return language;
         });
