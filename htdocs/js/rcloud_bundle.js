588337884a3886734b73d49c7b666cf92294d9a0
Gordon Woodhull <gordon@woodhull.com>
2016-10-28 10:01:25 -0400

    artifacts

--- htdocs/js/rcloud_bundle.js
+++ htdocs/js/rcloud_bundle.js
@@ -8612,67 +8612,68 @@ RCloud.UI.import_export = (function() {
 })();
 
 RCloud.UI.pull_and_replace = (function() {
-	
-	var dialog_ = $('#pull-changes-dialog'),
-		select_by_ = $('#pull-changes-by'),
-		pull_notebook_file_ = $('#pull-notebook-file'),
-		pull_notebook_url_ = $('#pull-notebook-url'),
-		pull_notebook_id_ = $('#pull-notebook-id'),
-		btn_cancel_ = dialog_.find('.btn-cancel'),
-		btn_close_ = dialog_.find('.close'),
-		error_selector_ = '#pull-error',
-		btn_pull_ = dialog_.find('.btn-primary'),
-		inputs_ = [pull_notebook_file_, pull_notebook_url_, pull_notebook_id_],
-		notebook_from_file_,
-		same_notebook_error_ = 'You cannot pull from your current notebook; the source must be a different notebook.',
-		show_dialog = function() {
-			rcloud.get_notebook_property(shell.gistname(), 'pull-changes-by').then(function(val) {
-				if(val && val.indexOf(':') !== -1) {
 
-					// split and set:
-					var separatorIndex = val.indexOf(':');
-					var type = val.substring(0, separatorIndex);
-					var value = val.substring(separatorIndex + 1);
+    var dialog_ = $('#pull-changes-dialog'),
+        select_by_ = $('#pull-changes-by'),
+        pull_notebook_file_ = $('#pull-notebook-file'),
+        pull_notebook_url_ = $('#pull-notebook-url'),
+        pull_notebook_id_ = $('#pull-notebook-id'),
+        btn_cancel_ = dialog_.find('.btn-cancel'),
+        btn_close_ = dialog_.find('.close'),
+        error_selector_ = '#pull-error',
+        btn_pull_ = dialog_.find('.btn-primary'),
+        inputs_ = [pull_notebook_file_, pull_notebook_url_, pull_notebook_id_],
+        notebook_from_file_,
+        same_notebook_error_ = 'You cannot pull from your current notebook; the source must be a different notebook.',
+        show_dialog = function() {
+            rcloud.get_notebook_property(shell.gistname(), 'pull-changes-by').then(function(val) {
+                if(val && val.indexOf(':') !== -1) {
 
-					// update pulled by method:
-					update_pulled_by(type, value);
-				} 
+                    // split and set:
+                    var separatorIndex = val.indexOf(':');
+                    var type = val.substring(0, separatorIndex);
+                    var value = val.substring(separatorIndex + 1);
 
-				dialog_.modal({
-	                keyboard: false
-	            });
-			});
-		},
-		close_dialog = function() {
+                    // update pulled by method:
+                    update_pulled_by(type, value);
+                }
+                else update_pulled_by('url');
 
-			// reset pulling state:
-			reset_pulling_state();
+                dialog_.modal({
+                    keyboard: false
+                });
+            });
+        },
+        close_dialog = function() {
 
-			// 
-			inputs_.forEach(function(input) {
-				input.val('');
-			})
+            // reset pulling state:
+            reset_pulling_state();
 
-			// default to URL for the next time:
-			update_pulled_by('url');
+            //
+            inputs_.forEach(function(input) {
+                input.val('');
+            });
 
-			dialog_.modal('hide');
-		},
-		update_pulled_by = function(pulled_method, value) {
-			clear_error();
-			select_by_.val(pulled_method);
-			$(dialog_).find('div[data-by]').hide();
-			$(dialog_).find('div[data-by="' + pulled_method + '"]').show();
+            // default to URL for the next time:
+            update_pulled_by('url');
 
-			if(!_.isUndefined(value)) {
-				// and set the value coming in:
-				get_input().val(pulled_method === 'file' ? '' : value);
-			}
+            dialog_.modal('hide');
+        },
+        update_pulled_by = function(pulled_method, value) {
+            clear_error();
+            select_by_.val(pulled_method);
+            $(dialog_).find('div[data-by]').hide();
+            $(dialog_).find('div[data-by="' + pulled_method + '"]').show();
 
-			update_pull_button_state();
-		},
-		upload_file = function(file) {
-			Notebook.read_from_file(file, {
+            if(!_.isUndefined(value)) {
+                // and set the value coming in:
+                get_input().val(pulled_method === 'file' ? '' : value);
+            }
+
+            update_pull_button_state();
+        },
+        upload_file = function(file) {
+            Notebook.read_from_file(file, {
                 on_load_end: function() {
                     // TODO
                 },
@@ -8684,141 +8685,143 @@ RCloud.UI.pull_and_replace = (function() {
                     update_pull_button_state();
                 }
             });
-		},
-		do_pull = function() {
+        },
+        do_pull = function() {
 
-			update_when_pulling();
+            update_when_pulling();
 
-			var method = get_method(),
-				value = get_input().val();
-			
-			var get_notebook_func, notebook;
+            var method = get_method(),
+                value = get_input().val();
 
-			if(method === 'id') {
-				get_notebook_func = function() { return rcloud.get_notebook(value); }
-			} else if(method === 'file') {
-				get_notebook_func = function() { return Promise.resolve(notebook_from_file_); }
- 			} else if(method === 'url') {
- 				get_notebook_func = function() { 
- 					var id = RCloud.utils.get_notebook_from_url(value);
- 					if(!id) {
- 						return Promise.reject(new Error('Invalid URL'));	
- 					} else if(id === shell.gistname()) {
- 						return Promise.reject(new Error(same_notebook_error_));
- 					}else {
- 						return rcloud.get_notebook(id);
- 					}
- 				}
- 			}
+            var get_notebook_func, notebook;
 
-			get_notebook_func().then(function(notebook) {
-				rcloud.set_notebook_property(shell.gistname(), 'pull-changes-by', method + ':' + value);
-				editor.pull_and_replace_notebook(notebook).then(function() {
-					close_dialog();
-				});
-			}).catch(function(e) {
-				reset_pulling_state();
-				show_error(e.message);
-			});
+            if(method === 'id') {
+                get_notebook_func = function() { return rcloud.get_notebook(value); };
+            } else if(method === 'file') {
+                get_notebook_func = function() { return Promise.resolve(notebook_from_file_); };
+             } else if(method === 'url') {
+                 get_notebook_func = function() {
+                     var id = RCloud.utils.get_notebook_from_url(value);
+                     if(!id) {
+                         return Promise.reject(new Error('Invalid URL'));
+                     } else if(id === shell.gistname()) {
+                         return Promise.reject(new Error(same_notebook_error_));
+                     }else {
+                         return rcloud.get_notebook(id);
+                     }
+                 };
+             }
 
-		},
-		get_method = function() {
-			return select_by_.val();
-		},
-		get_input = function() {
-			return $('#pull-notebook-' + get_method());
-		},
-		clear_error = function() {
-			$(error_selector_).remove();
-		},		
-		show_error = function(errorText) {
-			clear_error();
+            get_notebook_func().then(function(notebook) {
+                return Promise.all([
+                    rcloud.set_notebook_property(shell.gistname(), 'pull-changes-by', method + ':' + value),
+                    editor.pull_and_replace_notebook(notebook).then(function() {
+                        close_dialog();
+                    })
+                ]);
+            }).catch(function(e) {
+                reset_pulling_state();
+                show_error(e.message);
+            });
 
-			$('<div />', {
-				id: error_selector_.substring(1),
-				text: errorText
-			}).appendTo($(dialog_).find('div[data-by="' + get_method() + '"]'));
+        },
+        get_method = function() {
+            return select_by_.val();
+        },
+        get_input = function() {
+            return $('#pull-notebook-' + get_method());
+        },
+        clear_error = function() {
+            $(error_selector_).remove();
+        },
+        show_error = function(errorText) {
+            clear_error();
 
-			update_pull_button_state();
-		},
-		update_when_pulling = function() {
-			btn_pull_.text('Pulling');
-			dialog_.addClass('pulling');
-		},
-		reset_pulling_state = function() {
-			btn_pull_.text('Pull');
-			dialog_.removeClass('pulling');
-		},
-		update_pull_button_state = function() {
-			var enable = false;
+            $('<div />', {
+                id: error_selector_.substring(1),
+                text: errorText
+            }).appendTo($(dialog_).find('div[data-by="' + get_method() + '"]'));
 
-			if((get_method() === 'id' && get_input().val() != shell.gistname() && get_input().val()) ||
-				(get_method() !== 'id' && get_input().val()) && !$(error_selector_).is(':visible')) {
-				enable = true;
-			}
+            update_pull_button_state();
+        },
+        update_when_pulling = function() {
+            btn_pull_.text('Pulling');
+            dialog_.addClass('pulling');
+        },
+        reset_pulling_state = function() {
+            btn_pull_.text('Pull');
+            dialog_.removeClass('pulling');
+        },
+        update_pull_button_state = function() {
+            var enable = false;
 
-			ui_utils[(enable ? 'enable' : 'disable') + '_bs_button'](btn_pull_);
-		};
+            if((get_method() === 'id' && get_input().val() != shell.gistname() && get_input().val()) ||
+               (get_method() !== 'id' && get_input().val()) && !$(error_selector_).is(':visible')) {
+                enable = true;
+            }
 
-	return {
-		init: function() {
-			RCloud.UI.advanced_menu.add({
- 				pull_and_replace_notebook: {
+            ui_utils[(enable ? 'enable' : 'disable') + '_bs_button'](btn_pull_);
+        };
+
+    return {
+        init: function() {
+            RCloud.UI.advanced_menu.add({
+                 pull_and_replace_notebook: {
                     sort: 3000,
-                    text: "Pull and replace from notebook",
+                    text: "Pull and Replace Notebook",
                     modes: ['edit'],
-					action: function() {
-						show_dialog();
+                    action: function() {
+                        show_dialog();
                     }
                 }
             });
 
-			[btn_cancel_, btn_close_].forEach(function(button) {
-				button.click(close_dialog);
-			});
+            [btn_cancel_, btn_close_].forEach(function(button) {
+                button.click(close_dialog);
+            });
 
-			select_by_.change(function() {
-				pull_notebook_file_.val(null);
-				update_pulled_by($(this).val());
-			});
+            select_by_.change(function() {
+                pull_notebook_file_.val(null);
+                update_pulled_by($(this).val());
+            });
 
-			// inputs:
-			inputs_.forEach(function(input) {
-				input.bind(input.data('pull'), function() { 
-					setTimeout(function() {
+            // inputs:
+            inputs_.forEach(function(input) {
+                input.bind(input.data('pull'), function() {
+                    setTimeout(function() {
 
-						if(get_method() === 'id' && get_input().val() === shell.gistname()) {
-							show_error(same_notebook_error_);
-						} else {
-							clear_error();
-						}
+                        if(get_method() === 'id' && get_input().val() === shell.gistname()) {
+                            show_error(same_notebook_error_);
+                        } else {
+                            clear_error();
+                        }
 
-						update_pull_button_state();
-					}, 0); 
-				});
-			});
+                        update_pull_button_state();
+                    }, 0);
+                });
+            });
 
-			pull_notebook_file_.click(function() {
-				clear_error();
-				pull_notebook_file_.val(null);
-				update_pull_button_state();
-			}).change(function() {
-				upload_file(pull_notebook_file_[0].files[0]); 
-			});
+            pull_notebook_file_.click(function() {
+                clear_error();
+                pull_notebook_file_.val(null);
+                update_pull_button_state();
+            }).change(function() {
+                upload_file(pull_notebook_file_[0].files[0]);
+            });
 
-			[pull_notebook_url_, pull_notebook_id_].forEach(function(control) {
-				control.keydown(function(e) {
-					if(e.keyCode === $.ui.keyCode.ENTER && !btn_pull_.hasClass('disabled')) {
-						do_pull();
-					}
-				});
-			});
+            [pull_notebook_url_, pull_notebook_id_].forEach(function(control) {
+                control.keydown(function(e) {
+                    if(e.keyCode === $.ui.keyCode.ENTER && !btn_pull_.hasClass('disabled')) {
+                        do_pull();
+                    }
+                });
+            });
 
-			btn_pull_.click(do_pull);
+            btn_pull_.click(do_pull);
 
             return this;
         }
-	};
+    };
 })();
 
 RCloud.UI.init = function() {
