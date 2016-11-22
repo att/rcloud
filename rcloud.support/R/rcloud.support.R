c8d079b011a18947347ce061d30e82a153d4545f
Prateek Baranwal <Prateek.Baranwal@gmail.com>
2016-11-22 10:20:43 -0500

    Fix Solr Async Calls on updates
    The latest curl seems to resolve the SSL bug when curl calls are forked

--- rcloud.support/R/rcloud.support.R
+++ rcloud.support/R/rcloud.support.R
@@ -354,8 +354,7 @@ rcloud.update.notebook <- function(id, content, is.current = TRUE) {
     if (nzConf("solr.url") && is.null(group)) { # don't index private/encrypted notebooks
         star.count <- rcloud.notebook.star.count(id)
         # Curl SSL Bug. Don't fork Curl. Refer http://stackoverflow.com/questions/15466809/libcurl-ssl-error-after-fork
-        #mcparallel(update.solr(res, star.count), detached=TRUE)
-        update.solr(res,star.count)
+        mcparallel(update.solr(res, star.count), detached=TRUE) 
     }
     aug.res
 }
