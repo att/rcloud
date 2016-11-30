abdf0fe6edcf18fd6dfd24def29f4ca7b417beba
Gordon Woodhull <gordon@woodhull.com>
2016-11-30 15:20:38 -0500

    remove 750K asset size limit
    fixes #1701
    
    since we transparently load assets which have been truncated by GitHub,
    we no longer need asset size limits.
    
    large uploads may still cause instability, especially in
    firefox (#1680), but that's a separate issue

--- htdocs/js/upload_utils.js
+++ htdocs/js/upload_utils.js
@@ -65,8 +65,6 @@
         return RCloud.utils.promise_sequence(
             options.files,
             function(file) {
-                if(file.size > 750000)
-                    return Promise.reject(new Error('File ' + file.name + ' rejected: maximum asset size is 750KB'));
                 return text_or_binary_reader()(file)
                     .then(function(content) {
                         if(_.isString(content) && Notebook.empty_for_github(content))
