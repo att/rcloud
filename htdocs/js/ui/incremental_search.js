789a109cc15fbe8b5f0ad363fe94d909395ef5d1
Shane Porter <shane@freshclickmedia.com>
2017-09-12 11:28:33 +0100

    dialog basics

--- htdocs/js/ui/incremental_search.js
+++ htdocs/js/ui/incremental_search.js
@@ -1,8 +1,18 @@
 
 RCloud.UI.incremental_search = (function() { 
     
+    var _template = _.template($('#incremental_search_template').html()),
+        _elementSelector = '#incremental_search';
+
     function toggle_incremental_search() {
-        
+        var $el = $(_elementSelector);
+        if($el.length) {
+            $el.remove();
+        } else {
+            $('body').append(_template({
+                // pass data in here
+            }));
+        }
     }
     
     var result = {
