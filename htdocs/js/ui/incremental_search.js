a7bc2d8349ecfe7c5db0f45525c09f6885ee72cc
Shane Porter <shane@freshclickmedia.com>
2017-09-13 12:43:00 +0100

    bootstrap dialog

--- htdocs/js/ui/incremental_search.js
+++ htdocs/js/ui/incremental_search.js
@@ -1,22 +1,23 @@
 
 RCloud.UI.incremental_search = (function() { 
     
-    var _template = _.template($('#incremental_search_template').html()),
-        _elementSelector = '#incremental_search';
+    var _template = _.template($('#tree-finder-template').html()),
+        _elementSelector = '#tree-finder-dialog';
+        _inputsSelector = _elementSelector + ' input',
+        _dialogVisible = false,
+        _dialog = undefined;
 
     function toggle_incremental_search() {
-        var $el = $(_elementSelector);
-        if($el.length) {
-            $el.remove();
-        } else {
-            $('body').append(_template({
-                // pass data in here
-            }));
+        if(!_dialogVisible) {
+            _dialog.modal({keyboard: true});
         }
+
+        _dialogVisible = true;
     }
     
     var result = {
         init: function() {
+            
             RCloud.UI.shortcut_manager.add([{
                 category: 'Incremental Search',
                 id: 'incremental_search',
@@ -33,6 +34,34 @@ RCloud.UI.incremental_search = (function() {
                     toggle_incremental_search();
                 }
             }]);
+            
+            $('body').append(_template({
+                // pass data in here
+            }));
+
+            _dialog = $(_elementSelector);
+
+            $(_dialog).on('hidden.bs.modal', function (e) {
+                $(_elementSelector).modal('hide');
+                $(_elementSelector).find('> p').show();
+                $(_elementSelector).find('.results_list').html('');
+                _dialogVisible = false;
+            });
+
+            // events:
+            var entries = [];
+            $(_inputsSelector).on('keyup', function(e) {
+                $(_inputsSelector).map(function(index) {
+                    entries.push($(this).val());
+                });
+
+                // eventually, do a search with these params:
+                console.log({
+                    username: entries[0],
+                    parentFolderName: entries[1],
+                    name: entries[2]
+                });
+            });
         }
     };
 
