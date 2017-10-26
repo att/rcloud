01ba043463e1f382a28cf0867bd99014b601ab0f
dougmet <douglas.j.ashton@gmail.com>
2017-10-27 00:42:31 +0100

    remove comment from solrsetup

--- conf/solr/solrsetup.sh
+++ conf/solr/solrsetup.sh
@@ -32,7 +32,7 @@ cd "$DEST"
 DEST="`pwd`"
 
 VER=7.1.0
-#curl -L  http://archive.apache.org/dist/lucene/solr/$VER/solr-$VER.tgz | tar xz
+curl -L  http://archive.apache.org/dist/lucene/solr/$VER/solr-$VER.tgz | tar xz
 ## Apache servers are *extremely* slow (<3Mbit!), so use our server instead
 # curl -L -O http://r.research.att.com/solr/solr-$VER.tgz
 
