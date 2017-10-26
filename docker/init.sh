6ba98758c9c7d5a8d085c74b2cf0dfa623c150bb
dougmet <douglas.j.ashton@gmail.com>
2017-10-27 00:41:09 +0100

    reset docker configs

--- docker/init.sh
+++ docker/init.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-sudo su - rcloud -c "ROOT=/data/rcloud /data/rcloud/scripts/build.sh --core"
 sudo su - rcloud -c "ROOT=/data/rcloud /data/rcloud/conf/start"
 sudo su - rcloud -c "/data/rcloud/docker/ulogd/ulogd /data/rcloud/run/ulog"
