cf0474c10a77d5e0bff9f87ae83a38275a0e6f9d
Mateusz Rogalski <useless5771@gmail.com>
2017-11-23 14:23:40 +0000

    Fix resizing of data frame viewer panel, FIX #2503

--- rcloud.packages/rcloud.viewer/inst/javascript/rcloud.viewer.js
+++ rcloud.packages/rcloud.viewer/inst/javascript/rcloud.viewer.js
@@ -27,7 +27,21 @@ return {
     },
     view: function(data, title, k) {
         $('#viewer-body-wrapper > div').remove();
-        $('#viewer-body-wrapper').append($(data));
+        $('#collapse-data-viewer').data('panel-sizer', function(panel) {
+          var widgetDiv = $(panel).find('.rcloud-htmlwidget-content');
+          var height = widgetDiv.data('panel-initial-height'); 
+          var padding = RCloud.UI.collapsible_column.default_padder(panel);
+          return {height: height, padding: padding};
+        });
+        var widgetDiv = $(data);
+        widgetDiv.height('100%');
+        var iframe = widgetDiv.find('iframe');
+        if(iframe.length > 0) {
+          // override whatever fixed value is set on iframe, so it can be resized by column.js
+          widgetDiv.data('panel-initial-height', iframe.get(0).height);
+          iframe.get(0).height = '100%';
+        }
+        $('#viewer-body-wrapper').append(widgetDiv);
         RCloud.UI.right_panel.collapse($("#collapse-data-viewer"), false, false);
         k();
     }
