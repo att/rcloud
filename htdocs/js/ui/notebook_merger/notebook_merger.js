eada5a725e2e526f94d6f0992750c415f3abe9af
Shane Porter <shane@freshclickmedia.com>
2018-02-08 15:23:14 +0000

    initial commit for notebook merger

---
+++ htdocs/js/ui/notebook_merger/notebook_merger.js
@@ -0,0 +1,14 @@
+RCloud.UI.notebook_merger = (function() {
+
+    var notebook_merger = class {
+        constructor() {
+            console.log('creating a new notebook_merger.');
+        }
+        show_dialog() {
+            alert('showing dialog');
+        }
+    }
+
+    return notebook_merger;
+
+})();
