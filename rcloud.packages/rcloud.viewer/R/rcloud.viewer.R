16513f1d9227a364c304ac32fafa5b9b19c90ae6
Shane Porter <shane@freshclickmedia.com>
2018-02-21 14:29:35 +0000

    default page size of 20

--- rcloud.packages/rcloud.viewer/R/rcloud.viewer.R
+++ rcloud.packages/rcloud.viewer/R/rcloud.viewer.R
@@ -1,14 +1,15 @@
 # there doesn't seem to be a lower-level way to customize View?
-validLengths <- c(10, 25, 50, 100, 200)
+validLengths <- c(10, 20, 50, 100, 200)
+defaultLength <- 20
 
 getPageSize <- function() {
   setting <- rcloud.config.get.user.option('dataframe-page-size')   
   if(is.null(setting)) {
-    25
+    defaultLength
   } else if(setting %in% validLengths) {
     setting
   } else {
-    25
+    defaultLength
   }
 }
 
