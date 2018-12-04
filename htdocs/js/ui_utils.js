4a61979a796a350f00d93c05ba8c2f9b51cbad60
Gordon Woodhull <gordon@woodhull.com>
2018-12-04 08:37:58 -0500

    update backspace-prevention code
    we were hitting some of the cases in rcloud.params which had meanwhile been fixed in the SO answer
    fixes #2629

--- htdocs/js/ui_utils.js
+++ htdocs/js/ui_utils.js
@@ -701,23 +701,31 @@ ui_utils.prevent_backspace = function($doc) {
     // Prevent the backspace key from navigating back.
     // from http://stackoverflow.com/a/2768256/676195
     $doc.unbind('keydown').bind('keydown', function (event) {
-        var doPrevent = false;
-        if (event.keyCode === $.ui.keyCode.BACKSPACE) {
-            var d = event.srcElement || event.target;
-            if((d.tagName.toUpperCase() === 'INPUT' &&
-                (d.type.toUpperCase() === 'TEXT' || d.type.toUpperCase() === 'PASSWORD' ||
-                 d.type.toUpperCase() === 'FILE' || d.type.toUpperCase() === 'EMAIL' )) ||
-               d.tagName.toUpperCase() === 'TEXTAREA' ||
-               d.isContentEditable) {
-                doPrevent = d.readOnly || d.disabled;
+        if (event.keyCode === 8) {
+            var doPrevent = true;
+            var types = ["text", "password", "file", "search", "email", "number", "date", "color", "datetime", "datetime-local", "month", "range", "search", "tel", "time", "url", "week"];
+            var d = $(event.srcElement || event.target);
+            var disabled = d.prop("readonly") || d.prop("disabled");
+            if (!disabled) {
+                if (d[0].isContentEditable) {
+                    doPrevent = false;
+                } else if (d.is("input")) {
+                    var type = d.attr("type");
+                    if (type) {
+                        type = type.toLowerCase();
+                    }
+                    if (types.indexOf(type) > -1) {
+                        doPrevent = false;
+                    }
+                } else if (d.is("textarea")) {
+                    doPrevent = false;
+                }
             }
-            else {
-                doPrevent = true;
+            if (doPrevent) {
+                event.preventDefault();
+                return false;
             }
         }
-
-        if(doPrevent)
-            event.preventDefault();
     });
 };
 
