fb6662832c5a3c89855ec65eb79e9f4a61f69843
Gordon Woodhull <gordon@woodhull.com>
2018-12-07 17:03:36 -0500

    restore #2635 error as a warning instead

--- htdocs/js/ui/notebook_merger/merger_view.js
+++ htdocs/js/ui/notebook_merger/merger_view.js
@@ -563,7 +563,7 @@ RCloudNotebookMerger.view = (function(model) {
             })[0];
             let editor_descriptor = this._editors[model_filename];
             if(!editor_descriptor) {
-              //console.error('Editor for file ' + model_filename + ' and model ' + model.id + ' not found!');
+              console.warn('editor for model id ' + model.id + ' not found');
               return [];
             }
             if(editor_descriptor.apply_change) {
