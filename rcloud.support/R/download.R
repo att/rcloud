e1689fd48a2b4d1b2546137b7d011226071fa8e7
Simon Urbanek <simon.urbanek@R-project.org>
2020-03-27 20:49:33 +0000

    make rcloud.download.file() more user-friendly (#2722) and collapse character vectors into one string with newlines (#2721)

--- rcloud.support/R/download.R
+++ rcloud.support/R/download.R
@@ -1,5 +1,21 @@
 .download.file <- NULL
 rcloud.download.file <- function(filename, content, mimetype) {
+  if (!is.character(filename) || length(filename) != 1) stop("filename must be a string")
+  if (missing(content)) {
+     filename <- path.expand(filename)
+     sz <- file.info(filename)$size
+     if (is.na(sz)) stop("file `", filename, "' cannot be accessed")
+     content <- readBin(filename, raw(), sz)
+     filename <- basename(filename)
+  }
+
+  if (missing(mimetype))
+    mimetype <- mime::guess_type(filename)
+
+  ## the JS side (#2721) doesn't append \n between elements, so we have to paste it into one string
+  if (is.character(content) && length(content) > 1L)
+     content <- paste(content, collapse="\n")
+
   if (is.null(.download.file)) {
     code <- paste(readLines(system.file("javascript", "download.js", package="rcloud.support")), collapse='\n')
     .download.file <- rcloud.install.js.module("rcloud.message.dialog", code)
