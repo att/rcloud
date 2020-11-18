b6e726b680ed622174813cf4004863316288e6d2
Gordon Woodhull <gordon@woodhull.com>
2020-11-18 08:17:02 -0500

    setenv ROOT

--- src/R/rcloud.R
+++ src/R/rcloud.R
@@ -1,5 +1,7 @@
 run.rcloud <- function() {
   debug <- FALSE
   rsconf <- system.file('conf/rserve.conf', package='rcloud')
+  root <- system.file('', package='rcloud')
+  Sys.setenv(ROOT=root)
   Rserve::Rserve(debug, args=c("--RS-conf", rsconf, "--vanilla", "--no-save"))
 }
